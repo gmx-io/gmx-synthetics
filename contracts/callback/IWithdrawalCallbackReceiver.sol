58848c5df12c66dfa3cf87c071f8fa2d69430e3d
X <phi@gmx.io>
2022-11-20 12:56:16 +0800

    update callbacks

--- contracts/callback/IWithdrawalCallbackReceiver.sol
+++ contracts/callback/IWithdrawalCallbackReceiver.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 import "../withdrawal/Withdrawal.sol";
 
 interface IWithdrawalCallbackReceiver {
-    function withdrawalExecuted(bytes32 key, Withdrawal.Props memory withdrawal) external;
-    function withdrawalCancelled(bytes32 key, Withdrawal.Props memory withdrawal) external;
+    function beforeWithdrawalExecution(bytes32 key, Withdrawal.Props memory withdrawal) external;
+    function afterWithdrawalExecution(bytes32 key, Withdrawal.Props memory withdrawal) external;
+    function afterWithdrawalCancellation(bytes32 key, Withdrawal.Props memory withdrawal) external;
 }
