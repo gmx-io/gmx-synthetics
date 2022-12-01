b6cdbdddac03b20d918f5f5fd2f9d80384c6e084
X <phi@gmx.io>
2022-12-01 17:16:47 +0800

    add natspec for fees

--- contracts/fee/FeeReceiver.sol
+++ contracts/fee/FeeReceiver.sol
@@ -2,9 +2,22 @@
 
 pragma solidity ^0.8.0;
 
-contract FeeReceiver {
+import "../bank/Bank.sol";
+
+// @title FeeReceiver
+// @dev Contract to receive fees
+contract FeeReceiver is Bank {
+    // @param key the fee action
+    // @param token the fee token
+    // @param amount the fee amount
     event FeeReceived(bytes32 key, address token, uint256 amount);
 
+    constructor(RoleStore _roleStore, DataStore _dataStore) Bank(_roleStore, _dataStore) {}
+
+    // @dev called after a fee is received
+    // @param key the fee action
+    // @param token the fee token
+    // @param amount the fee amount
     function notifyFeeReceived(bytes32 key, address token, uint256 amount) external {
         emit FeeReceived(key, token, amount);
     }
