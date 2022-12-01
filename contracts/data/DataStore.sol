0d6d1536e18d63ab1e5b88cb1e3937c0ef2e4c35
X <phi@gmx.io>
2022-12-01 15:11:08 +0800

    add natspec for ArbSys, Chain, DataStore

--- contracts/data/DataStore.sol
+++ contracts/data/DataStore.sol
@@ -5,33 +5,58 @@ pragma solidity ^0.8.0;
 import "../role/RoleModule.sol";
 import "../utils/Calc.sol";
 
+// @title DataStore
+// @dev DataStore for all general state values
 contract DataStore is RoleModule {
     using SafeCast for int256;
 
+    // store for uint values
     mapping(bytes32 => uint256) public uintValues;
+    // store for int values
     mapping(bytes32 => int256) public intValues;
+    // store for address values
     mapping(bytes32 => address) public addressValues;
+    // store for bool values
     mapping(bytes32 => bool) public boolValues;
+    // store for string values
     mapping(bytes32 => string) public stringValues;
+    // store for bytes32 values
     mapping(bytes32 => bytes32) public dataValues;
 
     constructor(RoleStore _roleStore) RoleModule(_roleStore) {}
 
+    // @dev get the uint value for the given key
+    // @param key the key of the value
+    // @return the uint value for the key
     function getUint(bytes32 key) external view returns (uint256) {
         return uintValues[key];
     }
 
+    // @dev set the uint value for the given key
+    // @param key the key of the value
+    // @param value the value to set
+    // @return the uint value for the key
     function setUint(bytes32 key, uint256 value) external onlyController returns (uint256) {
         uintValues[key] = value;
         return value;
     }
 
+    // @dev add the input int value to the existing uint value
+    // @param key the key of the value
+    // @param value the input int value
+    // @return the new uint value
     function applyDeltaToUint(bytes32 key, int256 value) external onlyController returns (uint256) {
         uint256 nextUint = Calc.sum(uintValues[key], value);
         uintValues[key] = nextUint;
         return nextUint;
     }
 
+    // @dev add the input int value to the existing uint value, prevent the uint
+    // value from becoming negative if ensurePositiveValue is true
+    // @param key the key of the value
+    // @param value the input int value
+    // @param ensurePositiveValue whether to ensure the uint value remains positive
+    // @return the new uint value
     function applyDeltaToUint(bytes32 key, int256 value, bool ensurePositiveValue) external onlyController returns (uint256) {
         uint256 uintValue = uintValues[key];
         if (ensurePositiveValue && value < 0 && (-value).toUint256() > uintValue) {
@@ -44,76 +69,131 @@ contract DataStore is RoleModule {
         return nextUint;
     }
 
+    // @dev add the input uint value to the existing uint value
+    // @param key the key of the value
+    // @param value the input uint value
+    // @return the new uint value
     function incrementUint(bytes32 key, uint256 value) external onlyController returns (uint256) {
         uint256 nextUint = uintValues[key] + value;
         uintValues[key] = nextUint;
         return nextUint;
     }
 
+    // @dev subtract the input uint value from the existing uint value
+    // @param key the key of the value
+    // @param value the input uint value
+    // @return the new uint value
     function decrementUint(bytes32 key, uint256 value) external onlyController returns (uint256) {
         uint256 nextUint = uintValues[key] - value;
         uintValues[key] = nextUint;
         return nextUint;
     }
 
+    // @dev get the int value for the given key
+    // @param key the key of the value
+    // @return the int value for the key
     function getInt(bytes32 key) external view returns (int256) {
         return intValues[key];
     }
 
+    // @dev set the int value for the given key
+    // @param key the key of the value
+    // @param value the value to set
+    // @return the int value for the key
     function setInt(bytes32 key, int256 value) external onlyController returns (int256) {
         intValues[key] = value;
         return value;
     }
 
+    // @dev add the input int value to the existing int value
+    // @param key the key of the value
+    // @param value the input int value
+    // @return the new int value
     function applyDeltaToInt(bytes32 key, int256 value) external onlyController returns (int256) {
         int256 nextInt = intValues[key] + value;
         intValues[key] = nextInt;
         return nextInt;
     }
 
+    // @dev add the input int value to the existing int value
+    // @param key the key of the value
+    // @param value the input int value
+    // @return the new int value
     function incrementInt(bytes32 key, int256 value) external onlyController returns (int256) {
         int256 nextInt = intValues[key] + value;
         intValues[key] = nextInt;
         return nextInt;
     }
 
+    // @dev subtract the input int value from the existing int value
+    // @param key the key of the value
+    // @param value the input int value
+    // @return the new int value
     function decrementInt(bytes32 key, int256 value) external onlyController returns (int256) {
         int256 nextInt = intValues[key] - value;
         intValues[key] = nextInt;
         return nextInt;
     }
 
+    // @dev get the address value for the given key
+    // @param key the key of the value
+    // @return the address value for the key
     function getAddress(bytes32 key) external view returns (address) {
         return addressValues[key];
     }
 
+    // @dev set the address value for the given key
+    // @param key the key of the value
+    // @param value the value to set
+    // @return the address value for the key
     function setAddress(bytes32 key, address value) external onlyController returns (address) {
         addressValues[key] = value;
         return value;
     }
 
+    // @dev get the bool value for the given key
+    // @param key the key of the value
+    // @return the bool value for the key
     function getBool(bytes32 key) external view returns (bool) {
         return boolValues[key];
     }
 
+    // @dev set the bool value for the given key
+    // @param key the key of the value
+    // @param value the value to set
+    // @return the bool value for the key
     function setBool(bytes32 key, bool value) external onlyController returns (bool) {
         boolValues[key] = value;
         return value;
     }
 
+    // @dev get the string value for the given key
+    // @param key the key of the value
+    // @return the string value for the key
     function getString(bytes32 key) external view returns (string memory) {
         return stringValues[key];
     }
 
+    // @dev set the string value for the given key
+    // @param key the key of the value
+    // @param value the value to set
+    // @return the string value for the key
     function setString(bytes32 key, string memory value) external onlyController returns (string memory) {
         stringValues[key] = value;
         return value;
     }
 
+    // @dev get the bytes32 value for the given key
+    // @param key the key of the value
+    // @return the bytes32 value for the key
     function getData(bytes32 key) external view returns (bytes32) {
         return dataValues[key];
     }
 
+    // @dev set the bytes32 value for the given key
+    // @param key the key of the value
+    // @param value the value to set
+    // @return the bytes32 value for the key
     function setData(bytes32 key, bytes32 value) external onlyController returns (bytes32) {
         dataValues[key] = value;
         return value;
