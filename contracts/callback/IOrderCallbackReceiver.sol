ac29f5d7131735136e1d01fd21b5cfb388ce1062
X <phi@gmx.io>
2022-12-01 14:57:42 +0800

    add natspec for callback interfaces

--- contracts/callback/IOrderCallbackReceiver.sol
+++ contracts/callback/IOrderCallbackReceiver.sol
@@ -4,9 +4,26 @@ pragma solidity ^0.8.0;
 
 import "../order/Order.sol";
 
+// @title IOrderCallbackReceiver
+// @dev interface for an order callback contract
 interface IOrderCallbackReceiver {
+    // @dev called before an order execution
+    // @param key the key of the order
+    // @param order the order to be executed
     function beforeOrderExecution(bytes32 key, Order.Props memory order) external;
+
+    // @dev called after an order execution
+    // @param key the key of the order
+    // @param order the order that was executed
     function afterOrderExecution(bytes32 key, Order.Props memory order) external;
+
+    // @dev called after an order cancellation
+    // @param key the key of the order
+    // @param order the order that was cancelled
     function afterOrderCancellation(bytes32 key, Order.Props memory order) external;
+
+    // @dev called after an order has been frozen, see OrderUtils.freezeOrder in OrderHandler for more info
+    // @param key the key of the order
+    // @param order the order that was frozen
     function afterOrderFrozen(bytes32 key, Order.Props memory order) external;
 }
