081afaf48e1b20bd3bbf1fd6d0eb784c93544fcf
X <phi@gmx.io>
2022-12-02 16:44:23 +0800

    add natspec for role contracts

--- contracts/role/Role.sol
+++ contracts/role/Role.sol
@@ -2,13 +2,46 @@
 
 pragma solidity ^0.8.0;
 
+/**
+ * @title Role
+ * @dev Library for role keys
+ */
 library Role {
+    /**
+     * @dev The CONTROLLER role.
+     */
     bytes32 public constant CONTROLLER = keccak256(abi.encode("CONTROLLER"));
+
+    /**
+     * @dev The ROUTER_PLUGIN role.
+     */
     bytes32 public constant ROUTER_PLUGIN = keccak256(abi.encode("ROUTER_PLUGIN"));
+
+    /**
+     * @dev The MARKET_KEEPER role.
+     */
     bytes32 public constant MARKET_KEEPER = keccak256(abi.encode("MARKET_KEEPER"));
+
+    /**
+     * @dev The ORDER_KEEPER role.
+     */
     bytes32 public constant ORDER_KEEPER = keccak256(abi.encode("ORDER_KEEPER"));
+
+    /**
+     * @dev The FROZEN_ORDER_KEEPER role.
+     */
     bytes32 public constant FROZEN_ORDER_KEEPER = keccak256(abi.encode("FROZEN_ORDER_KEEPER"));
+
+    /**
+     * @dev The PRICING_KEEPER role.
+     */
     bytes32 public constant PRICING_KEEPER = keccak256(abi.encode("PRICING_KEEPER"));
+    /**
+     * @dev The LIQUIDATION_KEEPER role.
+     */
     bytes32 public constant LIQUIDATION_KEEPER = keccak256(abi.encode("LIQUIDATION_KEEPER"));
+    /**
+     * @dev The ADL_KEEPER role.
+     */
     bytes32 public constant ADL_KEEPER = keccak256(abi.encode("ADL_KEEPER"));
 }
