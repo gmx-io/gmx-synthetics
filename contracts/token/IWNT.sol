a95867f494f8284b235f8248e4fdfa56d495b031
X <phi@gmx.io>
2022-12-02 17:17:39 +0800

    add natspec for token contracts

--- contracts/token/IWNT.sol
+++ contracts/token/IWNT.sol
@@ -2,7 +2,12 @@
 
 pragma solidity ^0.8.0;
 
-// Interface for Wrapped Native Tokens, e.g. WETH
+/**
+ * @title IWNT
+ * @dev Interface for Wrapped Native Tokens, e.g. WETH
+ * The contract is named WNT instead of WETH for a more general reference name
+ * that can be used on any blockchain
+ */
 interface IWNT {
     function deposit() external payable;
     function withdraw(uint256 amount) external;
