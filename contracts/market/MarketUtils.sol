7f6dd41066ca087592326a966fa9547996a943ef
X <phi@gmx.io>
2023-01-11 15:30:06 +0800

    fix swap path

--- contracts/market/MarketUtils.sol
+++ contracts/market/MarketUtils.sol
@@ -1427,6 +1427,7 @@ library MarketUtils {
     // @param swapPath list of market addresses
     function getEnabledMarkets(DataStore dataStore, MarketStore marketStore, address[] memory swapPath, bool allowSwapPathFlag) internal view returns (Market.Props[] memory) {
         Market.Props[] memory markets = new Market.Props[](swapPath.length);
+        uint256 indexAdjustment = 0;
 
         for (uint256 i = 0; i < swapPath.length; i++) {
             address marketAddress = swapPath[i];
@@ -1437,10 +1438,12 @@ library MarketUtils {
                 marketAddress == SWAP_PNL_TOKEN_TO_COLLATERAL_TOKEN ||
                 marketAddress == SWAP_COLLATERAL_TOKEN_TO_PNL_TOKEN)
             ) {
-                    continue;
+                markets = new Market.Props[](swapPath.length - 1);
+                indexAdjustment = 1;
+                continue;
             }
 
-            markets[i] = getEnabledMarket(dataStore, marketStore, marketAddress);
+            markets[i - indexAdjustment] = getEnabledMarket(dataStore, marketStore, marketAddress);
         }
 
         return markets;
