300a7c38d5ed941b75724b236f4ea40a567c1d22
X <phi@gmx.io>
2023-01-13 15:22:12 +0800

    add file filter for metrics

--- metrics.ts
+++ metrics.ts
@@ -19,7 +19,28 @@ const metrics = new SolidityMetricsContainer("metricsContainerName", options);
 async function run() {
   const files = await getAllFiles("./contracts").toArray();
 
+  const fileFilter;
+  // const fileFilter = {
+  //   "./contracts/deposit/DepositStoreUtils.sol": true,
+  //   "./contracts/deposit/DepositEventUtils.sol": true,
+  //   "./contracts/market/MarketStoreUtils.sol": true,
+  //   "./contracts/market/MarketEventUtils.sol": true,
+  //   "./contracts/order/OrderStoreUtils.sol": true,
+  //   "./contracts/order/OrderEventUtils.sol": true,
+  //   "./contracts/position/PositionStoreUtils.sol": true,
+  //   "./contracts/position/PositionEventUtils.sol": true,
+  //   "./contracts/referral/ReferralEventUtils.sol": true,
+  //   "./contracts/withdrawal/WithdrawalStoreUtils.sol": true,
+  //   "./contracts/withdrawal/WithdrawalEventUtils.sol": true,
+  // };
+
   for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    if (fileFilter && !fileFilter[file]) {
+      continue;
+    }
+
+    console.info(i, file);
     await metrics.analyze(files[i]);
   }
 
