2fe522b6d446e21a438d8f340b62039258be138c
X <phi@gmx.io>
2023-01-31 13:02:57 +0800

    update coverage

--- .solcover.js
+++ .solcover.js
@@ -1,4 +1,15 @@
 module.exports = {
   configureYulOptimizer: true,
-  skipFiles: ["mock", "reader", "test"],
+  skipFiles: [
+    "mock",
+    "reader",
+    "test",
+    "event",
+    "deposit/DepositEventUtils.sol",
+    "market/MarketEventUtils.sol",
+    "order/OrderEventUtils.sol",
+    "position/PositionEventUtils.sol",
+    "referral/ReferralEventUtils.sol",
+    "withdrawal/WithdrawalEventUtils.sol",
+  ],
 };
