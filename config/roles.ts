0b6d78c736a503042e97bf0dd1024ba82b2a5612
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2023-03-21 13:58:31 +0000

    redeploy avalanche fuji add roles config

---
+++ config/roles.ts
@@ -0,0 +1,25 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+export type RolesConfig = {
+  account: string;
+  roles: string[];
+}[];
+
+const config: {
+  [network: string]: RolesConfig;
+} = {
+  avalancheFuji: [
+    {
+      account: "0xC84f3398eDf6336E1Ef55b50Ca3F9f9f96B8b504",
+      roles: ["CONTROLLER", "ORDER_KEEPER", "LIQUIDATION_KEEPER", "MARKET_KEEPER", "FROZEN_ORDER_KEEPER"],
+    },
+    {
+      account: "0xFb11f15f206bdA02c224EDC744b0E50E46137046",
+      roles: ["CONTROLLER", "ORDER_KEEPER", "LIQUIDATION_KEEPER", "MARKET_KEEPER", "FROZEN_ORDER_KEEPER"],
+    },
+  ],
+};
+
+export default async function (hre: HardhatRuntimeEnvironment): Promise<RolesConfig> {
+  return config[hre.network.name];
+}
