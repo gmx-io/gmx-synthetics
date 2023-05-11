ae722da79f25ceca1110bd4d0f5030b9b5ea9119
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2023-05-11 18:17:50 +0100

    add more helpers to OracleModuleTest grant CONTROLLER to OracleModuleTest

--- deploy/deployOracleModuleTest.ts
+++ deploy/deployOracleModuleTest.ts
@@ -1,7 +1,11 @@
 import { createDeployFunction } from "../utils/deploy";
+import { grantRoleIfNotGranted } from "../utils/role";
 
 const func = createDeployFunction({
   contractName: "OracleModuleTest",
+  afterDeploy: async ({ deployedContract }) => {
+    await grantRoleIfNotGranted(deployedContract.address, "CONTROLLER");
+  },
 });
 
 export default func;
