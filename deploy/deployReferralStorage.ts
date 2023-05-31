e8ec510ffd0299d9bf454d95d8cad863dec7ed82
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2023-05-31 18:32:44 +0300

    deploy referral storage in testnet only

--- deploy/deployReferralStorage.ts
+++ deploy/deployReferralStorage.ts
@@ -1,8 +1,13 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { createDeployFunction } from "../utils/deploy";
 
 const func = createDeployFunction({
   contractName: "ReferralStorage",
   id: "ReferralStorage",
 });
+
+func.skip = async ({ network }: HardhatRuntimeEnvironment) => {
+  return network.name !== "avalancheFuji" && network.name !== "arbitrumGoerli";
+};
 
 export default func;
