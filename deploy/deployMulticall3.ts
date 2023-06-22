68986d9fdb34d68d21c735f84ab406f5e7d4111b
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2023-06-22 12:24:39 +0300

    new deployment

--- deploy/deployMulticall3.ts
+++ deploy/deployMulticall3.ts
@@ -2,6 +2,7 @@ import { createDeployFunction } from "../utils/deploy";
 
 const func = createDeployFunction({
   contractName: "Multicall3",
+  id: "Multicall3",
 });
 // override tags
 func.tags = ["Multicall"];
