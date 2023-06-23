cd8a2b85eedae46930926b24943ad16fd7535f8d
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2023-06-23 20:16:50 +0300

    add updating referral tiers to deployment

--- deploy/deployReferralStorage.ts
+++ deploy/deployReferralStorage.ts
@@ -3,7 +3,32 @@ import { createDeployFunction } from "../utils/deploy";
 
 const func = createDeployFunction({
   contractName: "ReferralStorage",
-  id: "ReferralStorage_2",
+  // id: "ReferralStorage_2",
+  afterDeploy: async ({ deployedContract, deployer, network, deployments }) => {
+    const { read, execute, log } = deployments;
+
+    const tiers = [
+      { totalRebate: 1000, discountShare: 5000 },
+      { totalRebate: 2000, discountShare: 5000 },
+      { totalRebate: 2500, discountShare: 4000 },
+    ];
+
+    for (const [tierId, tier] of tiers.entries()) {
+      const onchainTier = await read("ReferralStorage", "tiers", tierId);
+      if (onchainTier.totalRebate.eq(tier.totalRebate) && onchainTier.discountShare.eq(tier.discountShare)) {
+        continue;
+      }
+      log(`Updating tier ${tierId} to ${JSON.stringify(tier)}`);
+      await execute(
+        "ReferralStorage",
+        { from: deployer, log: true },
+        "setTier",
+        tierId,
+        tier.totalRebate,
+        tier.discountShare
+      );
+    }
+  },
 });
 
 func.skip = async ({ network }: HardhatRuntimeEnvironment) => {
