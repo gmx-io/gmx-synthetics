ce885ac0c8c028968323098b501c3a0aa7094f94
X <phi@gmx.io>
2023-07-12 18:19:09 +0800

    update deployments

--- config/roles.ts
+++ config/roles.ts
@@ -28,11 +28,35 @@ export default async function (hre: HardhatRuntimeEnvironment): Promise<RolesCon
         account: "0xe7bfff2ab721264887230037940490351700a068",
         roles: ["CONTROLLER", "MARKET_KEEPER"],
       },
+      {
+        account: "0xE47b36382DC50b90bCF6176Ddb159C4b9333A7AB",
+        roles: ["ORDER_KEEPER", "ADL_KEEPER", "LIQUIDATION_KEEPER", "FROZEN_ORDER_KEEPER"],
+      },
+      {
+        account: "0xC539cB358a58aC67185BaAD4d5E3f7fCfc903700",
+        roles: ["ORDER_KEEPER", "ADL_KEEPER", "LIQUIDATION_KEEPER", "FROZEN_ORDER_KEEPER"],
+      },
+      {
+        account: "0xf1e1B2F4796d984CCb8485d43db0c64B83C1FA6d",
+        roles: ["ORDER_KEEPER", "ADL_KEEPER", "LIQUIDATION_KEEPER", "FROZEN_ORDER_KEEPER"],
+      },
     ],
     avalanche: [
       {
         account: "0xe7bfff2ab721264887230037940490351700a068",
         roles: ["CONTROLLER", "MARKET_KEEPER"],
+      },
+      {
+        account: "0xE47b36382DC50b90bCF6176Ddb159C4b9333A7AB",
+        roles: ["ORDER_KEEPER", "ADL_KEEPER", "LIQUIDATION_KEEPER", "FROZEN_ORDER_KEEPER"],
+      },
+      {
+        account: "0xC539cB358a58aC67185BaAD4d5E3f7fCfc903700",
+        roles: ["ORDER_KEEPER", "ADL_KEEPER", "LIQUIDATION_KEEPER", "FROZEN_ORDER_KEEPER"],
+      },
+      {
+        account: "0xf1e1B2F4796d984CCb8485d43db0c64B83C1FA6d",
+        roles: ["ORDER_KEEPER", "ADL_KEEPER", "LIQUIDATION_KEEPER", "FROZEN_ORDER_KEEPER"],
       },
     ],
     arbitrumGoerli: [
