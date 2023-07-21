2bcadc9be2428b25d8fc188cd1571c06683bcbda
X <phi@gmx.io>
2023-07-21 10:22:03 +0800

    add deployment ids for vaults

--- deploy/deployWithdrawalVault.ts
+++ deploy/deployWithdrawalVault.ts
@@ -8,6 +8,7 @@ const func = createDeployFunction({
   getDeployArgs: async ({ dependencyContracts }) => {
     return constructorContracts.map((dependencyName) => dependencyContracts[dependencyName].address);
   },
+  id: "WithdrawalVault_1",
 });
 
 export default func;
