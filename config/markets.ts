b7da125e4773ae37363fb17c1e979b2c6563b63c
X <phi@gmx.io>
2023-07-25 17:26:42 +0800

    add script to validate markets

--- config/markets.ts
+++ config/markets.ts
@@ -279,8 +279,8 @@ const config: {
       maxLongTokenPoolAmount: expandDecimals(500, 18),
       maxShortTokenPoolAmount: expandDecimals(1_000_000, 6),
 
-      negativePositionImpactFactor: decimalToFloat(15, 10), // 0.3% for 2,000,000 USD of imbalance
-      positivePositionImpactFactor: decimalToFloat(75, 11), // 0.15% for 2,000,000 USD of imbalance
+      negativePositionImpactFactor: decimalToFloat(4, 9), // 0.3% for 750,000 USD of imbalance
+      positivePositionImpactFactor: decimalToFloat(2, 9), // 0.15% for 750,000 USD of imbalance
 
       // use the swap impact factor for WETH
       negativeSwapImpactFactor: decimalToFloat(12, 11), // 0.3% for 25,000,000 USD of imbalance
@@ -301,8 +301,8 @@ const config: {
       maxLongTokenPoolAmount: expandDecimals(500, 18),
       maxShortTokenPoolAmount: expandDecimals(1_000_000, 6),
 
-      negativePositionImpactFactor: decimalToFloat(3, 9), // 0.3% for 1,000,000 USD of imbalance
-      positivePositionImpactFactor: decimalToFloat(15, 10), // 0.15% for 1,000,000 USD of imbalance
+      negativePositionImpactFactor: decimalToFloat(4, 9), // 0.3% for 750,000 USD of imbalance
+      positivePositionImpactFactor: decimalToFloat(2, 9), // 0.15% for 750,000 USD of imbalance
 
       // use the swap impact factor for WETH
       negativeSwapImpactFactor: decimalToFloat(12, 11), // 0.3% for 25,000,000 USD of imbalance
@@ -343,8 +343,8 @@ const config: {
       maxLongTokenPoolAmount: expandDecimals(500, 18),
       maxShortTokenPoolAmount: expandDecimals(1_000_000, 6),
 
-      negativePositionImpactFactor: decimalToFloat(15, 10), // 0.3% for 2,000,000 USD of imbalance
-      positivePositionImpactFactor: decimalToFloat(75, 11), // 0.15% for 2,000,000 USD of imbalance
+      negativePositionImpactFactor: decimalToFloat(4, 9), // 0.3% for 750,000 USD of imbalance
+      positivePositionImpactFactor: decimalToFloat(2, 9), // 0.15% for 750,000 USD of imbalance
 
       // use the swap impact factor for WETH
       negativeSwapImpactFactor: decimalToFloat(12, 11), // 0.3% for 25,000,000 USD of imbalance
@@ -494,8 +494,8 @@ const config: {
       maxLongTokenPoolAmount: expandDecimals(75_000, 18),
       maxShortTokenPoolAmount: expandDecimals(1_000_000, 6),
 
-      negativePositionImpactFactor: decimalToFloat(15, 10), // 0.3% for 2,000,000 USD of imbalance
-      positivePositionImpactFactor: decimalToFloat(75, 11), // 0.15% for 2,000,000 USD of imbalance
+      negativePositionImpactFactor: decimalToFloat(4, 9), // 0.3% for 750,000 USD of imbalance
+      positivePositionImpactFactor: decimalToFloat(2, 9), // 0.15% for 750,000 USD of imbalance
 
       // use the swap impact factor for WAVAX
       negativeSwapImpactFactor: decimalToFloat(1, 8), // 0.3% for 300,000 USD of imbalance
@@ -516,8 +516,8 @@ const config: {
       maxLongTokenPoolAmount: expandDecimals(75_000, 18),
       maxShortTokenPoolAmount: expandDecimals(1_000_000, 6),
 
-      negativePositionImpactFactor: decimalToFloat(3, 9), // 0.3% for 1,000,000 USD of imbalance
-      positivePositionImpactFactor: decimalToFloat(15, 10), // 0.15% for 1,000,000 USD of imbalance
+      negativePositionImpactFactor: decimalToFloat(4, 9), // 0.3% for 750,000 USD of imbalance
+      positivePositionImpactFactor: decimalToFloat(2, 9), // 0.15% for 750,000 USD of imbalance
 
       // use the swap impact factor for WAVAX
       negativeSwapImpactFactor: decimalToFloat(1, 8), // 0.3% for 300,000 USD of imbalance
@@ -558,8 +558,8 @@ const config: {
       maxLongTokenPoolAmount: expandDecimals(75_000, 18),
       maxShortTokenPoolAmount: expandDecimals(1_000_000, 6),
 
-      negativePositionImpactFactor: decimalToFloat(15, 10), // 0.3% for 2,000,000 USD of imbalance
-      positivePositionImpactFactor: decimalToFloat(75, 11), // 0.15% for 2,000,000 USD of imbalance
+      negativePositionImpactFactor: decimalToFloat(4, 9), // 0.3% for 750,000 USD of imbalance
+      positivePositionImpactFactor: decimalToFloat(2, 9), // 0.15% for 750,000 USD of imbalance
 
       // use the swap impact factor for WAVAX
       negativeSwapImpactFactor: decimalToFloat(1, 8), // 0.3% for 300,000 USD of imbalance
---
+++ scripts/validateMarkets.ts
@@ -0,0 +1,190 @@
+import hre from "hardhat";
+import { decimalToFloat } from "../utils/math";
+import * as keys from "../utils/keys";
+
+const recommendedMarketConfig = {
+  arbitrum: {
+    BTC: {
+      negativeImpactFactor: decimalToFloat(5, 11).div(2),
+      expectedImpactRatio: 1,
+    },
+    WETH: {
+      negativeImpactFactor: decimalToFloat(5, 11).div(2),
+      expectedImpactRatio: 1,
+    },
+    LINK: {
+      negativeImpactFactor: decimalToFloat(8, 9).div(2),
+      expectedImpactRatio: 2,
+    },
+    ARB: {
+      negativeImpactFactor: decimalToFloat(8, 9).div(2),
+      expectedImpactRatio: 2,
+    },
+    UNI: {
+      negativeImpactFactor: decimalToFloat(4, 8).div(2),
+      expectedImpactRatio: 2,
+    },
+    LTC: {
+      negativeImpactFactor: decimalToFloat(8, 9).div(2),
+      expectedImpactRatio: 2,
+    },
+    DOGE: {
+      negativeImpactFactor: decimalToFloat(8, 9).div(2),
+      expectedImpactRatio: 2,
+    },
+    SOL: {
+      negativeImpactFactor: decimalToFloat(5, 9).div(2),
+      expectedImpactRatio: 2,
+    },
+    XRP: {
+      negativeImpactFactor: decimalToFloat(5, 9).div(2),
+      expectedImpactRatio: 2,
+    },
+  },
+  avalanche: {
+    "BTC.b": {
+      negativeImpactFactor: decimalToFloat(5, 11).div(2),
+      expectedImpactRatio: 1,
+    },
+    "WETH.e": {
+      negativeImpactFactor: decimalToFloat(5, 11).div(2),
+      expectedImpactRatio: 1,
+    },
+    WAVAX: {
+      negativeImpactFactor: decimalToFloat(1, 8).div(2),
+      expectedImpactRatio: 2,
+    },
+    LTC: {
+      negativeImpactFactor: decimalToFloat(8, 9).div(2),
+      expectedImpactRatio: 2,
+    },
+    DOGE: {
+      negativeImpactFactor: decimalToFloat(8, 9).div(2),
+      expectedImpactRatio: 2,
+    },
+    SOL: {
+      negativeImpactFactor: decimalToFloat(5, 9).div(2),
+      expectedImpactRatio: 2,
+    },
+    XRP: {
+      negativeImpactFactor: decimalToFloat(5, 9).div(2),
+      expectedImpactRatio: 2,
+    },
+  },
+};
+
+const configTokenMapping = {
+  arbitrum: {
+    "WBTC.e": "BTC",
+  },
+};
+
+async function main() {
+  const tokens = await hre.gmx.getTokens();
+  const addressToSymbol: { [address: string]: string } = {};
+  for (const [tokenSymbol, tokenConfig] of Object.entries(tokens)) {
+    let address = tokenConfig.address;
+    if (!address) {
+      address = (await hre.ethers.getContract(tokenSymbol)).address;
+    }
+    addressToSymbol[address] = tokenSymbol;
+  }
+
+  const reader = await hre.ethers.getContract("Reader");
+  const dataStore = await hre.ethers.getContract("DataStore");
+  console.log("reading data from DataStore %s Reader %s", dataStore.address, reader.address);
+  const markets = [...(await reader.getMarkets(dataStore.address, 0, 100))];
+  markets.sort((a, b) => a.indexToken.localeCompare(b.indexToken));
+
+  const errors = [];
+
+  for (const market of markets) {
+    if (market.indexToken == ethers.constants.AddressZero) {
+      continue;
+    }
+
+    const indexTokenSymbol = addressToSymbol[market.indexToken];
+    const longTokenSymbol = addressToSymbol[market.longToken];
+    const shortTokenSymbol = addressToSymbol[market.shortToken];
+
+    console.log(
+      "%s index: %s long: %s short: %s",
+      market.marketToken,
+      indexTokenSymbol?.padEnd(5) || "(swap only)",
+      longTokenSymbol?.padEnd(5),
+      shortTokenSymbol?.padEnd(5)
+    );
+    const recommendedPerpConfig = recommendedMarketConfig[hre.network.name][indexTokenSymbol];
+
+    if (!recommendedPerpConfig || !recommendedPerpConfig.negativeImpactFactor) {
+      throw new Error(`Empty recommendedPerpConfig for ${indexTokenSymbol}`);
+    }
+
+    const negativePositionImpactFactor = await dataStore.getUint(
+      keys.positionImpactFactorKey(market.marketToken, false)
+    );
+    const positivePositionImpactFactor = await dataStore.getUint(
+      keys.positionImpactFactorKey(market.marketToken, true)
+    );
+
+    const percentageOfPerpImpactRecommendation = recommendedPerpConfig.negativeImpactFactor
+      .mul(100)
+      .div(negativePositionImpactFactor);
+
+    console.log(`Position impact % of recommendation: ${percentageOfPerpImpactRecommendation}%`);
+
+    if (!negativePositionImpactFactor.eq(positivePositionImpactFactor.mul(recommendedPerpConfig.expectedImpactRatio))) {
+      throw new Error(`Invalid position impact factors for ${indexTokenSymbol}`);
+    }
+
+    if (negativePositionImpactFactor.lt(recommendedPerpConfig.negativeImpactFactor)) {
+      errors.push({
+        message: `Invalid negativePositionImpactFactor for ${indexTokenSymbol}`,
+        expected: recommendedPerpConfig.negativeImpactFactor,
+        actual: negativePositionImpactFactor,
+      });
+    }
+
+    const recommendedSwapConfig =
+      recommendedMarketConfig[hre.network.name][longTokenSymbol] ||
+      recommendedMarketConfig[hre.network.name][configTokenMapping[hre.network.name][longTokenSymbol]];
+
+    if (!recommendedSwapConfig) {
+      throw new Error(`Empty recommendedSwapConfig for ${longTokenSymbol}`);
+    }
+
+    const negativeSwapImpactFactor = await dataStore.getUint(keys.swapImpactFactorKey(market.marketToken, false));
+    const positiveSwapImpactFactor = await dataStore.getUint(keys.swapImpactFactorKey(market.marketToken, true));
+
+    const percentageOfSwapImpactRecommendation = recommendedSwapConfig.negativeImpactFactor
+      .mul(100)
+      .div(negativeSwapImpactFactor);
+
+    console.log(`Swap impact % of recommendation: ${percentageOfSwapImpactRecommendation}%`);
+
+    if (!negativeSwapImpactFactor.eq(positiveSwapImpactFactor.mul(recommendedSwapConfig.expectedImpactRatio))) {
+      throw new Error(`Invalid swap impact factors for ${longTokenSymbol}`);
+    }
+
+    if (negativeSwapImpactFactor.lt(recommendedSwapConfig.negativeImpactFactor)) {
+      errors.push({
+        message: `Invalid negativeSwapImpactFactor for ${longTokenSymbol}`,
+        expected: recommendedSwapConfig.negativeImpactFactor,
+        actual: negativeSwapImpactFactor,
+      });
+    }
+  }
+
+  for (const error of errors) {
+    console.log(`Error: ${error.message}, expected: ${error.expected.toString()}, actual: ${error.actual.toString()}`);
+  }
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((ex) => {
+    console.error(ex);
+    process.exit(1);
+  });
