8bed4ae992c58e04e44115e1906af61c9fcb48c3
X <phi@gmx.io>
2023-07-27 11:11:24 +0800

    update config

--- test/config/Config.ts
+++ test/config/Config.ts
@@ -4,19 +4,21 @@ import { deployFixture } from "../../utils/fixture";
 import { EXCLUDED_CONFIG_KEYS } from "../../utils/config";
 import { grantRole } from "../../utils/role";
 import { encodeData, hashString } from "../../utils/hash";
+import { decimalToFloat } from "../../utils/math";
 import { TOKEN_ORACLE_TYPES } from "../../utils/oracle";
 import * as keys from "../../utils/keys";
 import Keys from "../../artifacts/contracts/data/Keys.sol/Keys.json";
 
 describe("Config", () => {
   let fixture;
-  let user0;
+  let user0, user1;
   let config, dataStore, roleStore, ethUsdMarket, wnt;
+  const { AddressZero } = ethers.constants;
 
   beforeEach(async () => {
     fixture = await deployFixture();
     ({ config, dataStore, roleStore, ethUsdMarket, wnt } = fixture.contracts);
-    ({ user0 } = fixture.accounts);
+    ({ user0, user1 } = fixture.accounts);
 
     await grantRole(roleStore, user0.address, "CONFIG_KEEPER");
   });
@@ -66,7 +68,7 @@ describe("Config", () => {
   it("setAddress", async () => {
     const key = keys.isMarketDisabledKey(ethUsdMarket.marketToken);
 
-    expect(await dataStore.getAddress(key)).eq(ethers.constants.AddressZero);
+    expect(await dataStore.getAddress(key)).eq(AddressZero);
 
     await config
       .connect(user0)
@@ -113,5 +115,152 @@ describe("Config", () => {
       .setInt(keys.SWAP_IMPACT_FACTOR, encodeData(["address", "bool"], [ethUsdMarket.marketToken, true]), -500);
 
     expect(await dataStore.getInt(key)).eq(-500);
+  });
+
+  it("sets values", async () => {
+    const getValue = ({ type, initial, index }) => {
+      if (type === "Address") {
+        return user1.address;
+      }
+      if (type === "Bool") {
+        return !initial;
+      }
+      return index + 1;
+    };
+
+    const list = [
+      {
+        key: keys.HOLDING_ADDRESS,
+        initial: AddressZero,
+        type: "Address",
+      },
+      {
+        key: keys.MIN_HANDLE_EXECUTION_ERROR_GAS,
+        initial: 1_000_000,
+        type: "Uint",
+      },
+      {
+        key: keys.MAX_SWAP_PATH_LENGTH,
+        initial: 5,
+        type: "Uint",
+      },
+      {
+        key: keys.MAX_CALLBACK_GAS_LIMIT,
+        initial: 2_000_000,
+        type: "Uint",
+      },
+      {
+        key: keys.MIN_POSITION_SIZE_USD,
+        initial: decimalToFloat(1),
+        type: "Uint",
+      },
+      {
+        key: keys.MIN_ORACLE_BLOCK_CONFIRMATIONS,
+        initial: 255,
+        type: "Uint",
+      },
+      {
+        key: keys.MAX_ORACLE_PRICE_AGE,
+        initial: 3600,
+        type: "Uint",
+      },
+      {
+        key: keys.MAX_ORACLE_REF_PRICE_DEVIATION_FACTOR,
+        initial: decimalToFloat(5, 1),
+        type: "Uint",
+      },
+      {
+        key: keys.POSITION_FEE_RECEIVER_FACTOR,
+        initial: 0,
+        type: "Uint",
+      },
+      {
+        key: keys.SWAP_FEE_RECEIVER_FACTOR,
+        initial: 0,
+        type: "Uint",
+      },
+      {
+        key: keys.BORROWING_FEE_RECEIVER_FACTOR,
+        initial: 0,
+        type: "Uint",
+      },
+      {
+        key: keys.ESTIMATED_GAS_FEE_BASE_AMOUNT,
+        initial: 0,
+        type: "Uint",
+      },
+      {
+        key: keys.ESTIMATED_GAS_FEE_MULTIPLIER_FACTOR,
+        initial: 0,
+        type: "Uint",
+      },
+      {
+        key: keys.SINGLE_SWAP_GAS_LIMIT,
+        initial: 0,
+        type: "Uint",
+      },
+      {
+        key: keys.INCREASE_ORDER_GAS_LIMIT,
+        initial: 0,
+        type: "Uint",
+      },
+      {
+        key: keys.DECREASE_ORDER_GAS_LIMIT,
+        initial: 0,
+        type: "Uint",
+      },
+      {
+        key: keys.SWAP_ORDER_GAS_LIMIT,
+        initial: 0,
+        type: "Uint",
+      },
+      {
+        key: keys.NATIVE_TOKEN_TRANSFER_GAS_LIMIT,
+        initial: 50_000,
+        type: "Uint",
+      },
+      {
+        key: keys.REQUEST_EXPIRATION_BLOCK_AGE,
+        initial: 0,
+        type: "Uint",
+      },
+      {
+        key: keys.MAX_UI_FEE_FACTOR,
+        initial: decimalToFloat(5, 5),
+        type: "Uint",
+      },
+      {
+        key: keys.SKIP_BORROWING_FEE_FOR_SMALLER_SIDE,
+        initial: false,
+        type: "Bool",
+      },
+    ];
+
+    for (let i = 0; i < list.length; i++) {
+      const { key, initial, type } = list[i];
+      const getMethod = `get${type}`;
+      const setMethod = `set${type}`;
+      expect(await dataStore[getMethod](key)).eq(initial, `initial ${i}: ${key}`);
+
+      const value = getValue({ type, initial, index: i });
+      await config.connect(user0)[setMethod](key, "0x", value);
+
+      expect(await dataStore[getMethod](key)).eq(value, `after ${i}: ${key}`);
+    }
+  });
+
+  it("sets max pnl factors", async () => {
+    const key = keys.maxPnlFactorKey(keys.MAX_PNL_FACTOR_FOR_TRADERS, ethUsdMarket.marketToken, true);
+    expect(await dataStore.getUint(key)).eq(decimalToFloat(5, 1));
+
+    await config
+      .connect(user0)
+      .setUint(
+        keys.MAX_PNL_FACTOR,
+        encodeData(["bytes32", "address", "bool"], [keys.MAX_PNL_FACTOR_FOR_TRADERS, ethUsdMarket.marketToken, true]),
+        700
+      );
+
+    expect(await dataStore.getUint(key)).eq(700);
   });
 });
