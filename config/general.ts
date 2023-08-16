fdb7777b6dd7fedbe70e1ac5f1b7c3b61049c95e
X <phi@gmx.io>
2023-08-16 10:22:10 +0800

    update limits

--- config/general.ts
+++ config/general.ts
@@ -89,13 +89,13 @@ export default async function ({ network }: HardhatRuntimeEnvironment) {
     },
     arbitrum: {
       requestExpirationBlockAge: 1200, // about 5 minutes assuming 4 blocks per second
-      estimatedGasFeeBaseAmount: 500_000 + 1_250_000,
-      executionGasFeeBaseAmount: 500_000 + 1_250_000,
+      estimatedGasFeeBaseAmount: 2_500_000,
+      executionGasFeeBaseAmount: 2_500_000,
     },
     avalanche: {
       requestExpirationBlockAge: 200, // about 5 minutes assuming 1 block per 3 seconds
-      estimatedGasFeeBaseAmount: 500_000 + 250_000,
-      executionGasFeeBaseAmount: 500_000 + 250_000,
+      estimatedGasFeeBaseAmount: 1_000_000,
+      executionGasFeeBaseAmount: 1_000_000,
     },
   }[network.name];
 
