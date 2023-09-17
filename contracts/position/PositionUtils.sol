a148ce80f9518139a785e5b0098ba0aa3300cf49
X <phi@gmx.io>
2023-09-17 08:42:04 +0800

    fix info.minCollateralUsdForLeverage

--- contracts/position/PositionUtils.sol
+++ contracts/position/PositionUtils.sol
@@ -407,6 +407,13 @@ library PositionUtils {
             + cache.priceImpactUsd
             - collateralCostUsd.toInt256();
 
+        cache.minCollateralFactor = MarketUtils.getMinCollateralFactor(dataStore, market.marketToken);
+
+        // validate if (remaining collateral) / position.size is less than the min collateral factor (max leverage exceeded)
+        // this validation includes the position fee to be paid when closing the position
+        // i.e. if the position does not have sufficient collateral after closing fees it is considered a liquidatable position
+        info.minCollateralUsdForLeverage = Precision.applyFactor(position.sizeInUsd(), cache.minCollateralFactor).toInt256();
+
         if (shouldValidateMinCollateralUsd) {
             info.minCollateralUsd = dataStore.getUint(Keys.MIN_COLLATERAL_USD).toInt256();
             if (info.remainingCollateralUsd < info.minCollateralUsd) {
@@ -417,13 +424,6 @@ library PositionUtils {
         if (info.remainingCollateralUsd <= 0) {
             return (true, "< 0", info);
         }
-
-        cache.minCollateralFactor = MarketUtils.getMinCollateralFactor(dataStore, market.marketToken);
-
-        // validate if (remaining collateral) / position.size is less than the min collateral factor (max leverage exceeded)
-        // this validation includes the position fee to be paid when closing the position
-        // i.e. if the position does not have sufficient collateral after closing fees it is considered a liquidatable position
-        info.minCollateralUsdForLeverage = Precision.applyFactor(position.sizeInUsd(), cache.minCollateralFactor).toInt256();
 
         if (info.remainingCollateralUsd < info.minCollateralUsdForLeverage) {
             return (true, "min collateral for leverage", info);
