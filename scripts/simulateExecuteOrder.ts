d9100c15392fdfb60929cc40d4569ae56a8a529c
X <phi@gmx.io>
2023-10-17 15:38:14 +0800

    update scripts

--- scripts/simulateExecuteOrder.ts
+++ scripts/simulateExecuteOrder.ts
@@ -1,34 +1,25 @@
 import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
 import { expandDecimals } from "../utils/math";
-// import { errorsInterface } from "../utils/error";
-
-// function printErrorReason() {
-//   const reasonBytes = "0x09f8c93700000000000000000000000059c8abb4592e8a317c148d16afec3b459131fa09";
-//   const reason = errorsInterface.parseError(reasonBytes);
-//   console.info("reason", reason);
-// }
 
 // to run the script:
 // 1. add the "forking" info to the hardhat network in hardhat.config.ts
-// 2. npx hardhat run scripts/simulateExecuteOrder.ts
+// 2. for the "forking" config it may be necessary to use a block number that is one less than the failing transaction
+// 3. KEEPER=<keeper address> FOR_NETWORK=<blockchain name> EXCHANGE_ROUTER=<exchange router address> ORDER_KEY=<key> npx hardhat run scripts/simulateExecuteOrder.ts
 //
 // note that the RPC URL must be working otherwise the script may fail
 // with unrelated errors, e.g. OnlyHardhatNetworkError
-async function main() {
-  const address = "0xe47b36382dc50b90bcf6176ddb159c4b9333a7ab";
+
+async function simulateExecuteOrderForArbitrum() {
+  const address = process.env.KEEPER;
   await impersonateAccount(address);
   const impersonatedSigner = await ethers.getSigner(address);
-  const exchangeRouter = await ethers.getContractAt(
-    "ExchangeRouter",
-    "0x79be2F4eC8A4143BaF963206cF133f3710856D0a",
-    impersonatedSigner
-  );
-  await exchangeRouter.simulateExecuteOrder("0x590df51732f141ce1b88dcb1f7c8a79cb617ed4604ef85e303087fcf0be34e2f", {
-    primaryTokens: ["0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7", "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e"],
+  const exchangeRouter = await ethers.getContractAt("ExchangeRouter", process.env.EXCHANGE_ROUTER, impersonatedSigner);
+  await exchangeRouter.simulateExecuteOrder(process.env.ORDER_KEY, {
+    primaryTokens: ["0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"],
     primaryPrices: [
       {
-        min: expandDecimals(1187, 10),
-        max: expandDecimals(1187, 10),
+        min: expandDecimals(1637, 12),
+        max: expandDecimals(1637, 12),
       },
       {
         min: expandDecimals(1, 24),
@@ -36,6 +27,15 @@ async function main() {
       },
     ],
   });
+}
+
+async function main() {
+  if (process.env.FOR_NETWORK === "arbitrum") {
+    await simulateExecuteOrderForArbitrum();
+    return;
+  }
+
+  throw new Error("Unsupported network");
 }
 
 main();
