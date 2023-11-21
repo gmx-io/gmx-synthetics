f9077c31e2263da24194e0eba82b64d128b9d56a
X <phi@gmx.io>
2023-11-21 17:21:56 +0800

    update scripts

--- utils/signer.ts
+++ utils/signer.ts
@@ -2,6 +2,11 @@ import hre from "hardhat";
 import express from "express";
 import cors from "cors";
 
+// to sign using an external wallet:
+// - run `yarn app`
+// - go to http://localhost:5173/signer
+// - connect a wallet and click on the "Sign" button
+
 export async function signExternally(unsignedTransaction) {
   const unsignedTransactionStr = JSON.stringify(unsignedTransaction);
   console.log("Transaction to be signed: ", unsignedTransactionStr);
