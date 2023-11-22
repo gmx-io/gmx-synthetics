1da3416d2e56e42a90b7af515b7156280a64adcd
X <phi@gmx.io>
2023-11-22 13:38:53 +0800

    fix deployments

--- deploy/deployRoleStore.ts
+++ deploy/deployRoleStore.ts
@@ -1,17 +1,8 @@
-import { grantRoleIfNotGranted } from "../utils/role";
 import { createDeployFunction } from "../utils/deploy";
 
 const func = createDeployFunction({
   contractName: "RoleStore",
   id: "RoleStore_3",
-  afterDeploy: async ({ gmx }) => {
-    const rolesConfig = await gmx.getRoles();
-    for (const { account, roles } of rolesConfig) {
-      for (const role of roles) {
-        await grantRoleIfNotGranted(account, role);
-      }
-    }
-  },
 });
 
 func.dependencies = func.dependencies.concat(["FundAccounts"]);
