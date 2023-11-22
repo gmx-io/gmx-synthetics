1afe77f75bda47ea90ea649c03041878b186a4e9
X <phi@gmx.io>
2023-11-22 10:05:31 +0800

    update scripts

--- deploy/configureRoles.ts
+++ deploy/configureRoles.ts
@@ -1,12 +1,36 @@
-import { grantRoleIfNotGranted } from "../utils/role";
+import { grantRoleIfNotGranted, revokeRoleIfGranted } from "../utils/role";
 
-const func = async ({ gmx }) => {
+// example rolesToRemove format:
+// {
+//   arbitrum: [
+//     {
+//       role: "CONTROLLER",
+//       member: "0x9d44B89Eb6FB382b712C562DfaFD8825829b422e",
+//     },
+//   ],
+// };
+
+const rolesToRemove = {
+  hardhat: [],
+  arbitrum: [],
+  avalanche: [],
+  avalancheFuji: [],
+  arbitrumGoerli: [],
+  avalancheFuji: [],
+};
+
+const func = async ({ gmx, network }) => {
   const rolesConfig = await gmx.getRoles();
   for (const role in rolesConfig) {
     const accounts = rolesConfig[role];
     for (const account in accounts) {
       await grantRoleIfNotGranted(account, role);
     }
+  }
+
+  const _rolesToRemove = rolesToRemove[network.name];
+  for (const { account, role } of _rolesToRemove) {
+    await revokeRoleIfGranted(account, role);
   }
 };
 
