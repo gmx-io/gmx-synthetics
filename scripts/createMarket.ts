5f28418354036b78c58028573fe3facac5ec6a8c
X <phi@gmx.io>
2024-02-28 16:56:15 +0800

    update scripts

--- scripts/createMarket.ts
+++ scripts/createMarket.ts
@@ -1,5 +1,5 @@
 import hre from "hardhat";
-import { DEFAULT_MARKET_TYPE } from "../utils/market";
+import { DEFAULT_MARKET_TYPE, createMarketConfigByKey, getMarketKey } from "../utils/market";
 
 async function main() {
   const marketFactory = await hre.ethers.getContract("MarketFactory");
@@ -21,11 +21,30 @@ async function main() {
   const longTokenAddress = tokens[tokenSymbols[1]].address;
   const shortTokenAddress = tokens[tokenSymbols[2]].address;
 
+  const marketConfigs = await hre.gmx.getMarkets();
+  const marketConfigKey = getMarketKey(indexTokenAddress, longTokenAddress, shortTokenAddress);
+  const marketConfigByKey = createMarketConfigByKey({ marketConfigs, tokens });
+  const marketConfig = marketConfigByKey[marketConfigKey];
+
+  if (!marketConfig) {
+    throw new Error("Empty market config");
+  }
+
   console.info(
-    `creating market... indexToken: ${indexTokenAddress}, longToken: ${longTokenAddress}, shortToken: ${shortTokenAddress}`
+    `creating market: indexToken: ${indexTokenAddress}, longToken: ${longTokenAddress}, shortToken: ${shortTokenAddress}`
   );
 
-  await marketFactory.createMarket(indexTokenAddress, longTokenAddress, shortTokenAddress, DEFAULT_MARKET_TYPE);
+  if (process.env.WRITE === "true") {
+    const tx = await marketFactory.createMarket(
+      indexTokenAddress,
+      longTokenAddress,
+      shortTokenAddress,
+      DEFAULT_MARKET_TYPE
+    );
+    console.log(`tx sent: ${tx.hash}`);
+  } else {
+    console.log("NOTE: executed in read-only mode, no transactions were sent");
+  }
 }
 
 main()
