763317a9bb880f6a22704d3328f624cb93c2bb57
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2024-03-14 19:54:48 +0400

    update ChainReader

--- scripts/updateChainReader.ts
+++ scripts/updateChainReader.ts
@@ -3,9 +3,18 @@ import hre from "hardhat";
 async function main() {
   const chainReader = await hre.ethers.getContract("ChainReader");
   console.log("chainReader", chainReader.address);
+  console.log("provider", hre.ethers.provider.connection);
   const latestBlock = await hre.ethers.provider.getBlockNumber();
-  const blockNumber = latestBlock - 1;
-  console.log(`estimating gas for ${blockNumber}`);
+
+  console.log("estimating getBlockHashWithDelayAndLatestBlockNumber(10)");
+  await chainReader.estimateGas.getBlockHashWithDelayAndLatestBlockNumber(10);
+
+  console.log("estimating updateLatestBlockHashWithDelay()");
+  console.log("encoded", chainReader.interface.encodeFunctionData("updateLatestBlockHashWithDelay", []));
+  await chainReader.estimateGas.updateLatestBlockHashWithDelay();
+
+  const blockNumber = latestBlock - 10;
+  console.log(`estimating gas for block ${blockNumber} latest block: ${latestBlock}`);
   const gas = await chainReader.estimateGas.updateLatestBlockHash(blockNumber);
   console.log("gas", gas.toString());
 }
