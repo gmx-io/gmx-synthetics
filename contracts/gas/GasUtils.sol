528fa455eb0294492853a560805191d431d95ddd
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2024-06-10 15:23:32 +0100

    remove comments

--- contracts/gas/GasUtils.sol
+++ contracts/gas/GasUtils.sol
@@ -218,33 +218,23 @@ library GasUtils {
     // @dev get estimated number of oracle prices for deposit
     // @param swapsCount number of swaps in the deposit
     function estimateDepositOraclePriceCount(uint256 swapsCount) internal pure returns (uint256) {
-        // each market requires 3 prices at most
-        // markets in swap path share 1 price at least, 3 prices at most
-        // assuming max swap path length of 3 the deposit uses 8 prices at most, 2 prices at least
         return 3 + swapsCount;
     }
 
     // @dev get estimated number of oracle prices for withdrawal
     // @param swapsCount number of swaps in the withdrawal
     function estimateWithdrawalOraclePriceCount(uint256 swapsCount) internal pure returns (uint256) {
-        // each market requires 3 prices at most
-        // markets in swap path share 1 price at least, 3 prices at most
-        // withdrawal uses 8 prices at most, 2 prices at least
-        // assuming max swap path length of 3 the withdrawal uses 8 prices at most, 2 prices at least
         return 3 + swapsCount;
     }
 
     // @dev get estimated number of oracle prices for order
     // @param swapsCount number of swaps in the order
     function estimateOrderOraclePriceCount(uint256 swapsCount) internal pure returns (uint256) {
-        // assuming max swap path length of 3 the order uses 6 prices at most, 2 prices at least
         return 3 + swapsCount;
     }
 
     // @dev get estimated number of oracle prices for shift
     function estimateShiftOraclePriceCount() internal pure returns (uint256) {
-        // 2 prices for the same long and short tokens
-        // a price for index token of both markets
         return 4;
     }
 
