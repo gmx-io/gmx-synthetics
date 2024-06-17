ee0b65f65f1b35d1bbab92ffd1eb5ea1e0e74875
X <phi@gmx.io>
2024-06-17 16:06:24 +0800

    add tests for refund execution fee callback

--- test/exchange/MarketIncreaseOrder.ts
+++ test/exchange/MarketIncreaseOrder.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 
+import { deployContract } from "../../utils/deploy";
 import { usingResult } from "../../utils/use";
 import { getMarketTokenPriceWithPoolValue } from "../../utils/market";
 import { deployFixture } from "../../utils/fixture";
@@ -235,6 +236,64 @@ describe("Exchange.MarketIncreaseOrder", () => {
         maxPrices: [expandDecimals(5500, 4), expandDecimals(1, 6)],
       },
     });
+  });
+
+  it("refunds execution fee", async () => {
+    await dataStore.setUint(keys.EXECUTION_GAS_FEE_MULTIPLIER_FACTOR, decimalToFloat(1));
+
+    const params = {
+      account: user0,
+      receiver: user1,
+      market: ethUsdMarket,
+      initialCollateralToken: usdc,
+      initialCollateralDeltaAmount: expandDecimals(100 * 1000, 6),
+      swapPath: [],
+      sizeDeltaUsd: decimalToFloat(100 * 1000),
+      acceptablePrice: expandDecimals(4990, 12),
+      executionFee: expandDecimals(2, 15),
+      minOutputAmount: expandDecimals(50000, 6),
+      orderType: OrderType.MarketIncrease,
+      isLong: false,
+      shouldUnwrapNativeToken: false,
+    };
+
+    const initialBalance = await provider.getBalance(user1.address);
+
+    await handleOrder(fixture, { create: params });
+
+    expect((await provider.getBalance(user1.address)).sub(initialBalance)).closeTo("162583985300672", "10000000000000");
+  });
+
+  it("refund execution fee callback", async () => {
+    await dataStore.setUint(keys.EXECUTION_GAS_FEE_MULTIPLIER_FACTOR, decimalToFloat(1));
+    const mockCallbackReceiver = await deployContract("MockCallbackReceiver", []);
+
+    const params = {
+      account: user0,
+      receiver: user1,
+      market: ethUsdMarket,
+      initialCollateralToken: usdc,
+      initialCollateralDeltaAmount: expandDecimals(100 * 1000, 6),
+      swapPath: [],
+      sizeDeltaUsd: decimalToFloat(100 * 1000),
+      acceptablePrice: expandDecimals(4990, 12),
+      executionFee: expandDecimals(2, 15),
+      minOutputAmount: expandDecimals(50000, 6),
+      orderType: OrderType.MarketIncrease,
+      isLong: false,
+      shouldUnwrapNativeToken: false,
+      callbackContract: mockCallbackReceiver,
+    };
+
+    const initialBalance = await provider.getBalance(user1.address);
+
+    expect(await provider.getBalance(mockCallbackReceiver.address)).eq("0");
+
+    await handleOrder(fixture, { create: params });
+
+    expect((await provider.getBalance(user1.address)).sub(initialBalance)).eq(0);
+
+    expect(await provider.getBalance(mockCallbackReceiver.address)).closeTo("142918985143352", "10000000000000");
   });
 
   it("validates reserve", async () => {
