3ba11d99976a7bc44b9720f51c4b3b5810aa34bc
X <phi@gmx.io>
2024-06-20 08:19:51 +0800

    update role scripts

--- scripts/updateRoles.ts
+++ scripts/updateRoles.ts
@@ -79,198 +79,30 @@ async function main() {
   const rolesToAdd = {
     arbitrum: [
       {
-        role: "CONTROLLER",
-        member: "0xd0db5ea893ad4a89e5dbbd94fbd25f0273bbd74c",
-      },
-      {
-        role: "CONTROLLER",
+        role: "ROLE_ADMIN",
         member: "0x2ecb664e934acd5df1ee889dbb2e7d6c1d7ce3cb",
       },
       {
-        role: "CONTROLLER",
-        member: "0xb8fc96d7a413c462f611a7ac0c912c2fe26eabc4",
-      },
-      {
-        role: "CONTROLLER",
-        member: "0xb0c681de9cb4b75ed0a620c04a958bc05f4087b7",
-      },
-      {
-        role: "CONTROLLER",
-        member: "0x26bc03c944a4800299b4bdfb5edce314dd497511",
-      },
-      {
-        role: "CONTROLLER",
-        member: "0x321f3739983cc3e911fd67a83d1ee76238894bd0",
-      },
-      {
-        role: "CONTROLLER",
-        member: "0xa19fa3f0d8e7b7a8963420de504b624167e709b2",
-      },
-      {
-        role: "CONTROLLER",
-        member: "0xb0fc2a48b873da40e7bc25658e5e6137616ac2ee",
-      },
-      {
-        role: "CONTROLLER",
+        role: "ROUTER_PLUGIN",
         member: "0x69c527fc77291722b52649e45c838e41be8bf5d5",
       },
       {
-        role: "CONTROLLER",
-        member: "0x55e9a5e1aed46500f746f7683e87f3d9f3c1e14e",
-      },
-      {
-        role: "CONTROLLER",
-        member: "0x08a902113f7f41a8658ebb1175f9c847bf4fb9d8",
-      },
-      {
-        role: "CONTROLLER",
+        role: "ROUTER_PLUGIN",
         member: "0x9f48160edc3ad78f4ca0e3fdf54a75d8fb228452",
-      },
-      {
-        role: "CONTROLLER",
-        member: "0x4895170e184441da9bd2bf95c120c07ba628eef0",
-      },
-      {
-        role: "CONTROLLER",
-        member: "0xea90ec1228f7d1b3d47d84d1c9d46dbdfeff7709",
       },
     ],
     avalanche: [
       {
-        role: "ADL_KEEPER",
-        member: "0x8e66ee36f2c7b9461f50aa0b53ef0e4e47f4abbf",
-      },
-      {
-        role: "ADL_KEEPER",
-        member: "0x6a2b3a13be0c723674bcfd722d4e133b3f356e05",
-      },
-      {
-        role: "ADL_KEEPER",
-        member: "0xdd5c59b7c4e8fad38732caffbebd20a61bf9f3fc",
-      },
-      {
-        role: "ADL_KEEPER",
-        member: "0xeb2bb25ddd2b1872d5189ae72fcec9b160dd3fb2",
-      },
-      {
-        role: "CONTROLLER",
-        member: "0x8514fc704317057fa86961ba9b9490956993a5ed",
-      },
-      {
-        role: "CONTROLLER",
+        role: "ROLE_ADMIN",
         member: "0x844d38f2c3875b8351feb4764718e1c64bd55c46",
       },
       {
-        role: "CONTROLLER",
-        member: "0xad7a7568f500f65aea3d9417a210cbc5dcd7b273",
-      },
-      {
-        role: "CONTROLLER",
-        member: "0xb54c8fb6b2f143dd58f5b00fde7da4fa05077b20",
-      },
-      {
-        role: "CONTROLLER",
-        member: "0x352f684ab9e97a6321a13cf03a61316b681d9fd2",
-      },
-      {
-        role: "CONTROLLER",
-        member: "0xae2453dca7704080052af3c212e862cab50d65c0",
-      },
-      {
-        role: "CONTROLLER",
-        member: "0xd1b861b50f8d8f9dd922453d1234a2abdf4d4ea5",
-      },
-      {
-        role: "CONTROLLER",
-        member: "0x32a0258007a6ea78265a5ae4dbb28f176be4a8eb",
-      },
-      {
-        role: "CONTROLLER",
+        role: "ROUTER_PLUGIN",
         member: "0x3be24aed1a4ccadebf2956e02c27a00726d4327d",
       },
       {
-        role: "CONTROLLER",
-        member: "0xcf2ffd3fc8d2cf78d087681f9acd35c799e0d88d",
-      },
-      {
-        role: "CONTROLLER",
-        member: "0x0e9a0419e5144fe3c73ff30446a1e4d04e1224f0",
-      },
-      {
-        role: "CONTROLLER",
+        role: "ROUTER_PLUGIN",
         member: "0xe5485a4fd6527911e9b82a75a1bfed6e47be2241",
-      },
-      {
-        role: "CONTROLLER",
-        member: "0x28ad6ff2683a3d36c05f1d9ec95b907086431a27",
-      },
-      {
-        role: "CONTROLLER",
-        member: "0x7da618ee7b32af18b749a3715332dbcd820d0913",
-      },
-      {
-        role: "FROZEN_ORDER_KEEPER",
-        member: "0x8e66ee36f2c7b9461f50aa0b53ef0e4e47f4abbf",
-      },
-      {
-        role: "FROZEN_ORDER_KEEPER",
-        member: "0x6a2b3a13be0c723674bcfd722d4e133b3f356e05",
-      },
-      {
-        role: "FROZEN_ORDER_KEEPER",
-        member: "0xdd5c59b7c4e8fad38732caffbebd20a61bf9f3fc",
-      },
-      {
-        role: "FROZEN_ORDER_KEEPER",
-        member: "0xeb2bb25ddd2b1872d5189ae72fcec9b160dd3fb2",
-      },
-      {
-        role: "LIMITED_CONFIG_KEEPER",
-        member: "0x8e66ee36f2c7b9461f50aa0b53ef0e4e47f4abbf",
-      },
-      {
-        role: "LIMITED_CONFIG_KEEPER",
-        member: "0x6a2b3a13be0c723674bcfd722d4e133b3f356e05",
-      },
-      {
-        role: "LIMITED_CONFIG_KEEPER",
-        member: "0xdd5c59b7c4e8fad38732caffbebd20a61bf9f3fc",
-      },
-      {
-        role: "LIMITED_CONFIG_KEEPER",
-        member: "0xeb2bb25ddd2b1872d5189ae72fcec9b160dd3fb2",
-      },
-      {
-        role: "LIQUIDATION_KEEPER",
-        member: "0x8e66ee36f2c7b9461f50aa0b53ef0e4e47f4abbf",
-      },
-      {
-        role: "LIQUIDATION_KEEPER",
-        member: "0x6a2b3a13be0c723674bcfd722d4e133b3f356e05",
-      },
-      {
-        role: "LIQUIDATION_KEEPER",
-        member: "0xdd5c59b7c4e8fad38732caffbebd20a61bf9f3fc",
-      },
-      {
-        role: "LIQUIDATION_KEEPER",
-        member: "0xeb2bb25ddd2b1872d5189ae72fcec9b160dd3fb2",
-      },
-      {
-        role: "ORDER_KEEPER",
-        member: "0x8e66ee36f2c7b9461f50aa0b53ef0e4e47f4abbf",
-      },
-      {
-        role: "ORDER_KEEPER",
-        member: "0x6a2b3a13be0c723674bcfd722d4e133b3f356e05",
-      },
-      {
-        role: "ORDER_KEEPER",
-        member: "0xdd5c59b7c4e8fad38732caffbebd20a61bf9f3fc",
-      },
-      {
-        role: "ORDER_KEEPER",
-        member: "0xeb2bb25ddd2b1872d5189ae72fcec9b160dd3fb2",
       },
     ],
   };
