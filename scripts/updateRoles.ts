4fb8307b91fa31f1b049c4a818d051cc01e80875
X <phi@gmx.io>
2024-07-15 15:32:34 +0800

    update scripts

--- scripts/updateRoles.ts
+++ scripts/updateRoles.ts
@@ -14,11 +14,11 @@ async function getTimelock() {
   const network = hre.network.name;
 
   if (network === "arbitrum") {
-    return await ethers.getContractAt("Timelock", "0x62aB76Ed722C507f297f2B97920dCA04518fe274");
+    return await ethers.getContractAt("Timelock", "0x2ECB664e934aCd5DF1EE889Dbb2E7D6C1d7CE3Cb");
   }
 
   if (network === "avalanche") {
-    return await ethers.getContractAt("Timelock", "0x4Db91a1Fa4ba3c75510B2885d7d7da48E0209F38");
+    return await ethers.getContractAt("Timelock", "0x844D38f2c3875b8351feB4764718E1c64bD55c46");
   }
 
   throw new Error("Unsupported network");
@@ -77,40 +77,151 @@ async function main() {
   const timelock = await getTimelock();
 
   const rolesToAdd = {
+    arbitrum: [],
+    avalanche: [],
+  };
+
+  const rolesToRemove = {
     arbitrum: [
       {
-        role: "ROLE_ADMIN",
-        member: "0x2ecb664e934acd5df1ee889dbb2e7d6c1d7ce3cb",
+        role: "CONTROLLER",
+        member: "0x7C68C7866A64FA2160F78EEaE12217FFbf871fa8",
       },
       {
         role: "ROUTER_PLUGIN",
-        member: "0x69c527fc77291722b52649e45c838e41be8bf5d5",
+        member: "0x7C68C7866A64FA2160F78EEaE12217FFbf871fa8",
+      },
+      {
+        role: "CONTROLLER",
+        member: "0x78F414436148B8588BDEe4771EA5eB75148668aa",
       },
       {
         role: "ROUTER_PLUGIN",
-        member: "0x9f48160edc3ad78f4ca0e3fdf54a75d8fb228452",
+        member: "0x78F414436148B8588BDEe4771EA5eB75148668aa",
       },
     ],
     avalanche: [
       {
-        role: "ROLE_ADMIN",
-        member: "0x844d38f2c3875b8351feb4764718e1c64bd55c46",
+        role: "CONTROLLER",
+        member: "0x11E590f6092D557bF71BaDEd50D81521674F8275",
       },
       {
         role: "ROUTER_PLUGIN",
-        member: "0x3be24aed1a4ccadebf2956e02c27a00726d4327d",
+        member: "0x11E590f6092D557bF71BaDEd50D81521674F8275",
+      },
+      {
+        role: "CONTROLLER",
+        member: "0xA60862ecc8bd976519e56231bDfAF697C5ce2156",
       },
       {
         role: "ROUTER_PLUGIN",
-        member: "0xe5485a4fd6527911e9b82a75a1bfed6e47be2241",
+        member: "0xA60862ecc8bd976519e56231bDfAF697C5ce2156",
       },
     ],
   };
 
-  const rolesToRemove = {
-    arbitrum: [],
-    avalanche: [],
-  };
+  // const rolesToRemove = {
+  //   arbitrum: [
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0x9e0521C3dbB18E849F4955087E065E5C9C879917",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0xbF56A2F030C3F920F0E2aD9Cf456B9954c49383a",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0x352f684ab9e97a6321a13CF03A61316B681D9fD2",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0x9E32088F3c1a5EB38D32d1Ec6ba0bCBF499DC9ac",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0x9Dc4f12Eb2d8405b499FB5B8AF79a5f64aB8a457",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0x8514fc704317057FA86961Ba9b9490956993A5ed",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0xF6b804F6Cc847a22F2D022C9b0373190850bE34D",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0xa11B501c2dd83Acd29F6727570f2502FAaa617F2",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0x62aB76Ed722C507f297f2B97920dCA04518fe274",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0x226ED647C6eA2C0cE4C08578e2F37b8c2F922849",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0x1847C11d9B11aDDb48e4bB2b55fCE6F9D1606039",
+  //     },
+  //     {
+  //       role: "ROLE_ADMIN",
+  //       member: "0x62aB76Ed722C507f297f2B97920dCA04518fe274",
+  //     },
+  //   ],
+  //   avalanche: [
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0x7309223E21dc2FFbb660E5Bd5aBF95ae06ba4Da0",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0x931C18AF613f56289253F0EeD57F315dE7dbAFcd",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0xc7D8E3561f1247EBDa491bA5f042699C2807C33C",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0xd3B6E962f135634C43415d57A28E688Fb4f15A58",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0x790Ee987b9B253374d700b07F16347a7d4C4ff2e",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0x72fa3978E2E330C7B2debc23CB676A3ae63333F6",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0x5c5DBbcDf420B5d81d4FfDBa5b26Eb24E6E60d52",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0xEE027373517a6D96Fe62f70E9A0A395cB5a39Eee",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0x090FA7eb8B4647DaDbEA315E68f8f88e8E62Bd54",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0x4Db91a1Fa4ba3c75510B2885d7d7da48E0209F38",
+  //     },
+  //     {
+  //       role: "CONTROLLER",
+  //       member: "0xb964d8f746fA13024aEEDAeF1d015698bbD0cFCE",
+  //     },
+  //     {
+  //       role: "ROLE_ADMIN",
+  //       member: "0x4Db91a1Fa4ba3c75510B2885d7d7da48E0209F38",
+  //     },
+  //   ],
+  // };
 
   const multicallWriteParams = [];
 
