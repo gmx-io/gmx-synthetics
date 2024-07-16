f701736a3eb26e3d6eb130f5bb5964dbeac18026
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2024-07-16 18:21:52 +0400

    update init timestamp scripts to support LIMIT env var

--- scripts/initializeOrderTimestamps.ts
+++ scripts/initializeOrderTimestamps.ts
@@ -7,16 +7,25 @@ async function main() {
 
   const ordersCount = await dataStore.getBytes32Count(keys.ORDER_LIST);
 
+  let limit = ordersCount;
+  if (process.env.LIMIT) {
+    limit = Math.min(Number(process.env.LIMIT), ordersCount);
+  }
+  console.log("ordersCount: %s limit: %s", ordersCount, limit);
+
   const chunkLength = 100;
-  for (let from = 0; from < ordersCount; from += chunkLength) {
-    const to = Math.min(ordersCount, from + chunkLength);
+  for (let from = 0; from < limit; from += chunkLength) {
+    const to = Math.min(limit, from + chunkLength);
+
     console.log("updating orders chunk %s-%s", from, to);
 
     if (process.env.WRITE === "true") {
       const tx = await timestampInitializer.initializeOrderTimestamps(from, to);
       console.log("tx sent %s", tx.hash);
     } else {
-      await timestampInitializer.callStatic.initializeOrderTimestamps(from, to, {
+      await hre.ethers.provider.call({
+        to: timestampInitializer.address,
+        data: timestampInitializer.interface.encodeFunctionData("initializeOrderTimestamps", [from, to]),
         from: "0xe47b36382dc50b90bcf6176ddb159c4b9333a7ab",
       });
     }
