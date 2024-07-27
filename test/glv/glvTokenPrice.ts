bdee195d09ca8f9a771f2e60afd1db45ef7c0906
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2024-07-28 01:05:02 +0400

    glv: add tests for glv token price

--- test/glv/glvTokenPrice.ts
+++ test/glv/glvTokenPrice.ts
@@ -10,7 +10,7 @@ import {
   handleGlvWithdrawal,
 } from "../../utils/glv";
 import { deployFixture } from "../../utils/fixture";
-import { expandDecimals } from "../../utils/math";
+import { decimalToFloat, expandDecimals } from "../../utils/math";
 import { getBalanceOf } from "../../utils/token";
 import { errorsContract } from "../../utils/error";
 import * as keys from "../../utils/keys";
@@ -18,6 +18,7 @@ import { increaseTime } from "../../utils/time";
 import { printGasUsage } from "../../utils/gas";
 import { contractAt } from "../../utils/deploy";
 import { BigNumberish } from "ethers";
+import { handleDeposit } from "../../utils/deposit";
 
 function getPriceProp(price: BigNumberish, decimals: number) {
   return {
@@ -28,12 +29,12 @@ function getPriceProp(price: BigNumberish, decimals: number) {
 
 describe("Glv Token Price", () => {
   let fixture;
-  let glvReader, dataStore, ethUsdMarket, solUsdMarket, ethUsdGlvAddress;
+  let glvReader, reader, dataStore, ethUsdMarket, solUsdMarket, ethUsdGlvAddress, wnt, usdc;
 
   beforeEach(async () => {
     fixture = await deployFixture();
 
-    ({ glvReader, dataStore, ethUsdMarket, solUsdMarket, ethUsdGlvAddress } = fixture.contracts);
+    ({ glvReader, wnt, usdc, reader, dataStore, ethUsdMarket, solUsdMarket, ethUsdGlvAddress } = fixture.contracts);
   });
 
   it("glv token price", async () => {
@@ -47,9 +48,21 @@ describe("Glv Token Price", () => {
         ethUsdGlvAddress,
         true
       );
-      console.log("price: %s value: %s supply: %s", price, value, supply);
       return [price, value, supply];
     }
+
+    // deposit $50k WETH / $50k USDC directly at different price
+    // for market token and glv token prices differ
+    await handleDeposit(fixture, {
+      create: {
+        longTokenAmount: expandDecimals(8, 18),
+        shortTokenAmount: expandDecimals(10 * 5000, 6),
+      },
+      execute: {
+        minPrices: [expandDecimals(6250, 4), expandDecimals(1, 6)],
+        maxPrices: [expandDecimals(6250, 4), expandDecimals(1, 6)],
+      },
+    });
 
     let [price, value, supply] = await getGlvTokenPrice();
 
@@ -57,19 +70,22 @@ describe("Glv Token Price", () => {
     expect(value).to.be.eq(0);
     expect(supply).to.be.eq(0);
 
+    // at eth price = $5k pool is $40k WETH / $50k USDC
+    // deposit $55k WETH / $45 USDC to balance the pool to simplify calculations
+    // new eth:usd pool size is $95k WETH / $95k USDC
     await handleGlvDeposit(fixture, {
       create: {
-        longTokenAmount: expandDecimals(10, 18),
-        shortTokenAmount: expandDecimals(10 * 5000, 6),
+        longTokenAmount: expandDecimals(11, 18),
+        shortTokenAmount: expandDecimals(9 * 5000, 6),
       },
     });
 
     [price, value, supply] = await getGlvTokenPrice();
 
     // deposited $100k
-    expect(price).to.be.eq(expandDecimals(1, 30));
-    expect(value).to.be.eq(expandDecimals(100_000, 30));
-    expect(supply).to.be.eq(expandDecimals(100_000, 18));
+    expect(price).to.be.closeTo(expandDecimals(1, 30), expandDecimals(1, 15));
+    expect(value).to.be.closeTo(expandDecimals(100_000, 30), expandDecimals(1, 15));
+    expect(supply).to.be.closeTo(expandDecimals(100_000, 18), 100);
 
     await handleGlvDeposit(fixture, {
       create: {
@@ -79,32 +95,76 @@ describe("Glv Token Price", () => {
       },
     });
 
-    // deposited $10k
+    // deposited $10k, total is $110k
     [price, value, supply] = await getGlvTokenPrice();
-    expect(price).to.be.eq(expandDecimals(1, 30));
-    expect(value).to.be.eq(expandDecimals(110_000, 30));
-    expect(supply).to.be.eq(expandDecimals(110_000, 18));
+    expect(price).to.be.closeTo(expandDecimals(1, 30), expandDecimals(1, 24));
+    expect(value).to.be.closeTo(expandDecimals(110_000, 30), expandDecimals(1, 24));
+    expect(supply).to.be.closeTo(expandDecimals(110_000, 18), 100);
 
     await handleGlvShift(fixture, {
       create: {
         fromMarket: ethUsdMarket,
         toMarket: solUsdMarket,
-        marketTokenAmount: expandDecimals(10000, 18),
-        minMarketTokens: expandDecimals(10000, 18),
+        marketTokenAmount: expandDecimals(10_000, 18),
       },
     });
 
     // price/value/supply should not change
     [price, value, supply] = await getGlvTokenPrice();
-    expect(price).to.be.eq(expandDecimals(1, 30));
-    expect(value).to.be.eq(expandDecimals(110_000, 30));
-    expect(supply).to.be.eq(expandDecimals(110_000, 18));
+    expect(price).to.be.closeTo(expandDecimals(1, 30), expandDecimals(1, 24));
+    expect(value).to.be.closeTo(expandDecimals(110_000, 30), expandDecimals(1, 24));
+    expect(supply).to.be.closeTo(expandDecimals(110_000, 18), 100);
 
-    // eth price grew by 10%
-    [price, value, supply] = await getGlvTokenPrice(5500);
-    expect(price).to.be.eq(expandDecimals(105, 28));
-    expect(value).to.be.eq(expandDecimals(115_500, 30));
-    expect(supply).to.be.eq(expandDecimals(110_000, 18));
+    // const wethBalanceForEthUsdMarket = await getBalanceOf(wnt.address, ethUsdMarket.marketToken);
+    // const usdcBalanceForEthUsdMarket = await getBalanceOf(usdc.address, ethUsdMarket.marketToken);
+    // const wethBalanceForSolUsdMarket = await getBalanceOf(wnt.address, solUsdMarket.marketToken);
+    // const usdcBalanceForSolUsdMarket = await getBalanceOf(usdc.address, solUsdMarket.marketToken);
+
+    // console.log("weth total balance: %s", wethBalanceForEthUsdMarket.add(wethBalanceForSolUsdMarket));
+    // console.log("usdc total balance: %s", usdcBalanceForEthUsdMarket.add(usdcBalanceForSolUsdMarket));
+
+    // const ethUsdMarketBalance = await getBalanceOf(ethUsdMarket.marketToken, ethUsdGlvAddress);
+    // const solUsdMarketBalance = await getBalanceOf(solUsdMarket.marketToken, ethUsdGlvAddress);
+
+    // const [ethUsdMarketTokenPrice] = await reader.getMarketTokenPrice(
+    //   dataStore.address,
+    //   ethUsdMarket,
+    //   getPriceProp(6000, 12),
+    //   getPriceProp(6000, 12),
+    //   getPriceProp(1, 24),
+    //   ethers.constants.HashZero,
+    //   true
+    // );
+    // const [solUsdMarketTokenPrice] = await reader.getMarketTokenPrice(
+    //   dataStore.address,
+    //   solUsdMarket,
+    //   getPriceProp(600, 12),
+    //   getPriceProp(6000, 12),
+    //   getPriceProp(1, 24),
+    //   ethers.constants.HashZero,
+    //   true
+    // );
+    // console.log(2);
+
+    // console.log(
+    //   "eth market. balance: %s price: %s value: %s",
+    //   ethUsdMarketBalance,
+    //   ethUsdMarketTokenPrice,
+    //   ethUsdMarketTokenPrice.mul(ethUsdMarketBalance).div(expandDecimals(1, 18))
+    // );
+    // console.log(
+    //   "sol market. balance: %s price: %s value: %s",
+    //   solUsdMarketBalance,
+    //   solUsdMarketTokenPrice,
+    //   solUsdMarketTokenPrice.mul(solUsdMarketBalance).div(expandDecimals(1, 18))
+    // );
+
+    // eth price grew by 20%, GLV value grew by 10%
+    [price, value, supply] = await getGlvTokenPrice(6000);
+
+    expect(price).to.be.closeTo(expandDecimals(11, 29), expandDecimals(1, 24));
+    expect(value).to.be.closeTo(expandDecimals(121_000, 30), expandDecimals(1, 24));
+    expect(supply).to.be.closeTo(expandDecimals(110_000, 18), 100);
 
     await handleGlvWithdrawal(fixture, {
       create: {
@@ -114,8 +174,8 @@ describe("Glv Token Price", () => {
     });
 
     [price, value, supply] = await getGlvTokenPrice();
-    expect(price).to.be.eq(expandDecimals(1, 30));
-    expect(value).to.be.eq(expandDecimals(109_000, 30));
-    expect(supply).to.be.eq(expandDecimals(109_000, 18));
+    expect(price).to.be.closeTo(expandDecimals(1, 30), expandDecimals(1, 24));
+    expect(value).to.be.closeTo(expandDecimals(109_000, 30), expandDecimals(1, 24));
+    expect(supply).to.be.closeTo(expandDecimals(109_000, 18), 100);
   });
 });
