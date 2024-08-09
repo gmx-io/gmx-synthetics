98a9abbf603e36a61b1a98e415d7b2917af81a2e
S1_Dev <122179833+SentinelDev1@users.noreply.github.com>
2024-08-09 14:55:28 -0400

    Update IRiskOracle.sol
    Allows the ConfigSyncer to interact with the Chaos Labs Risk Oracle contract

--- contracts/config/IRiskOracle.sol
+++ contracts/config/IRiskOracle.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: BUSL-1.1
 
+pragma solidity ^0.8.0;
+
+// Using interface for the Chaos Labs Risk Oracle contract so that importing the contract is not necessary
+interface IRiskOracle {
+    struct RiskParameterUpdate {
+        uint256 timestamp;
+        bytes newValue;
+        string referenceId;
+        bytes previousValue;
+        string updateType;
+        uint256 updateId;
+        bytes market;
+        bytes additionalData;
+    }
+    function getLatestUpdateByParameterAndMarket(string memory updateType, bytes memory market) external view returns (RiskParameterUpdate memory);
+}
