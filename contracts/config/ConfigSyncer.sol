817ab421106c8c4fcabde8e0cfa88659c7b2445e
S1_Dev <122179833+SentinelDev1@users.noreply.github.com>
2024-08-09 15:56:39 -0400

    Update ConfigSyncer.sol
    A number of changes including:
    -Removed isParameterDisabled mapping and corresponding logic
    -Moved the bytesToUint256() function to the Cast library
    -Removed modifiers and thus added LIMITED_CONFIG_KEEPER modifier to RoleModule
    -RoleModule is now inherited
    -Removed require statements and added custom errors in the Errors contract and updated to use if statements plus custom errors
    -Moved IRiskOracle to a separate file in the config folder
    -Various minor optimizations including: N
    -Now using keccak256(abi.encode()) for setIsParameterTypeDisabled and setIsMarketParameterDisabled mappings
    -Caching market and parameterType in the sync function to local variables
    -Using calldata instead of memory in function inputs
    -Updated input variable naming and comment style to better reflect codebase norms

--- contracts/config/ConfigSyncer.sol
+++ contracts/config/ConfigSyncer.sol
@@ -5,11 +5,13 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 import "./Config.sol";
+import "./IRiskOracle.sol";
 import "../reader/Reader.sol";
 import "../market/Market.sol";
 import "../role/RoleModule.sol";
+import "../utils/Cast.sol";
 
-contract ConfigSyncer is ReentrancyGuard {
+contract ConfigSyncer is ReentrancyGuard, RoleModule {
     // Uint256 that is equal to the latest update published by the Chaos Labs Risk Oracle contract that has already been applied by this contract
     uint256 public latestUpdate;
     
@@ -21,9 +23,6 @@ contract ConfigSyncer is ReentrancyGuard {
     Reader public immutable reader;
     Config public immutable config;
     RoleStore public immutable roleStore;
-    
-    // Bool that if true disables the ability to apply any updates
-    bool public isDisabled;
 
     // Mapping from updateId to bool that returns true if an update has already been applied and false if it hasn't yet been applied
     mapping(uint256 => bool) public isCompleted;
@@ -32,10 +31,10 @@ contract ConfigSyncer is ReentrancyGuard {
     mapping(address => bool) public isMarketDisabled;
 
     // Mapping that if true disables the ability to apply updates of a given parameter type for all markets
-    mapping(string => bool) public isParameterDisabled;
+    mapping(bytes32 => bool) public isParameterTypeDisabled;
 
     // Mapping that if true disables the ability to apply updates of a given parameter type for a given market (bytes = market and parameter type abiEncodePacked)
-    mapping(bytes => bool) public isMarketParameterDisabled;
+    mapping(bytes32 => bool) public isMarketParameterDisabled;
 
     constructor(address _riskOracle, address _dataStore, Reader _reader, Config _config, RoleStore _roleStore) {
         // Initialize riskOracle and dataStore addresses
@@ -48,95 +47,83 @@ contract ConfigSyncer is ReentrancyGuard {
         roleStore = _roleStore;
     }
 
-    modifier onlyConfigKeeper() {
-        if (
-            !roleStore.hasRole(msg.sender, Role.CONFIG_KEEPER)
-        ) {
-            revert Errors.Unauthorized(msg.sender, "CONFIG_KEEPER");
+    
+     // @dev Enables or disables the ability to apply updates of any parameter type for a specific market.
+     // @param market The address of the market.
+     // @param disabled A boolean indicating whether the market is disabled.
+    function setIsMarketDisabled(address market, bool disabled) external onlyConfigKeeper nonReentrant {
+        isMarketDisabled[market] = disabled;
+    }
+
+    
+     // @dev Enables or disables the ability to apply updates for a specific parameter type across all markets.
+     // @param parameterType The type of the parameter to be enabled or disabled.
+     // @param disabled A boolean indicating whether the parameter type is disabled.
+    function setIsParameterTypeDisabled(string calldata parameterType, bool disabled) external onlyConfigKeeper nonReentrant {
+        isParameterTypeDisabled[keccak256(abi.encode(parameterType))] = disabled;
+    }
+
+     // @dev Enables or disables the ability to apply updates for a specific market and parameter type combination.
+     // @param market The address of the market.
+     // @param parameterType The type of the parameter to be enabled or disabled for the market.
+     // @param disabled A boolean indicating whether the market-parameter combination is disabled.
+    function setIsMarketParameterDisabled(address market, string calldata parameterType, bool disabled) external onlyConfigKeeper nonReentrant {
+        isMarketParameterDisabled[keccak256(abi.encode(market, parameterType))] = disabled;
+    }
+
+    
+     // @dev Allows the LIMITED_CONFIG_KEEPER to apply updates with the provided markets and parameter types.
+     // @param markets An array of market addresses for which updates are to be applied.
+     // @param parameterTypes An array of parameter types corresponding to each market for which updates are to be applied.
+     // Requirements:
+     // - The length of the markets and parameterTypes arrays must be the same.
+     // - The specified market must not be disabled.
+     // - The specified parameter type must not be disabled.
+     // - The combination of market and parameter type must not be disabled.
+     // - The update must not have already been applied.
+    function sync(address[] calldata markets, string[] calldata parameterTypes) external onlyLimitedConfigKeeper nonReentrant {
+        if (markets.length != parameterTypes.length) {
+            revert Errors.MarketsAndParameterTypesDifferentLengths(markets.length, parameterTypes.length);
         }
 
-        _;
-    }
-
-    modifier onlyLimitedConfigKeeper() {
-        if (
-            !roleStore.hasRole(msg.sender, Role.LIMITED_CONFIG_KEEPER)
-        ) {
-            revert Errors.Unauthorized(msg.sender, "LIMITED_CONFIG_KEEPER");
-        }
-
-        _;
-    }
-
-    // Allows CONFIG_KEEPER to enable or disable the ability to apply updates
-    function setIsDisabled(bool _disabled) external onlyConfigKeeper nonReentrant {
-        isDisabled = _disabled;
-    }
-
-    // Allows CONFIG_KEEPER to enable or disable the ability to apply updates of any parameter type for a specific market
-    function setIsMarketDisabled(address _market, bool _disabled) external onlyConfigKeeper nonReentrant {
-        isMarketDisabled[_market] = _disabled;
-    }
-
-    // Allows CONFIG_KEEPER to enable or disable the ability to apply updates for a specific parameter type for all markets
-    function setIsParameterDisabled(string memory _parameterType, bool _disabled) external onlyConfigKeeper nonReentrant {
-        isParameterDisabled[_parameterType] = _disabled;
-    }
-
-    // Allows CONFIG_KEEPER to enable or disable the ability to apply updates for a specific market and parameter type combination
-    function setIsMarketParameterDisabled(address _market, string memory _parameterType, bool _disabled) external onlyConfigKeeper nonReentrant {
-        isMarketParameterDisabled[abi.encodePacked(_market, _parameterType)] = _disabled;
-    }
-
-    // Allows the LIMITED_CONFIG_KEEPER to apply updates with the provided _markets and _parameterTypes
-    function sync(address[] memory _markets, string[] memory _parameterTypes) external onlyLimitedConfigKeeper nonReentrant {
-        require(!isDisabled);
-        require(_markets.length == _parameterTypes.length);
         uint256 cachedLatestUpdate = latestUpdate;
-        for (uint256 i = 0; i < _markets.length; i++) {
-            require(!isMarketDisabled[_markets[i]]);
-            require(!isParameterDisabled[_parameterTypes[i]]);
-            require(!isMarketParameterDisabled[abi.encodePacked(_markets[i], _parameterTypes[i])]);
-            IRiskOracle.RiskParameterUpdate memory riskParameterUpdate = IRiskOracle(riskOracle).getLatestUpdateByParameterAndMarket(_parameterTypes[i], abi.encode(_markets[i]));
+        address market;
+        string memory parameterType;
+
+        for (uint256 i = 0; i < markets.length; i++) {
+            market = markets[i];
+            parameterType = parameterTypes[i];
+            if (isMarketDisabled[market]) {
+                revert Errors.UpdatesDisabledForMarket(market);
+            }
+
+            if (isParameterTypeDisabled[keccak256(abi.encode(parameterType))]) {
+                revert Errors.UpdatesDisabledForParameterType(parameterType);
+            }
+
+            if (!isMarketParameterDisabled[keccak256(abi.encode(market, parameterType))]) {
+                revert Errors.UpdatesDisabledForMarketParameter(market, parameterType);
+            }
+
+            IRiskOracle.RiskParameterUpdate memory riskParameterUpdate = IRiskOracle(riskOracle).getLatestUpdateByParameterAndMarket(parameterTypes[i], abi.encode(markets[i]));
             uint256 updateId = riskParameterUpdate.updateId;
-            require(!isCompleted[updateId]);
+            
+            if (isCompleted[updateId]) {
+                revert Errors.UpdateAlreadyApplied(updateId);
+            }
+
+            uint256 updatedValue = Cast.bytesToUint256(riskParameterUpdate.newValue);
             (bytes32 baseKey, bytes memory data) = abi.decode(riskParameterUpdate.additionalData, (bytes32, bytes));
-            uint256 updatedValue = bytesToUint256(riskParameterUpdate.newValue);
             config.setUint(baseKey, data, updatedValue);
             isCompleted[updateId] = true;
+            
             if (updateId > cachedLatestUpdate) {
                 cachedLatestUpdate = riskParameterUpdate.updateId;
             }
         }
+        
         if (cachedLatestUpdate > latestUpdate) {
             latestUpdate = cachedLatestUpdate;
         }
     }
-
-    // Handles cases in which the uint256 stored in bytes is stored without padding
-    function bytesToUint256(bytes memory _bytes) internal pure returns (uint256 value) {
-        require(_bytes.length <= 32, "Data length exceeds 32 bytes");
-        if (_bytes.length == 0) {
-            return 0;
-        }
-        assembly {
-            value := mload(add(_bytes, 32))
-        }
-        value = value >> (8 * (32 - _bytes.length));
-    }
-}
-
-// Using interface for the Chaos Labs Risk Oracle contract so that importing the contract is not necessary
-interface IRiskOracle {
-    struct RiskParameterUpdate {
-        uint256 timestamp;
-        bytes newValue;
-        string referenceId;
-        bytes previousValue;
-        string updateType;
-        uint256 updateId;
-        bytes market;
-        bytes additionalData;
-    }
-    function getLatestUpdateByParameterAndMarket(string memory updateType, bytes memory market) external view returns (RiskParameterUpdate memory);
 }
