59b3667e02f3a9fd20ddf0bb29df9f86be30081a
S1_Dev <122179833+SentinelDev1@users.noreply.github.com>
2024-08-09 15:45:34 -0400

    Update Cast.sol
    Added function used in ConfigSync to convert uint256 stored as bytes to uint256 and handle cases where the bytes is not padded to 32 bytes (as is needed to use abi.decode)

--- contracts/utils/Cast.sol
+++ contracts/utils/Cast.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.0;
 
+import "../error/Errors.sol";
+
 /**
  * @title Cast
  * @dev Library for casting functions
@@ -9,5 +11,29 @@ pragma solidity ^0.8.0;
 library Cast {
     function toBytes32(address value) internal pure returns (bytes32) {
         return bytes32(uint256(uint160(value)));
+    }
+    
+    /**
+     * @dev Converts a bytes array to a uint256.
+     * Handles cases where the uint256 stored in bytes is stored with or without padding.
+     * @param uint256AsBytes The bytes array representing the uint256 value.
+     * @return value The uint256 value obtained from the bytes array.
+     */
+    function bytesToUint256(bytes memory uint256AsBytes) internal pure returns (uint256 value) {
+        uint256 length = uint256AsBytes.length;
+        
+        if(length > 32) {
+            revert Errors.Uint256AsBytesLengthExceeds32Bytes(length);
+        }
+
+        if (length == 0) {
+            return 0;
+        }
+
+        assembly {
+            value := mload(add(uint256AsBytes, 32))
+        }
+
+        value = value >> (8 * (32 - length));
     }
 }
