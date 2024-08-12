596ccee491c9b66e2c3d1afe21956d992dccce56
S1_Dev <122179833+SentinelDev1@users.noreply.github.com>
2024-08-12 16:56:24 -0400

    ConfigSyncer now using dataStore for storage
    -Removed all mappings and the uint256 latestUpdate variable and now storing these data points in dataStore using keys. 
    
    -Removed setter functions since this data will now be set via the Config contract. 
    
    -Other minor optimizations including caching local variables where possible.

--- contracts/config/ConfigSyncer.sol
+++ contracts/config/ConfigSyncer.sol
@@ -8,52 +8,17 @@ import "./Config.sol";
 import "./IRiskOracle.sol";
 
 contract ConfigSyncer is ReentrancyGuard, RoleModule {
-    // Contract instance for the Config contract
+    // Contract instances for the Config and DataStore contracts
     Config public immutable config;
+    DataStore public immutable dataStore;
 
     // Address for the Chaos Labs RiskOracle contract
     address public immutable riskOracle;
-    
-    // Uint256 that is equal to the latest update published by the Chaos Labs Risk Oracle contract that has already been applied by this contract
-    uint256 public latestUpdate;
 
-    // Mapping from updateId to bool that returns true if an update has already been applied and false if it hasn't yet been applied
-    mapping(uint256 => bool) public isCompleted;
-
-    // Mapping that if true disables the ability to apply all updates of any parameter type for a given market
-    mapping(address => bool) public isMarketDisabled;
-
-    // Mapping that if true disables the ability to apply updates of a given parameter type for all markets
-    mapping(bytes32 => bool) public isParameterTypeDisabled;
-
-    // Mapping that if true disables the ability to apply updates of a given parameter type for a given market (bytes32 = keccak256(abiPacked(market, parameter type))
-    mapping(bytes32 => bool) public isMarketParameterDisabled;
-
-    constructor(RoleStore _roleStore, Config _config, address _riskOracle) RoleModule(_roleStore) {
+    constructor(RoleStore _roleStore, Config _config, DataStore _dataStore, address _riskOracle) RoleModule(_roleStore) {
         config = _config;
+        dataStore = _dataStore;
         riskOracle = _riskOracle;
-    }
-
-     // @dev Enables or disables the ability to apply updates of any parameter type for a specific market.
-     // @param market The address of the market.
-     // @param disabled A boolean indicating whether the market is disabled.
-    function setIsMarketDisabled(address market, bool disabled) external onlyConfigKeeper nonReentrant {
-        isMarketDisabled[market] = disabled;
-    }
-
-     // @dev Enables or disables the ability to apply updates for a specific parameter type across all markets.
-     // @param parameterType The type of the parameter to be enabled or disabled.
-     // @param disabled A boolean indicating whether the parameter type is disabled.
-    function setIsParameterTypeDisabled(string calldata parameterType, bool disabled) external onlyConfigKeeper nonReentrant {
-        isParameterTypeDisabled[keccak256(abi.encode(parameterType))] = disabled;
-    }
-
-     // @dev Enables or disables the ability to apply updates for a specific market and parameter type combination.
-     // @param market The address of the market.
-     // @param parameterType The type of the parameter to be enabled or disabled for the market.
-     // @param disabled A boolean indicating whether the market-parameter combination is disabled.
-    function setIsMarketParameterDisabled(address market, string calldata parameterType, bool disabled) external onlyConfigKeeper nonReentrant {
-        isMarketParameterDisabled[keccak256(abi.encode(market, parameterType))] = disabled;
     }
 
      // @dev Allows the LIMITED_CONFIG_KEEPER to apply updates with the provided markets and parameter types.
@@ -61,16 +26,25 @@ contract ConfigSyncer is ReentrancyGuard, RoleModule {
      // @param parameterTypes An array of parameter types corresponding to each market for which updates are to be applied.
      // Requirements:
      // - The length of the markets and parameterTypes arrays must be the same.
-     // - The specified market must not be disabled.
-     // - The specified parameter type must not be disabled.
-     // - The combination of market and parameter type must not be disabled.
+     // - The SyncConfig functionality must not be disabled.
+     // - Updates for the specified market must not be disabled.
+     // - Updates for the specified parameter type must not be disabled.
+     // - Updates for the combination of market and parameter type must not be disabled.
      // - The update must not have already been applied.
-    function sync(address[] calldata markets, string[] calldata parameterTypes) external onlyLimitedConfigKeeper nonReentrant {
+    function syncConfig(address[] calldata markets, string[] calldata parameterTypes) external onlyLimitedConfigKeeper nonReentrant {
+        bytes32 cachedSyncConfigFeatureDisabledKey = Keys.SYNC_CONFIG_FEATURE_DISABLED;
+        
+        if (dataStore.getBool(cachedSyncConfigFeatureDisabledKey)) {
+                revert Errors.SyncConfigUpdatesDisabled();
+            }
+        
         if (markets.length != parameterTypes.length) {
-            revert Errors.MarketsAndParameterTypesDifferentLengths(markets.length, parameterTypes.length);
+            revert Errors.SyncConfigInvalidInputLengths(markets.length, parameterTypes.length);
         }
 
-        uint256 cachedLatestUpdate = latestUpdate;
+        bytes32 cachedSyncConfigLatestUpdateIdKey = Keys.SYNC_CONFIG_LATEST_UPDATE_ID;
+        bytes32 cachedSyncConfigUpdateCompletedKey = Keys.SYNC_CONFIG_UPDATE_COMPLETED;
+        uint256 cachedLatestUpdate = dataStore.getUint(cachedSyncConfigLatestUpdateIdKey);
         address market;
         string memory parameterType;
 
@@ -78,37 +52,37 @@ contract ConfigSyncer is ReentrancyGuard, RoleModule {
             market = markets[i];
             parameterType = parameterTypes[i];
             
-            if (isMarketDisabled[market]) {
-                revert Errors.UpdatesDisabledForMarket(market);
+            if (dataStore.getBool(keccak256(bytes.concat(cachedSyncConfigFeatureDisabledKey, abi.encode(market))))) {
+                revert Errors.SyncConfigUpdatesDisabledForMarket(market);
             }
 
-            if (isParameterTypeDisabled[keccak256(abi.encode(parameterType))]) {
-                revert Errors.UpdatesDisabledForParameterType(parameterType);
+            if (dataStore.getBool(keccak256(bytes.concat(cachedSyncConfigFeatureDisabledKey, abi.encode(parameterType))))) {
+                revert Errors.SyncConfigUpdatesDisabledForParameterType(parameterType);
             }
 
-            if (!isMarketParameterDisabled[keccak256(abi.encode(market, parameterType))]) {
-                revert Errors.UpdatesDisabledForMarketParameter(market, parameterType);
+            if (dataStore.getBool(keccak256(bytes.concat(cachedSyncConfigFeatureDisabledKey, abi.encode(market), abi.encode(parameterType))))) {
+                revert Errors.SyncConfigUpdatesDisabledForMarketParameter(market, parameterType);
             }
 
-            IRiskOracle.RiskParameterUpdate memory riskParameterUpdate = IRiskOracle(riskOracle).getLatestUpdateByParameterAndMarket(parameterTypes[i], abi.encode(markets[i]));
+            IRiskOracle.RiskParameterUpdate memory riskParameterUpdate = IRiskOracle(riskOracle).getLatestUpdateByParameterAndMarket(parameterType, abi.encode(market));
             uint256 updateId = riskParameterUpdate.updateId;
             
-            if (isCompleted[updateId]) {
-                revert Errors.UpdateAlreadyApplied(updateId);
+            if (dataStore.getBool(keccak256(bytes.concat(cachedSyncConfigUpdateCompletedKey, abi.encode(updateId))))) {
+                revert Errors.SyncConfigUpdateAlreadyApplied(updateId);
             }
 
             uint256 updatedValue = Cast.bytesToUint256(riskParameterUpdate.newValue);
             (bytes32 baseKey, bytes memory data) = abi.decode(riskParameterUpdate.additionalData, (bytes32, bytes));
             config.setUint(baseKey, data, updatedValue);
-            isCompleted[updateId] = true;
+            config.setBool(cachedSyncConfigUpdateCompletedKey, abi.encode(updateId), true);
             
             if (updateId > cachedLatestUpdate) {
-                cachedLatestUpdate = riskParameterUpdate.updateId;
+                cachedLatestUpdate = updateId;
             }
         }
         
-        if (cachedLatestUpdate > latestUpdate) {
-            latestUpdate = cachedLatestUpdate;
+        if (cachedLatestUpdate > dataStore.getUint(cachedSyncConfigLatestUpdateIdKey)) {
+            config.setUint(cachedSyncConfigLatestUpdateIdKey, new bytes(0), cachedLatestUpdate);
         }
     }
 }
