4dbcb7a5b0f5e81d9f7a82723969d092b38a3f63
S1_Dev <122179833+SentinelDev1@users.noreply.github.com>
2024-08-12 14:40:11 -0400

    Cleanup ConfigSyncer, variables, imports and constructor
    Removing unused imports, variables and updating the constructor accordingly.

--- contracts/config/ConfigSyncer.sol
+++ contracts/config/ConfigSyncer.sol
@@ -6,23 +6,16 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 import "./Config.sol";
 import "./IRiskOracle.sol";
-import "../reader/Reader.sol";
-import "../market/Market.sol";
-import "../role/RoleModule.sol";
-import "../utils/Cast.sol";
 
 contract ConfigSyncer is ReentrancyGuard, RoleModule {
+    // Contract instance for the Config contract
+    Config public immutable config;
+
+    // Address for the Chaos Labs RiskOracle contract
+    address public immutable riskOracle;
+    
     // Uint256 that is equal to the latest update published by the Chaos Labs Risk Oracle contract that has already been applied by this contract
     uint256 public latestUpdate;
-    
-    // Addresses for the RiskOracle and DataStore contracts
-    address public immutable riskOracle;
-    address public immutable dataStore;
-
-    // Contract instances for the Reader, Config and RoleStore contracts
-    Reader public immutable reader;
-    Config public immutable config;
-    RoleStore public immutable roleStore;
 
     // Mapping from updateId to bool that returns true if an update has already been applied and false if it hasn't yet been applied
     mapping(uint256 => bool) public isCompleted;
@@ -36,18 +29,11 @@ contract ConfigSyncer is ReentrancyGuard, RoleModule {
     // Mapping that if true disables the ability to apply updates of a given parameter type for a given market (bytes32 = keccak256(abiPacked(market, parameter type))
     mapping(bytes32 => bool) public isMarketParameterDisabled;
 
-    constructor(address _riskOracle, address _dataStore, Reader _reader, Config _config, RoleStore _roleStore) {
-        // Initialize riskOracle and dataStore addresses
-        riskOracle = _riskOracle;
-        dataStore = _dataStore;
-        
-        // Initialize with contract addresses
-        reader = _reader;
+    constructor(RoleStore _roleStore, Config _config, address _riskOracle) RoleModule(_roleStore) {
         config = _config;
-        roleStore = _roleStore;
+        riskOracle = _riskOracle;
     }
 
-    
      // @dev Enables or disables the ability to apply updates of any parameter type for a specific market.
      // @param market The address of the market.
      // @param disabled A boolean indicating whether the market is disabled.
@@ -55,7 +41,6 @@ contract ConfigSyncer is ReentrancyGuard, RoleModule {
         isMarketDisabled[market] = disabled;
     }
 
-    
      // @dev Enables or disables the ability to apply updates for a specific parameter type across all markets.
      // @param parameterType The type of the parameter to be enabled or disabled.
      // @param disabled A boolean indicating whether the parameter type is disabled.
