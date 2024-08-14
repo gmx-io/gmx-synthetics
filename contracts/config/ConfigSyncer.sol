57a94e2b13dc8ae9103b1f3fd8c77e8ee80f2196
S1_Dev <122179833+SentinelDev1@users.noreply.github.com>
2024-08-14 18:14:50 -0400

    Update ConfigSyncer.sol
    1. Changed all instances of parameterType(s) to parameter(s)
    
    2. Added an event that is emitted when an update is completed (or if is already completed but not disabled) with the following params: uint256 updateId, uint256 prevValue, uint256 nextValue and bool updateApplied
    
    3. Changed so that if an update has already been completed there is no error, it just skips updating for that market and parameter combination and this is reflected in the emitted event updateApplied bool (false in this case)
    
    4. Retrieve the prevValue for the purpose of emitting the event

--- contracts/config/ConfigSyncer.sol
+++ contracts/config/ConfigSyncer.sol
@@ -11,71 +11,94 @@ import "../feature/FeatureUtils.sol";
 // @title ConfigSyncer
 // @dev Contract to handle market parameter updates
 contract ConfigSyncer is ReentrancyGuard, RoleModule {
+    using EventUtils for EventUtils.UintItems;
+    using EventUtils for EventUtils.BoolItems;
+
     Config public immutable config;
     DataStore public immutable dataStore;
+    EventEmitter public immutable eventEmitter;
     address public immutable riskOracle;
 
     constructor(
         RoleStore _roleStore, 
         Config _config, 
         DataStore _dataStore, 
+        EventEmitter _eventEmitter,
         address _riskOracle
     ) RoleModule(_roleStore) {
         config = _config;
         dataStore = _dataStore;
+        eventEmitter = _eventEmitter;
         riskOracle = _riskOracle;
     }
 
-    // @dev Allows the LIMITED_CONFIG_KEEPER to apply updates with the provided markets and parameter types.
+    // @dev Allows the LIMITED_CONFIG_KEEPER to apply updates with the provided markets and parameters.
     // @param markets An array of market addresses for which updates are to be applied.
-    // @param parameterTypes An array of parameter types corresponding to each market for which updates are to be applied.
+    // @param parameters An array of parameters corresponding to each market for which updates are to be applied.
     function sync(
         address[] calldata markets, 
-        string[] calldata parameterTypes
+        string[] calldata parameters
     ) external onlyLimitedConfigKeeper nonReentrant {
         FeatureUtils.validateFeature(dataStore, Keys.syncConfigFeatureDisabledKey(address(this)));
         
-        if (markets.length != parameterTypes.length) {
-            revert Errors.SyncConfigInvalidInputLengths(markets.length, parameterTypes.length);
+        if (markets.length != parameters.length) {
+            revert Errors.SyncConfigInvalidInputLengths(markets.length, parameters.length);
         }
 
         uint256 latestUpdateId = dataStore.getUint(Keys.syncConfigLatestUpdateIdKey());
 
         for (uint256 i; i < markets.length; i++) {
             address market = markets[i];
-            string memory parameterType = parameterTypes[i];
-            
+            string memory parameter = parameters[i];
+            bool updateApplied;
+
             bool syncConfigMarketDisabled = dataStore.getBool(Keys.syncConfigMarketDisabledKey(market));
             if (syncConfigMarketDisabled) {
                 revert Errors.SyncConfigUpdatesDisabledForMarket(market);
             }
             
-            bool syncConfigParameterTypeDisabled = dataStore.getBool(Keys.syncConfigParameterTypeDisabledKey(parameterType));
-            if (syncConfigParameterTypeDisabled) {
-                revert Errors.SyncConfigUpdatesDisabledForParameterType(parameterType);
+            bool syncConfigparameterDisabled = dataStore.getBool(Keys.syncConfigParameterDisabledKey(parameter));
+            if (syncConfigparameterDisabled) {
+                revert Errors.SyncConfigUpdatesDisabledForParameter(parameter);
             }
 
-            bool syncConfigMarketParameterDisabled = dataStore.getBool(Keys.syncConfigMarketParameterDisabledKey(market, parameterType));
+            bool syncConfigMarketParameterDisabled = dataStore.getBool(Keys.syncConfigMarketParameterDisabledKey(market, parameter));
             if (syncConfigMarketParameterDisabled) {
-                revert Errors.SyncConfigUpdatesDisabledForMarketParameter(market, parameterType);
+                revert Errors.SyncConfigUpdatesDisabledForMarketParameter(market, parameter);
             }
 
-            IRiskOracle.RiskParameterUpdate memory riskParameterUpdate = IRiskOracle(riskOracle).getLatestUpdateByParameterAndMarket(parameterType, abi.encode(market));
+            IRiskOracle.RiskParameterUpdate memory riskParameterUpdate = IRiskOracle(riskOracle).getLatestUpdateByParameterAndMarket(parameter, abi.encode(market));
             uint256 updateId = riskParameterUpdate.updateId;
+            (bytes32 baseKey, bytes memory data) = abi.decode(riskParameterUpdate.additionalData, (bytes32, bytes));
+            bytes32 fullKey = data.length == 0 ? baseKey : keccak256(bytes.concat(baseKey, data));
+            uint256 prevValue = dataStore.getUint(fullKey);
+            uint256 updatedValue = Cast.bytesToUint256(riskParameterUpdate.newValue);
             
             bool syncConfigUpdatedCompleted = dataStore.getBool(Keys.syncConfigUpdateCompletedKey(updateId));
-            if (syncConfigUpdatedCompleted) {
-                revert Errors.SyncConfigUpdateAlreadyApplied(updateId);
+            if (!syncConfigUpdatedCompleted) {
+                config.setUint(baseKey, data, updatedValue);
+                dataStore.setBool(Keys.syncConfigUpdateCompletedKey(updateId), true);
+                updateApplied = true;
+
+                if (updateId > latestUpdateId) {
+                    latestUpdateId = updateId;
+                }
             }
 
-            uint256 updatedValue = Cast.bytesToUint256(riskParameterUpdate.newValue);
-            (bytes32 baseKey, bytes memory data) = abi.decode(riskParameterUpdate.additionalData, (bytes32, bytes));
-            config.setUint(baseKey, data, updatedValue);
-            dataStore.setBool(Keys.syncConfigUpdateCompletedKey(updateId), true);
+            EventUtils.EventLogData memory eventData;
+
+            eventData.uintItems.initItems(3);
+            eventData.uintItems.setItem(0, "updateId", updateId);
+            eventData.uintItems.setItem(1, "prevValue", prevValue);
+            eventData.uintItems.setItem(2, "nextValue", updatedValue);
+
+            eventData.boolItems.initItems(1);
+            eventData.boolItems.setItem(0, "updateApplied", updateApplied);
             
-            if (updateId > latestUpdateId) {
-                latestUpdateId = updateId;
-            }
+            eventEmitter.emitEventLog(
+                "SyncConfig",
+                eventData
+            );
         }
         
         if (latestUpdateId > dataStore.getUint(Keys.syncConfigLatestUpdateIdKey())) {
