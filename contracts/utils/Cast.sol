fb341bc70844690bbdab414eb2ac5c90bba8f0b5
S1_Dev <122179833+SentinelDev1@users.noreply.github.com>
2024-08-15 10:35:43 -0400

    Update Cast.sol
    Added comments back related to the bytesToUint256 function

--- contracts/utils/Cast.sol
+++ contracts/utils/Cast.sol
@@ -12,7 +12,13 @@ library Cast {
     function toBytes32(address value) internal pure returns (bytes32) {
         return bytes32(uint256(uint160(value)));
     }
-    
+
+    /**
+     * @dev Converts a bytes array to a uint256.
+     * Handles cases where the uint256 stored in bytes is stored with or without padding.
+     * @param uint256AsBytes The bytes array representing the uint256 value.
+     * @return value The uint256 value obtained from the bytes array.
+     */
     function bytesToUint256(bytes memory uint256AsBytes) internal pure returns (uint256) {
         uint256 length = uint256AsBytes.length;
         
