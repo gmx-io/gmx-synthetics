6a7158ae853c827e55623496be734b45679780d6
S1_Dev <122179833+SentinelDev1@users.noreply.github.com>
2024-08-19 14:20:03 -0400

    Update deployConfigSyncer.ts
    Updated to use MockRiskOracle when network is hardhat and the deployed RiskOracle address in other scenarios. Logic will now be in a config/riskOracle.ts file instead of config/overwrite.ts

--- deploy/deployConfigSyncer.ts
+++ deploy/deployConfigSyncer.ts
@@ -1,18 +1,30 @@
 import { grantRoleIfNotGranted } from "../utils/role";
 import { createDeployFunction } from "../utils/deploy";
 
-const constructorContracts = ["RoleStore", "Config", "DataStore", "EventEmitter", "RiskOracle"];
+const constructorContracts = ["RoleStore", "Config", "DataStore", "EventEmitter"];
 
 const func = createDeployFunction({
   contractName: "ConfigSyncer",
   dependencyNames: constructorContracts,
-  getDeployArgs: async ({ dependencyContracts }) => {
-    return constructorContracts.map((dependencyName) => dependencyContracts[dependencyName].address);
+  getDeployArgs: async ({ dependencyContracts, gmx, network, get }) => {
+    let riskOracleAddress = await gmx.getRiskOracle();
+    if (network.name === "hardhat") {
+      const riskOracle = await get("MockRiskOracle");
+      riskOracleAddress = riskOracle.address;
+    }
+    if (!riskOracleAddress) {
+      throw new Error("riskOracleAddress is not defined");
+    }
+    return constructorContracts
+      .map((dependencyName) => dependencyContracts[dependencyName].address)
+      .concat(riskOracleAddress);
   },
   afterDeploy: async ({ deployedContract }) => {
     await grantRoleIfNotGranted(deployedContract.address, "CONTROLLER");
     await grantRoleIfNotGranted(deployedContract.address, "CONFIG_KEEPER");
   },
 });
+
+func.dependencies = func.dependencies.concat(["MockRiskOracle"]);
 
 export default func;
