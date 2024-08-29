8191e911b9b57c23a5a5610adf0cf3bb09842dd2
S1_Dev <122179833+SentinelDev1@users.noreply.github.com>
2024-08-29 16:39:08 -0400

    Update deployMockRiskOracle.ts
    Updated to pass 'initialSenders' and 'initialUpdateTypes' as constructor param inputs to the updated MockRiskOracle contract on deployment.
    
    The 'initialSenders' array is set to include just the deploying 'wallet' address used for tests in the ConfigSyncer.ts test script.
    
    The 'initialUpdateTypes' array is set to include all parameters with base keys that equal true in the ConfigSyncer contract allowedBaseKeys plus maxPnlFactorForTradersLongs which is used for tests in the ConfigSyncer.ts test script. This should fix the two remediation findings from the Guardian audit (L-01 and L-02).

--- deploy/deployMockRiskOracle.ts
+++ deploy/deployMockRiskOracle.ts
@@ -3,6 +3,12 @@ import { createDeployFunction } from "../utils/deploy";
 
 const func = createDeployFunction({
   contractName: "MockRiskOracle",
+  getDeployArgs: async () => {
+    const [wallet] = await ethers.getSigners();
+    const initialSenders = [wallet.address];
+    const initialUpdateTypes = ["maxLongTokenPoolAmount", "maxShortTokenPoolAmount", "maxLongTokenPoolUsdForDeposit", "maxShortTokenPoolUsdForDeposit", "maxOpenInterestForLongs", "maxOpenInterestForShorts", "positivePositionImpactFactor", "negativePositionImpactFactor", "positionImpactExponentFactor", "positiveSwapImpactFactor", "negativeSwapImpactFactor", "swapImpactExponentFactor", "fundingIncreaseFactorPerSecond", "fundingDecreaseFactorPerSecond", "minFundingFactorPerSecond", "maxFundingFactorPerSecond", "borrowingFactorForLongs", "borrowingFactorForShorts", "borrowingExponentFactorForLongs", "borrowingExponentFactorForShorts", "reserveFactorLongs", "reserveFactorShorts", "openInterestReserveFactorLongs", "openInterestReserveFactorShorts", "optimalUsageFactor", "baseBorrowingFactor", "aboveOptimalUsageBorrowingFactor", "maxPnlFactorForTradersLongs"];
+    return [initialSenders, initialUpdateTypes];
+  },
 });
 
 func.skip = async ({ network }: HardhatRuntimeEnvironment) => {
