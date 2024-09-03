eeadee7bb11ae8b073348e8c8a66e3063317e187
S1_Dev <122179833+SentinelDev1@users.noreply.github.com>
2024-09-02 21:31:13 -0400

    Update updateRiskOracleConfig.ts
    Refactored and updated to take the markets, parameters and market parameters being updated from the config/riskOracle.ts file

--- scripts/updateRiskOracleConfig.ts
+++ scripts/updateRiskOracleConfig.ts
@@ -1,51 +1,85 @@
 import hre from "hardhat";
-
 import { encodeData } from "../utils/hash";
+import { getFullKey } from "../utils/config";
 import * as keys from "../utils/keys";
 
 export async function main() {
+  const riskOracleConfig = await hre.gmx.getRiskOracle();
+
+  const dataStore = await hre.ethers.getContract("DataStore");
   const config = await hre.ethers.getContract("Config");
 
-  const syncConfigKeys = [
-    // {
-    //   baseKey: keys.SYNC_CONFIG_MARKET_DISABLED,
-    //   data: encodeData(["address"], ["0x352f684ab9e97a6321a13CF03A61316B681D9fD2"]),
-    // },
-    {
-      baseKey: keys.SYNC_CONFIG_PARAMETER_DISABLED,
-      data: encodeData(["string"], ["maxLongTokenPoolAmount"]),
-    },
-    // {
-    //   baseKey: keys.SYNC_CONFIG_MARKET_PARAMETER_DISABLED,
-    //   data: encodeData(["address", "string"], ["0x352f684ab9e97a6321a13CF03A61316B681D9fD2", "maxLongTokenPoolAmount"]),
-    // },
-  ];
+  if (!riskOracleConfig) {
+    throw new Error("No configuration found for the current network");
+  }
 
-  const isDisabled = process.env.IS_DISABLED;
-  if (isDisabled === undefined) {
-    throw new Error("IS_DISABLED not specified");
+  const syncConfigKeys = [];
+  
+  for (const marketAddress in riskOracleConfig.markets) {
+    const marketConfig = riskOracleConfig.markets[marketAddress];
+    syncConfigKeys.push({
+      baseKey: keys.SYNC_CONFIG_MARKET_DISABLED,
+      data: encodeData(["address"], [marketAddress]),
+      newValue: marketConfig.syncConfigMarketDisabled,
+      description: `market: ${marketAddress}, market disabled: ${marketConfig.syncConfigMarketDisabled}`,
+    });
+    for (const marketParameter in marketConfig.marketParameters) {
+      syncConfigKeys.push({
+        baseKey: keys.SYNC_CONFIG_MARKET_PARAMETER_DISABLED,
+        data: encodeData(["address", "string"], [marketAddress, marketParameter]),
+        newValue: marketConfig.marketParameters[marketParameter],
+        description: `market: ${marketAddress}, parameter: ${marketParameter}, parameter disabled for market: ${marketConfig.marketParameters[marketParameter]}`,
+      });
+    }
+  }
+
+  for (const parameter in riskOracleConfig.parameters) {
+    syncConfigKeys.push({
+      baseKey: keys.SYNC_CONFIG_PARAMETER_DISABLED,
+      data: encodeData(["string"], [parameter]),
+      newValue: riskOracleConfig.parameters[parameter],
+      description: `parameter: ${parameter}, parameter disabled: ${riskOracleConfig.parameters[parameter]}`,
+    });
   }
 
   const multicallWriteParams = [];
 
   for (const syncConfigKey of syncConfigKeys) {
-    multicallWriteParams.push(
-      config.interface.encodeFunctionData("setBool", [
-        syncConfigKey.baseKey,
-        syncConfigKey.data,
-        isDisabled === "true" ? true : false,
-      ])
-    );
+    const baseKey = syncConfigKey.baseKey;
+    const data = syncConfigKey.data;
+    const key = getFullKey(baseKey, data);
+
+    const newValue = syncConfigKey.newValue;
+    const oldValue = await dataStore.getBool(key);
+
+    if (newValue !== oldValue) {
+      multicallWriteParams.push(
+        config.interface.encodeFunctionData("setBool", [
+          baseKey,
+          data,
+          newValue,
+        ])
+      );
+      console.info(`updating: ${syncConfigKey.description}`);
+    }
   }
 
-  console.info(`updating ${multicallWriteParams.length} features`);
+  console.info(`updating for ${multicallWriteParams.length} keys`);
   console.info("multicallWriteParams", multicallWriteParams);
 
-  if (process.env.WRITE) {
+  if (multicallWriteParams.length > 0 && process.env.WRITE === "true") {
+    console.log("sending transaction...");
     const tx = await config.multicall(multicallWriteParams);
     console.info(`tx sent: ${tx.hash}`);
-  } else {
-    console.info("NOTE: executed in read-only mode, no transactions were sent");
+  } 
+  else if (multicallWriteParams.length === 0 && process.env.WRITE === "true") {
+    console.info("no updates needed, thus no transactions were sent");
+  }
+  else if (multicallWriteParams.length > 0 && process.env.WRITE !== "true") {
+    console.info("executed in read-only mode, thus no transactions were sent");
+  }
+  else {
+    console.info("executed in read-only mode and no updates were needed, thus no transactions were sent");
   }
 }
 
