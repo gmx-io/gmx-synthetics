b397b1cba11a987a507e8c6cd8bc2c80c4e3308b
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2024-09-03 14:55:07 +0400

    update printGlvs

--- scripts/printGlvs.ts
+++ scripts/printGlvs.ts
@@ -17,9 +17,18 @@ async function main() {
   const glvInfoList = [...(await glvReader.getGlvInfoList(dataStore.address, 0, 100))];
 
   for (const glvInfo of glvInfoList) {
+    const glvTokenContract = await hre.ethers.getContractAt("GlvToken", glvInfo.glv.glvToken);
+    const [glvSymbol, glvName] = await Promise.all([glvTokenContract.symbol(), glvTokenContract.name()]);
     const longTokenSymbol = addressToSymbol[glvInfo.glv.longToken];
     const shortTokenSymbol = addressToSymbol[glvInfo.glv.shortToken];
-    console.log("%s long: %s short: %s", glvInfo.glv.glvToken, longTokenSymbol?.padEnd(5), shortTokenSymbol?.padEnd(5));
+    console.log(
+      '%s long: %s short: %s token name: "%s" symbol: "%s"',
+      glvInfo.glv.glvToken,
+      longTokenSymbol?.padEnd(5),
+      shortTokenSymbol?.padEnd(5),
+      glvName,
+      glvSymbol
+    );
     for (const market of glvInfo.markets) {
       console.log("\t%s", market);
     }
