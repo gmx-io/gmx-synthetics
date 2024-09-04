ae93850701e83ece5766ae38b1648f4cb4e97084
S1_Dev <SentinelDev1@proton.me>
2024-09-04 01:25:25 -0400

    Moved the logic previously in updateRiskOracleConfig.ts to updateRiskOracleConfigUtils.ts to better align with how other scripts are structured.
    Updated riskOracle.ts and updateRiskOracleConfigUtils.ts per the feedback from G in the PR and now most if not all comments should be addressed.

--- config/riskOracle.ts
+++ config/riskOracle.ts
@@ -4,8 +4,8 @@ export type RiskOracleConfig = {
   riskOracle?: string;
   markets?: {
     [marketAddress: string]: {
-      syncConfigMarketDisabled: boolean;
-      marketParameters: {
+      syncConfigMarketDisabled?: boolean;
+      parameters?: {
         [parameter: string]: boolean;
       };
     };
@@ -20,16 +20,12 @@ export default async function (hre: HardhatRuntimeEnvironment): Promise<RiskOrac
     localhost: {
       markets: {
         "0x1Da892c7AE651Fe4264D61f2110f8B0DEFA4AAE4": { // Address for hardhat deployment WETH/USDC pool
-          syncConfigMarketDisabled: false,
-          marketParameters: {
+          parameters: {
             maxLongTokenPoolAmount: true,
           },
         },
         "0xfe757C5BA67A02d98522BB8048b0037EFA193A98": { // Address for hardhat deployment WETH/USDT pool
           syncConfigMarketDisabled: true,
-          marketParameters: {
-            maxLongTokenPoolAmount: false,
-          },
         },
       },
       parameters: {
@@ -48,16 +44,12 @@ export default async function (hre: HardhatRuntimeEnvironment): Promise<RiskOrac
       riskOracle: "0xE05354F4187820bF0832bF1f5fAd6a0f592b8fB6",
       markets: {
         "0xD996ff47A1F763E1e55415BC4437c59292D1F415": { // Address for current Avalanche Fuji deployment AVAX/USDC pool
-          syncConfigMarketDisabled: false,
-          marketParameters: {
+          parameters: {
             maxLongTokenPoolAmount: true,
           },
         },
         "0xbf338a6C595f06B7Cfff2FA8c958d49201466374": { // Address for current Avalanche Fuji deployment ETH/USDC pool
           syncConfigMarketDisabled: true,
-          marketParameters: {
-            maxLongTokenPoolAmount: false,
-          },
         },
       },
       parameters: {
@@ -70,5 +62,32 @@ export default async function (hre: HardhatRuntimeEnvironment): Promise<RiskOrac
   };
 
   const riskOracleConfig: RiskOracleConfig = config[hre.network.name];
+  
+  if (riskOracleConfig.markets) {
+    for (const [marketAddress, marketConfig] of Object.entries(riskOracleConfig.markets)) {
+      if ('syncConfigMarketDisabled' in marketConfig) {
+        if (typeof marketConfig.syncConfigMarketDisabled !== 'boolean') {
+          throw new Error(`syncConfigMarketDisabled for market ${marketAddress} must be a boolean.`);
+        }
+      }
+
+      if (marketConfig.parameters) {
+        for (const [parameterKey, parameterValue] of Object.entries(marketConfig.parameters)) {
+          if (typeof parameterValue !== 'boolean') {
+            throw new Error(`Parameter ${parameterKey} for market ${marketAddress} must be a boolean.`);
+          }
+        }
+      }
+    }
+  }
+
+  if (riskOracleConfig.parameters) {
+    for (const [parameterKey, parameterValue] of Object.entries(riskOracleConfig.parameters)) {
+      if (typeof parameterValue !== 'boolean') {
+        throw new Error(`parameter ${parameterKey} must be a boolean.`);
+      }
+    }
+  }
+
   return riskOracleConfig;
 }
