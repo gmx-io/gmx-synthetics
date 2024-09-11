e9c8a0ced2a8731cd8f55e76bb701f7465084ae3
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2024-09-11 11:24:01 +0400

    increase BTC GLV caps

--- config/glvs.ts
+++ config/glvs.ts
@@ -86,8 +86,8 @@ export default async function ({ network }: HardhatRuntimeEnvironment) {
         markets: [
           {
             indexToken: "BTC",
-            glvMaxMarketTokenBalanceAmount: expandDecimals(6_500_000, 18),
-            glvMaxMarketTokenBalanceUsd: decimalToFloat(11_000_000),
+            glvMaxMarketTokenBalanceAmount: expandDecimals(10_000_000, 18),
+            glvMaxMarketTokenBalanceUsd: decimalToFloat(16_500_000),
           },
           {
             indexToken: "ORDI",
