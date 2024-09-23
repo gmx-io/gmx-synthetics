075fc16de6e99c54b5973b4055c13d709a7f88eb
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2024-09-23 17:02:31 +0400

    increase swap price impact for SHIB

--- config/markets.ts
+++ config/markets.ts
@@ -884,8 +884,8 @@ const config: {
       negativePositionImpactFactor: decimalToFloat(5, 10), // 0.05% for ~45,000 USD of imbalance
       positivePositionImpactFactor: decimalToFloat(25, 11), // 0.05% for ~90,000 USD of imbalance
 
-      positiveSwapImpactFactor: decimalToFloat(25, 10),
-      negativeSwapImpactFactor: decimalToFloat(25, 10),
+      positiveSwapImpactFactor: decimalToFloat(5, 9),
+      negativeSwapImpactFactor: decimalToFloat(5, 9),
 
       fundingIncreaseFactorPerSecond: decimalToFloat(116, 14), // 0.00000000000116, at least 3.5 hours to reach max funding
       fundingDecreaseFactorPerSecond: decimalToFloat(0), // not applicable if thresholdForDecreaseFunding = 0
