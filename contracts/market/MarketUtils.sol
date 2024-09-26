b1a0900f1e18766cf85e13b4361b0bfb15451f13
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2024-09-26 22:13:06 +0400

    do not account for oi / max oi in utilisation calculation

--- contracts/market/MarketUtils.sol
+++ contracts/market/MarketUtils.sol
@@ -504,13 +504,9 @@ library MarketUtils {
     ) internal view returns (uint256) {
         uint256 reserveFactor = getOpenInterestReserveFactor(dataStore, market.marketToken, isLong);
         uint256 maxReservedUsd = Precision.applyFactor(poolUsd, reserveFactor);
-        uint256 reserveUsageFactor = Precision.toFactor(reservedUsd, maxReservedUsd);
+        uint256 usageFactor = Precision.toFactor(reservedUsd, maxReservedUsd);
 
-        uint256 maxOpenInterest = getMaxOpenInterest(dataStore, market.marketToken, isLong);
-        uint256 openInterest = getOpenInterest(dataStore, market, isLong);
-        uint256 openInterestUsageFactor = Precision.toFactor(openInterest, maxOpenInterest);
-
-        return reserveUsageFactor > openInterestUsageFactor ? reserveUsageFactor : openInterestUsageFactor;
+        return usageFactor;
     }
 
     // @dev get the max open interest allowed for the market
