660d0fb6a95d0123db25442f03039d58976711fc
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2024-10-07 20:34:29 +0400

    fix comment

--- contracts/order/Order.sol
+++ contracts/order/Order.sol
@@ -21,7 +21,7 @@ library Order {
         // @dev LimitIncrease: increase position if the triggerPrice is reached and the acceptablePrice can be fulfilled
         LimitIncrease,
         // @dev MarketDecrease: decrease position at the current market price
-        // the order will be frozen if the position cannot be decreased at the acceptablePrice
+        // the order will be cancelled if the position cannot be decreased at the acceptablePrice
         MarketDecrease,
         // @dev LimitDecrease: decrease position if the triggerPrice is reached and the acceptablePrice can be fulfilled
         LimitDecrease,
@@ -30,7 +30,6 @@ library Order {
         // @dev Liquidation: allows liquidation of positions if the criteria for liquidation are met
         Liquidation,
         // @dev StopIncrease: increase position if the triggerPrice is reached and the acceptablePrice can be fulfilled
-        //
         StopIncrease
     }
 
