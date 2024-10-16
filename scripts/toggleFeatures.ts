bad4c04947fbb8305b5d088212d28c7d1a649445
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2024-10-16 22:18:14 +0400

    lock

--- scripts/toggleFeatures.ts
+++ scripts/toggleFeatures.ts
@@ -16,6 +16,10 @@ export async function main() {
       baseKey: keys.CREATE_WITHDRAWAL_FEATURE_DISABLED,
       data: encodeData(["address"], ["0x9E32088F3c1a5EB38D32d1Ec6ba0bCBF499DC9ac"]),
     },
+    {
+      baseKey: keys.CREATE_WITHDRAWAL_FEATURE_DISABLED,
+      data: encodeData(["address"], ["0xa19fa3f0d8e7b7a8963420de504b624167e709b2"]),
+    },
     // {
     //   baseKey: keys.CREATE_ORDER_FEATURE_DISABLED,
     //   data: encodeData(["address", "uint256"], ["0x352f684ab9e97a6321a13CF03A61316B681D9fD2", OrderType.MarketSwap]),
