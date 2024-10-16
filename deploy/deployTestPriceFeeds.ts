f8bb603c49fda05bcd5639428b9c89f0248b4126
X <phi@gmx.io>
2024-10-16 16:17:51 +0800

    update scripts to use new config structure

--- deploy/deployTestPriceFeeds.ts
+++ deploy/deployTestPriceFeeds.ts
@@ -3,9 +3,9 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 const func = async ({ getNamedAccounts, deployments, gmx }: HardhatRuntimeEnvironment) => {
   const { deploy, execute } = deployments;
   const { deployer } = await getNamedAccounts();
-  const oracleConfig = await gmx.getOracle();
+  const tokens = await gmx.getTokens();
 
-  for (const [tokenSymbol, { priceFeed }] of Object.entries(oracleConfig.tokens)) {
+  for (const [tokenSymbol, { priceFeed }] of Object.entries(tokens)) {
     if (!priceFeed || !priceFeed.deploy) {
       continue;
     }
