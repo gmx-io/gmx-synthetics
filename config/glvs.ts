b9a7a3dc3d2028ecbe7c38ee30f0093a762b85d6
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2024-10-29 00:53:00 +0400

    increase SUI GLV caps

--- config/glvs.ts
+++ config/glvs.ts
@@ -101,8 +101,8 @@ export default async function ({ network }: HardhatRuntimeEnvironment) {
           },
           {
             indexToken: "SUI",
-            glvMaxMarketTokenBalanceAmount: expandDecimals(450_000, 18),
-            glvMaxMarketTokenBalanceUsd: decimalToFloat(450_000),
+            glvMaxMarketTokenBalanceAmount: expandDecimals(550_000, 18),
+            glvMaxMarketTokenBalanceUsd: decimalToFloat(550_000),
           },
           {
             indexToken: "SEI",
