e41b3dc5da88318dadfd1eef9b7f2b49ef44783b
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2024-11-12 14:55:46 +0800

    update printGlvValueDiff

--- scripts/printGlvValueDiff.ts
+++ scripts/printGlvValueDiff.ts
@@ -1,18 +1,14 @@
 import hre from "hardhat";
 
-import got from "got";
 import { bigNumberify, formatAmount } from "../utils/math";
+import { parseLogs } from "../utils/event";
+import got from "got";
 
 function getValues() {
   if (hre.network.name === "arbitrum") {
     return {
-      tickersUrl: "https://arbitrum-api.gmxinfra2.io/prices/tickers",
-    };
-  }
-
-  if (hre.network.name === "avalanche") {
-    return {
-      tickersUrl: "https://avalanche-api.gmxinfra2.io/prices/tickers",
+      explorerUrl:
+        "https://api.arbiscan.io/api/?module=account&action=txlist&address=0x26dddaa629bb35fc1853d051561f2200dd190588&startblock=1&endblock=9999999999&page=1&sort=desc",
     };
   }
 
@@ -23,38 +19,63 @@ async function main() {
   const dataStore = await hre.ethers.getContract("DataStore");
   const reader = await hre.ethers.getContract("Reader");
   const glvReader = await hre.ethers.getContract("GlvReader");
-
-  const { tickersUrl } = getValues();
-
-  const glvToken = process.env.GLV || "0x528A5bac7E746C9A509A1f4F6dF58A03d44279F9"; // ETH;
-  const glvInfo = await glvReader.getGlvInfo(dataStore.address, glvToken);
-
-  const tickers = (await got(tickersUrl).json()) as any[];
-  const tickerByToken = Object.fromEntries(tickers.map((t) => [t.tokenAddress, t]));
-
-  const longTokenTicker = tickerByToken[glvInfo.glv.longToken];
-  const shortTokenTicker = tickerByToken[glvInfo.glv.shortToken];
-
-  const markets = await reader.getMarkets(dataStore.address, 0, 100);
-  const marketByAddress = Object.fromEntries(markets.map((m) => [m.marketToken, m]));
-  const indexTokenTickers = glvInfo.markets.map(
-    (marketToken) => tickerByToken[marketByAddress[marketToken].indexToken]
-  );
+  const eventEmitter = await hre.ethers.getContract("EventEmitter");
 
   const maximize = process.env.MAXIMIZE === "true" ? true : false;
+  const markets = await reader.getMarkets(dataStore.address, 0, 100);
+  const marketToIndexToken = Object.fromEntries(markets.map((market) => [market.marketToken, market.indexToken]));
 
-  const longTokenPrice = { min: longTokenTicker.minPrice, max: longTokenTicker.maxPrice };
-  const shortTokenPrice = { min: shortTokenTicker.minPrice, max: shortTokenTicker.maxPrice };
-  const indexTokenPrices = indexTokenTickers.map((t) => ({ min: t.minPrice, max: t.maxPrice }));
+  const { explorerUrl } = getValues();
+  const response = await got.get(explorerUrl);
+  const txs = JSON.parse(response.body).result.filter((tx) => {
+    return tx.functionName.includes("executeGlvShift") && tx.txreceipt_status === "1";
+  });
 
-  let totalDiff = bigNumberify(0);
+  console.log("found %s txs", txs.length);
 
-  for (const block of [
-    272389273, 272385458, 272373531, 272359030, 272339249, 272331968, 272324537, 272315845, 272301994, 272294575,
-    272286437, 272279013, 272271345, 272254639, 272247219, 272239796, 272232371, 272225036, 272217608, 272210256,
-    272202808, 272167214, 272145993, 272100327, 272073444, 272064919, 272057271, 272049970, 272042575, 272035213,
-    272027808, 272020446, 272013029, 272005710, 271998294, 271990895,
-  ]) {
+  const totalDiff = {};
+
+  const glvInfoCache = {};
+
+  for (const tx of txs) {
+    const txHash = tx.hash;
+    // for (const txHash of ["0x2e280c6c83d1c79bc4b604f90527267ddd987960de5ec0d4d05a6a8e9af9d9d0"]) {
+    // console.log("processing %s", tx.hash);
+    const receipt = await hre.ethers.provider.getTransactionReceipt(txHash);
+    const parsedLogs = parseLogs({ contracts: { eventEmitter } }, receipt);
+    const prices = {};
+    let glvToken: string;
+    let cancelled = false;
+
+    for (const log of parsedLogs) {
+      if (!log.parsedEventData) {
+        continue;
+      }
+      const eventName = log.parsedEventInfo.eventName;
+      const data = log.parsedEventData;
+      if (eventName === "GlvValueUpdated") {
+        glvToken = data.glv;
+      } else if (eventName === "OraclePriceUpdate") {
+        prices[data.token] = { min: data.minPrice, max: data.maxPrice };
+      } else if (eventName === "GlvShiftCancelled") {
+        cancelled = true;
+        break;
+      }
+    }
+
+    if (cancelled) {
+      console.log("cancelled at %s", txHash);
+      continue;
+    }
+
+    const glvInfo = glvInfoCache[glvToken] ?? (await glvReader.getGlvInfo(dataStore.address, glvToken));
+    if (!glvInfoCache[glvToken]) {
+      glvInfoCache[glvToken] = glvInfo;
+    }
+    const longTokenPrice = prices[glvInfo.glv.longToken];
+    const shortTokenPrice = prices[glvInfo.glv.shortToken];
+    const indexTokenPrices = glvInfo.markets.map((marketToken) => prices[marketToIndexToken[marketToken]]);
+
     const [[, glvValueBefore], [, glvValueAfter]] = await Promise.all([
       glvReader.getGlvTokenPrice(
         dataStore.address,
@@ -64,7 +85,7 @@ async function main() {
         shortTokenPrice,
         glvToken,
         maximize,
-        { blockTag: block - 1 }
+        { blockTag: receipt.blockNumber - 1 }
       ),
       glvReader.getGlvTokenPrice(
         dataStore.address,
@@ -74,20 +95,27 @@ async function main() {
         shortTokenPrice,
         glvToken,
         maximize,
-        { blockTag: block }
+        { blockTag: receipt.blockNumber }
       ),
     ]);
-    totalDiff = totalDiff.add(glvValueAfter.sub(glvValueBefore));
+    totalDiff[glvToken] = totalDiff[glvToken] ?? bigNumberify(0);
+    totalDiff[glvToken] = totalDiff[glvToken].add(glvValueAfter.sub(glvValueBefore));
     console.log(
-      "block %s glv value before %s after %s diff %s",
-      block,
-      formatAmount(glvValueBefore, 30, 10),
-      formatAmount(glvValueAfter, 30, 10),
-      formatAmount(glvValueAfter.sub(glvValueBefore), 30, 10)
+      "receipt.blockNumber %s tx %s (%s) glv %s value before %s after %s diff %s cumulative diff %s",
+      receipt.blockNumber,
+      txHash,
+      new Date(tx.timeStamp * 1000).toISOString(),
+      glvToken,
+      formatAmount(glvValueBefore, 30, 10, true),
+      formatAmount(glvValueAfter, 30, 10, true),
+      formatAmount(glvValueAfter.sub(glvValueBefore), 30, 10, true),
+      formatAmount(totalDiff[glvToken], 30, 10, true)
     );
   }
 
-  console.log("total diff %s", formatAmount(totalDiff, 30, 10));
+  for (const [glvToken, diff] of Object.entries(totalDiff) as any) {
+    console.log("glv %s total diff %s", glvToken, formatAmount(diff, 30, 10));
+  }
 }
 
 main()
