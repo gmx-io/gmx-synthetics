9baf71a6501bc8a76a0b366b9199e6ae20478f50
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2024-11-12 14:52:16 +0800

    reduce max buyback price impact

--- config/tokens.ts
+++ config/tokens.ts
@@ -79,8 +79,8 @@ export type TestTokenConfig = {
 export type TokenConfig = SyntheticTokenConfig | RealTokenConfig | TestTokenConfig;
 export type TokensConfig = { [tokenSymbol: string]: TokenConfig };
 
-const LOW_BUYBACK_IMPACT = percentageToFloat("0.3%");
-const MID_BUYBACK_IMPACT = percentageToFloat("0.5%");
+const LOW_BUYBACK_IMPACT = percentageToFloat("0.25%");
+const MID_BUYBACK_IMPACT = percentageToFloat("0.45%");
 
 const config: {
   [network: string]: TokensConfig;
