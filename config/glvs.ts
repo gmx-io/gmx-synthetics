19b99c87be480b2e3d98d830d4bf7a292c287a46
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2024-11-13 23:02:42 +0800

    reduce PEPE caps in GLV

--- config/glvs.ts
+++ config/glvs.ts
@@ -91,8 +91,8 @@ export default async function ({ network }: HardhatRuntimeEnvironment) {
           },
           {
             indexToken: "PEPE",
-            glvMaxMarketTokenBalanceAmount: expandDecimals(1_820_000, 18),
-            glvMaxMarketTokenBalanceUsd: decimalToFloat(2_000_000),
+            glvMaxMarketTokenBalanceAmount: expandDecimals(900_000, 18),
+            glvMaxMarketTokenBalanceUsd: decimalToFloat(1_000_000),
           },
           {
             indexToken: "POL",
