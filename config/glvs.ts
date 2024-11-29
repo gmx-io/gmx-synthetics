2b2dbfaa52b44024b28c96891aa6c70b4cf208f5
An <angmxio@gmail.com>
2024-11-29 14:34:27 +0400

    Revert "temporarily remove unlisted markets from glvs"
    This reverts commit 26b5d4e2f77d3a66860e113ab69c260d7522bb7f.

--- config/glvs.ts
+++ config/glvs.ts
@@ -139,11 +139,11 @@ export default async function ({ network }: HardhatRuntimeEnvironment) {
             glvMaxMarketTokenBalanceAmount: expandDecimals(500_000, 18),
             glvMaxMarketTokenBalanceUsd: decimalToFloat(500_000),
           },
-          // {
-          //   indexToken: "RENDER",
-          //   glvMaxMarketTokenBalanceAmount: expandDecimals(500_000, 18),
-          //   glvMaxMarketTokenBalanceUsd: decimalToFloat(500_000),
-          // },
+          {
+            indexToken: "RENDER",
+            glvMaxMarketTokenBalanceAmount: expandDecimals(500_000, 18),
+            glvMaxMarketTokenBalanceUsd: decimalToFloat(500_000),
+          },
         ],
       },
       {
@@ -206,11 +206,11 @@ export default async function ({ network }: HardhatRuntimeEnvironment) {
             glvMaxMarketTokenBalanceAmount: expandDecimals(1_000_000, 18),
             glvMaxMarketTokenBalanceUsd: decimalToFloat(1_000_000),
           },
-          // {
-          //   indexToken: "XLM",
-          //   glvMaxMarketTokenBalanceAmount: expandDecimals(1_000_000, 18),
-          //   glvMaxMarketTokenBalanceUsd: decimalToFloat(1_000_000),
-          // },
+          {
+            indexToken: "XLM",
+            glvMaxMarketTokenBalanceAmount: expandDecimals(1_000_000, 18),
+            glvMaxMarketTokenBalanceUsd: decimalToFloat(1_000_000),
+          },
           {
             indexToken: "BCH",
             glvMaxMarketTokenBalanceAmount: expandDecimals(500_000, 18),
