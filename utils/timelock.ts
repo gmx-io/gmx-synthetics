73b87a4c211bbfbeb370d3bd47fdcd3175e7da28
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2024-12-10 21:58:48 +0400

    add simulation before executing timelock

--- utils/timelock.ts
+++ utils/timelock.ts
@@ -3,13 +3,7 @@ import { signExternally } from "./signer";
 export async function timelockWriteMulticall({ timelock, multicallWriteParams }) {
   console.info("multicallWriteParams", multicallWriteParams);
 
-  if (process.env.WRITE === "true") {
-    if (multicallWriteParams.length === 0) {
-      throw new Error("multicallWriteParams is empty");
-    }
-
-    await signExternally(await timelock.populateTransaction.multicall(multicallWriteParams));
-  } else {
+  if (multicallWriteParams.length !== 0) {
     await hre.deployments.read(
       "Timelock",
       {
@@ -19,6 +13,14 @@ export async function timelockWriteMulticall({ timelock, multicallWriteParams })
       "multicall",
       multicallWriteParams
     );
+  }
+
+  if (process.env.WRITE === "true") {
+    if (multicallWriteParams.length === 0) {
+      throw new Error("multicallWriteParams is empty");
+    }
+    await signExternally(await timelock.populateTransaction.multicall(multicallWriteParams));
+  } else {
     console.info("NOTE: executed in read-only mode, no transactions were sent, simulation was successful");
   }
 }
