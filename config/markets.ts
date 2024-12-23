5816866326986add1ace04ad52c24ab67c64b183
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2024-12-23 20:51:08 +0400

    reduce XRP max OI

--- config/markets.ts
+++ config/markets.ts
@@ -2540,7 +2540,7 @@ const config: {
       // minCollateralFactor of 0.01 (1%) when open interest is 5,000,000 USD
       minCollateralFactorForOpenInterestMultiplier: exponentToFloat("2e-9"),
 
-      maxOpenInterest: decimalToFloat(1_000_000),
+      maxOpenInterest: decimalToFloat(5_000_000),
 
       fundingIncreaseFactorPerSecond: exponentToFloat("1.6e-12"), // 0.0000000000016, at least 3.5 hours to reach max funding
       maxFundingFactorPerSecond: exponentToFloat("2e-8"), // 0.000002%,  0.0072% per hour, 63% per year
