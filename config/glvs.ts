17215b21e2dba913f556255027bea50e8a7d63d7
An <angmxio@gmail.com>
2025-01-20 18:23:02 +0400

    update TRUMP glv cap

--- config/glvs.ts
+++ config/glvs.ts
@@ -146,8 +146,8 @@ export default async function ({ network }: HardhatRuntimeEnvironment) {
           },
           {
             indexToken: "TRUMP",
-            glvMaxMarketTokenBalanceAmount: expandDecimals(250_000, 18),
-            glvMaxMarketTokenBalanceUsd: decimalToFloat(250_000),
+            glvMaxMarketTokenBalanceAmount: expandDecimals(500_000, 18),
+            glvMaxMarketTokenBalanceUsd: decimalToFloat(500_000),
           },
         ],
       },
