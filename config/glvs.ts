7e14e0efad5d32707e447eaaa2f62758827d7209
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2025-01-20 19:39:54 +0400

    increase TRUMP glv cap

--- config/glvs.ts
+++ config/glvs.ts
@@ -146,8 +146,8 @@ export default async function ({ network }: HardhatRuntimeEnvironment) {
           },
           {
             indexToken: "TRUMP",
-            glvMaxMarketTokenBalanceAmount: expandDecimals(500_000, 18),
-            glvMaxMarketTokenBalanceUsd: decimalToFloat(500_000),
+            glvMaxMarketTokenBalanceAmount: expandDecimals(750_000, 18),
+            glvMaxMarketTokenBalanceUsd: decimalToFloat(750_000),
           },
         ],
       },
