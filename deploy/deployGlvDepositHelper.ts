782aaaee4ff0dc36972ff3b76fc6c84554d13baf
Max <maxgmx@protonmail.com>
2025-01-31 10:24:08 +0200

    Rename deploy script

--- deploy/deployGlvDepositHelper.ts
+++
@@ -1,8 +0,0 @@
-import { createDeployFunction } from "../utils/deploy";
-
-const func = createDeployFunction({
-  contractName: "GlvDepositCalc",
-  libraryNames: ["MarketUtils", "MarketStoreUtils"],
-});
-
-export default func;
