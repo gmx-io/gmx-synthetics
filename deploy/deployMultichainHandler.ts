fbd41c6d71fd2a5bdfcb17d4ee979f08254a9805
Max <maxgmx@protonmail.com>
2025-02-01 09:08:20 +0200

    Rename MultichainHandler in MultichainVaultHandler

--- deploy/deployMultichainHandler.ts
+++ deploy/deployMultichainHandler.ts
@@ -4,7 +4,7 @@ import { createDeployFunction } from "../utils/deploy";
 const constructorContracts = ["RoleStore", "DataStore", "EventEmitter", "Oracle", "MultichainVault", "ExchangeRouter"];
 
 const func = createDeployFunction({
-  contractName: "MultichainHandler",
+  contractName: "MultichainVaultHandler",
   dependencyNames: constructorContracts,
   getDeployArgs: async ({ dependencyContracts }) => {
     return constructorContracts.map((dependencyName) => dependencyContracts[dependencyName].address);
