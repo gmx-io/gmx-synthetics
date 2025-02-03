b70fb57896ff3739b1dddcea1f30442b11517c80
Max <maxgmx@protonmail.com>
2025-02-03 20:42:52 +0200

    Add multichain increase and decrease utils methods

--- contracts/multichain/MultichainUtils.sol
+++ contracts/multichain/MultichainUtils.sol
@@ -2,7 +2,18 @@
 
 pragma solidity ^0.8.0;
 
+import "../data/DataStore.sol";
+import "../data/Keys.sol";
+
 /**
  * @title MultichainUtils
  */
-library MultichainUtils {}
+library MultichainUtils {
+    function increaseBalance(DataStore dataStore, uint256 chainId, address account, address token, uint256 amount) internal {
+        dataStore.decrementUint(Keys.multichainBalanceKey(chainId, account, token), amount);
+    }
+
+    function decreaseBalance(DataStore dataStore, uint256 chainId, address account, address token, uint256 amount) internal {
+        dataStore.decrementUint(Keys.multichainBalanceKey(chainId, account, token), amount);
+    }
+}
