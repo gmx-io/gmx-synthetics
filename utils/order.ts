39c146e7a222041aabbb996bf5e15c738db65763
Max <maxgmx@protonmail.com>
2025-02-09 19:40:46 +0200

    Add srcChainId to orders

--- utils/order.ts
+++ utils/order.ts
@@ -131,6 +131,7 @@ export async function createOrder(fixture, overrides) {
       callbackGasLimit,
       minOutputAmount,
       validFromTime,
+      srcChainId,
     },
     orderType,
     decreasePositionSwapType,
