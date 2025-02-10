b1bcadd840290ad7f7948ca67d36cc5399aae26f
Max <maxgmx@protonmail.com>
2025-02-10 10:25:57 +0200

    Rename multichainId to srcChainId, remove second topic log

--- contracts/multichain/MultichainProviderSignature.sol
+++ contracts/multichain/MultichainProviderSignature.sol
@@ -14,24 +14,24 @@ contract MultichainProviderSignature is EIP712 {
     string private constant SIGNATURE_VERSION = "1";
 
     // Define the EIP-712 struct type:
-    // Message(address token,uint256 amount,address account,uint256 multichainId,uint32 srcEid)
+    // Message(address token,uint256 amount,address account,uint256 srcChainId,uint32 srcEid)
     bytes32 private constant _MESSAGE_TYPEHASH =
-        keccak256("Message(address token,uint256 amount,address account,uint256 multichainId,uint32 srcEid)");
+        keccak256("Message(address token,uint256 amount,address account,uint256 srcChainId,uint32 srcEid)");
 
     constructor() EIP712(SIGNING_DOMAIN, SIGNATURE_VERSION) {}
 
     /**
      * Check the signature for a given message
-     * @param message The ABI encoded parameters (token, amount, account, multichainId, srcEid).
+     * @param message The ABI encoded parameters (token, amount, account, srcChainId, srcEid).
      * @param signature The EIP-712 signature.
      */
     function isSigner(bytes calldata message, bytes calldata signature) external view returns (bool) {
         // Decode the message
-        (address token, uint256 amount, address account, uint256 multichainId, uint32 srcEid) = MultichainProviderUtils
+        (address token, uint256 amount, address account, uint256 srcChainId, uint32 srcEid) = MultichainProviderUtils
             .decodeWithdrawal(message);
 
         // Build the struct hash
-        bytes32 structHash = keccak256(abi.encode(_MESSAGE_TYPEHASH, token, amount, account, multichainId, srcEid));
+        bytes32 structHash = keccak256(abi.encode(_MESSAGE_TYPEHASH, token, amount, account, srcChainId, srcEid));
 
         // Get the typed data hash for EIP-712
         bytes32 hash = _hashTypedDataV4(structHash);
