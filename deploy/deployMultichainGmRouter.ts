fd3a8ed20c9fb119dcd7f9d642adf92920b49e60
Max <maxgmx@protonmail.com>
2025-02-18 09:47:24 +0200

    Add deploy script for MultichainGmRouter

---
+++ deploy/deployMultichainGmRouter.ts
@@ -0,0 +1,48 @@
+import { grantRoleIfNotGranted } from "../utils/role";
+import { createDeployFunction } from "../utils/deploy";
+
+const baseConstructorContracts = [
+  "Router",
+  "DataStore",
+  "EventEmitter",
+  "Oracle",
+  "OrderVault",
+  "OrderHandler",
+  "ExternalHandler",
+  "MultichainVault",
+];
+
+const gmConstructorContracts = ["DepositVault", "DepositHandler", "WithdrawalVault", "WithdrawalHandler", "ShiftVault"];
+
+const func = createDeployFunction({
+  contractName: "MultichainGmRouter",
+  dependencyNames: [...baseConstructorContracts, ...gmConstructorContracts],
+  getDeployArgs: async ({ dependencyContracts }) => {
+    const baseParams = {
+      router: dependencyContracts.Router.address,
+      dataStore: dependencyContracts.DataStore.address,
+      eventEmitter: dependencyContracts.EventEmitter.address,
+      oracle: dependencyContracts.Oracle.address,
+      orderVault: dependencyContracts.OrderVault.address,
+      orderHandler: dependencyContracts.OrderHandler.address,
+      externalHandler: dependencyContracts.ExternalHandler.address,
+      multichainVault: dependencyContracts.MultichainVault.address,
+    };
+
+    return [
+      baseParams,
+      dependencyContracts.DepositVault.address,
+      dependencyContracts.DepositHandler.address,
+      dependencyContracts.WithdrawalVault.address,
+      dependencyContracts.WithdrawalHandler.address,
+      dependencyContracts.ShiftVault.address,
+    ];
+  },
+  libraryNames: ["MarketStoreUtils", "MultichainUtils", "OrderStoreUtils", "RelayUtils", "ShiftUtils", "SwapUtils"],
+  afterDeploy: async ({ deployedContract }) => {
+    await grantRoleIfNotGranted(deployedContract.address, "CONTROLLER");
+    await grantRoleIfNotGranted(deployedContract.address, "ROUTER_PLUGIN");
+  },
+});
+
+export default func;
