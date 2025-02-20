b252c9f50028385d6564699011e338a0a86ebe73
Max <maxgmx@protonmail.com>
2025-02-20 21:37:24 +0200

    WIP test multichain withdrawal

--- test/multichain/MultichainGmRouter.ts
+++ test/multichain/MultichainGmRouter.ts
@@ -4,14 +4,25 @@ import { impersonateAccount, setBalance } from "@nomicfoundation/hardhat-network
 import { expandDecimals } from "../../utils/math";
 import { deployFixture } from "../../utils/fixture";
 import { GELATO_RELAY_ADDRESS } from "../../utils/relay/addresses";
-import { sendCreateDeposit } from "../../utils/relay/multichain";
+import { sendCreateDeposit, sendCreateWithdrawal } from "../../utils/relay/multichain";
 import * as keys from "../../utils/keys";
-import { getDepositKeys } from "../../utils/deposit";
+import { executeDeposit, getDepositCount, getDepositKeys } from "../../utils/deposit";
+import { getWithdrawalCount, getWithdrawalKeys } from "../../utils/withdrawal";
+import { getBalanceOf } from "../../utils/token";
 
 describe("MultichainGmRouter", () => {
   let fixture;
   let user0, user1, user2, user3;
-  let reader, dataStore, multichainGmRouter, depositVault, ethUsdMarket, wnt, usdc, layerZeroProvider, mockStargatePool;
+  let reader,
+    dataStore,
+    multichainGmRouter,
+    depositVault,
+    withdrawalVault,
+    ethUsdMarket,
+    wnt,
+    usdc,
+    layerZeroProvider,
+    mockStargatePool;
   let relaySigner;
   let chainId;
 
@@ -26,6 +37,7 @@ describe("MultichainGmRouter", () => {
       dataStore,
       multichainGmRouter,
       depositVault,
+      withdrawalVault,
       ethUsdMarket,
       wnt,
       usdc,
@@ -35,7 +47,7 @@ describe("MultichainGmRouter", () => {
 
     defaultParams = {
       addresses: {
-        receiver: user0.address,
+        receiver: user1.address,
         callbackContract: user1.address,
         uiFeeReceiver: user2.address,
         market: ethUsdMarket.marketToken,
@@ -44,7 +56,7 @@ describe("MultichainGmRouter", () => {
         longTokenSwapPath: [ethUsdMarket.marketToken],
         shortTokenSwapPath: [ethUsdMarket.marketToken],
       },
-      minMarketTokens: 0,
+      minMarketTokens: 100,
       shouldUnwrapNativeToken: false,
       executionFee: 0,
       callbackGasLimit: "200000",
@@ -65,11 +77,10 @@ describe("MultichainGmRouter", () => {
         feeAmount: expandDecimals(5, 15), // 0.005 ETH
         feeSwapPath: [],
       },
-      tokenPermits: [],
       transferRequests: {
         tokens: [wnt.address, usdc.address],
         receivers: [depositVault.address, depositVault.address],
-        amounts: [expandDecimals(1, 18), expandDecimals(5_000, 6)],
+        amounts: [expandDecimals(10, 18), expandDecimals(50_000, 6)],
       },
       account: user0.address,
       params: defaultParams,
@@ -84,8 +95,8 @@ describe("MultichainGmRouter", () => {
 
     await dataStore.setAddress(keys.FEE_RECEIVER, user3.address);
 
-    const wntAmount = expandDecimals(10, 18); // 10 ETH
-    const usdcAmount = expandDecimals(50_000, 6); // 50,000 USDC
+    const wntAmount = expandDecimals(15, 18); // 15 ETH
+    const usdcAmount = expandDecimals(75_000, 6); // 75,000 USDC
     await wnt.mint(user0.address, wntAmount);
     await usdc.mint(user0.address, usdcAmount);
 
@@ -112,11 +123,11 @@ describe("MultichainGmRouter", () => {
   describe("createDeposit", () => {
     it("creates deposit and sends relayer fee", async () => {
       expect(await dataStore.getUint(keys.multichainBalanceKey(user0.address, wnt.address))).to.eq(
-        expandDecimals(10, 18)
-      ); // 10 ETH
+        expandDecimals(15, 18)
+      ); // 15 ETH
       expect(await dataStore.getUint(keys.multichainBalanceKey(user0.address, usdc.address))).to.eq(
-        expandDecimals(50_000, 6)
-      ); // 50,000 USDC
+        expandDecimals(75_000, 6)
+      ); // 75,000 USDC
       expect(await wnt.balanceOf(GELATO_RELAY_ADDRESS)).to.eq(0);
       expect(await wnt.balanceOf(user3.address)).eq(0); // FEE_RECEIVER
 
@@ -124,11 +135,14 @@ describe("MultichainGmRouter", () => {
 
       // user's multichain balance was decreased by the deposit amounts + 0.005 ETH fee
       expect(await dataStore.getUint(keys.multichainBalanceKey(user0.address, wnt.address))).to.eq(
-        expandDecimals(8_995, 15)
-      ); // 10 - 1 - 0.005 = 8.995 ETH
+        // fee is paid first, transfers are proccessed afterwards => user must bridge deposit + fee
+        // TODO: should the 0.005 fee be taken from deposit instead of user's multichain balance
+        // e.g. if there are exactly 10 WNT in user's multichain balance and does a 10 WNT deposit, tx fails because there are no additional funds to pay the fee
+        expandDecimals(4_995, 15)
+      ); // 15 - 10 - 0.005 = 4.995 ETH
       expect(await dataStore.getUint(keys.multichainBalanceKey(user0.address, usdc.address))).to.eq(
-        expandDecimals(45_000, 6)
-      ); // 50,000 - 5,000 = 45,000 USDC
+        expandDecimals(25_000, 6)
+      ); // 75,000 - 50,000 = 25,000 USDC
       expect(await wnt.balanceOf(GELATO_RELAY_ADDRESS)).to.eq(createDepositParams.relayFeeAmount); // 0.002 ETH
       expect(await wnt.balanceOf(user3.address)).eq(0); // FEE_RECEIVER
 
@@ -138,19 +152,114 @@ describe("MultichainGmRouter", () => {
       expect(deposit.addresses.receiver).eq(defaultParams.addresses.receiver);
       expect(deposit.addresses.callbackContract).eq(defaultParams.addresses.callbackContract);
       expect(deposit.addresses.market).eq(defaultParams.addresses.market);
-      expect(deposit.addresses.initialLongToken).eq(defaultParams.addresses.initialLongToken);
-      expect(deposit.addresses.initialShortToken).eq(defaultParams.addresses.initialShortToken);
+      expect(deposit.addresses.initialLongToken).eq(createDepositParams.transferRequests.tokens[0]);
+      expect(deposit.addresses.initialShortToken).eq(createDepositParams.transferRequests.tokens[1]);
       expect(deposit.addresses.longTokenSwapPath).deep.eq(defaultParams.addresses.longTokenSwapPath);
       expect(deposit.addresses.shortTokenSwapPath).deep.eq(defaultParams.addresses.shortTokenSwapPath);
-      expect(deposit.numbers.initialLongTokenAmount).eq(createDepositParams.transferRequests.amounts[0]); // 1 ETH
-      expect(deposit.numbers.initialShortTokenAmount).eq(createDepositParams.transferRequests.amounts[1]); // 5,000 USDC
+      expect(deposit.numbers.initialLongTokenAmount).eq(createDepositParams.transferRequests.amounts[0]); // 10 ETH
+      expect(deposit.numbers.initialShortTokenAmount).eq(createDepositParams.transferRequests.amounts[1]); // 50,000 USDC
       expect(deposit.numbers.minMarketTokens).eq(defaultParams.minMarketTokens);
       expect(deposit.numbers.executionFee).eq(
-        createDepositParams.feeParams.feeAmount.sub(createDepositParams.relayFeeAmount)
+        ethers.BigNumber.from(createDepositParams.feeParams.feeAmount).sub(createDepositParams.relayFeeAmount)
       ); // 0.005 - 0.002 = 0.003 ETH
       expect(deposit.numbers.callbackGasLimit).eq(defaultParams.callbackGasLimit);
       expect(deposit.flags.shouldUnwrapNativeToken).eq(defaultParams.shouldUnwrapNativeToken);
       expect(deposit._dataList).deep.eq(defaultParams.dataList);
+    });
+  });
+
+  describe("createWithdrawal", () => {
+    let defaultWithdrawalParams;
+    let createWithdrawalParams: Parameters<typeof sendCreateWithdrawal>[0];
+    beforeEach(async () => {
+      defaultWithdrawalParams = {
+        addresses: {
+          receiver: user1.address,
+          callbackContract: user2.address,
+          uiFeeReceiver: user2.address,
+          market: ethUsdMarket.marketToken,
+          longTokenSwapPath: [ethUsdMarket.marketToken],
+          shortTokenSwapPath: [ethUsdMarket.marketToken],
+        },
+        minLongTokenAmount: 0,
+        minShortTokenAmount: 0,
+        shouldUnwrapNativeToken: false,
+        executionFee: 0,
+        callbackGasLimit: "200000",
+        dataList: [],
+      };
+
+      createWithdrawalParams = {
+        sender: relaySigner,
+        signer: user1, // user1 was the receiver of the deposit
+        feeParams: {
+          feeToken: wnt.address,
+          feeAmount: expandDecimals(5, 15), // 0.005 ETH
+          feeSwapPath: [],
+        },
+        transferRequests: {
+          tokens: [ethUsdMarket.marketToken],
+          receivers: [withdrawalVault.address],
+          amounts: [expandDecimals(100_000, 18)],
+        },
+        account: user1.address, // user1 was the receiver of the deposit
+        params: defaultWithdrawalParams,
+        deadline: 9999999999,
+        chainId,
+        srcChainId: chainId,
+        desChainId: chainId,
+        relayRouter: multichainGmRouter,
+        relayFeeToken: wnt.address,
+        relayFeeAmount: expandDecimals(2, 15), // 0.002 ETH
+      };
+    });
+
+    it("creates withdrawal and sends relayer fee", async () => {
+      await sendCreateDeposit(createDepositParams);
+
+      // const _initialLongToken = await contractAt("MintableToken", defaultParams.addresses.initialLongToken);
+      // await _initialLongToken.mint(depositVault.address, createDepositParams.transferRequests.amounts[0]);
+      // const _initialShortToken = await contractAt("MintableToken", defaultParams.addresses.initialShortToken);
+      // await _initialShortToken.mint(depositVault.address, createDepositParams.transferRequests.amounts[1]);
+
+      expect(await wnt.balanceOf(user0.address)).eq(0);
+      expect(await usdc.balanceOf(user0.address)).eq(0);
+      expect(await getBalanceOf(ethUsdMarket.marketToken, user1.address)).eq(0); // GM
+      expect(await wnt.balanceOf(depositVault.address)).eq(expandDecimals(10, 18).add(expandDecimals(3, 15))); // 10.003 ETH
+      expect(await usdc.balanceOf(depositVault.address)).eq(expandDecimals(50_000, 6)); // 50,000 USDC
+
+      // TODO: Deposit was cancelled: {"name":"UsdDeltaExceedsPoolValue","args":["-50000000000000000000000000000000000","0"]}
+      // if commenting out utils/deposit.ts/L140   =>   throw new Error(`Deposit was cancelled: ${getErrorString(cancellationReason)}`);
+      // then contracts execute the deposit, but funds are returned to user0 and no GM tokens are minted to user1
+      expect(await getDepositCount(dataStore)).eq(1);
+      await executeDeposit(fixture, { gasUsageLabel: "executeDeposit" });
+      expect(await getDepositCount(dataStore)).eq(0);
+
+      // expect(await wnt.balanceOf(user0.address)).eq(0); // TODO: executeDeposit failed and 10 ETH was returned to user0
+      // expect(await usdc.balanceOf(user0.address)).eq(0); // TODO: executeDeposit failed and 50,000 USDC was returned to user0
+      // expect(await getBalanceOf(ethUsdMarket.marketToken, user1.address)).eq(expandDecimals(100_000, 18)); // TODO: executeDeposit failed and no GM tokens were minted to user1
+      expect(await wnt.balanceOf(depositVault.address)).eq(0);
+      expect(await usdc.balanceOf(depositVault.address)).eq(0);
+
+      // TODO: fix executeDeposit to mint GM tokens
+
+      expect(await getWithdrawalCount(dataStore)).eq(0);
+      // await sendCreateWithdrawal(createWithdrawalParams);
+      // expect(await getWithdrawalCount(dataStore)).eq(1);
+
+      // const withdrawalKeys = await getWithdrawalKeys(dataStore, 0, 1);
+      // const withdrawal = await reader.getWithdrawal(dataStore.address, withdrawalKeys[0]);
+      // expect(withdrawal.addresses.account).eq(user1.address);
+      // expect(withdrawal.addresses.receiver).eq(defaultWithdrawalParams.addresses.receiver);
+      // expect(withdrawal.addresses.callbackContract).eq(defaultWithdrawalParams.addresses.callbackContract);
+      // expect(withdrawal.addresses.market).eq(defaultWithdrawalParams.addresses.market);
+      // expect(withdrawal.numbers.marketTokenAmount).eq(createWithdrawalParams.transferRequests.amounts[0]); // 100,000 GM
+      // expect(withdrawal.numbers.minLongTokenAmount).eq(createWithdrawalParams.params.minLongTokenAmount);
+      // expect(withdrawal.numbers.minShortTokenAmount).eq(createWithdrawalParams.params.minShortTokenAmount);
+      // expect(withdrawal.numbers.executionFee).eq(createWithdrawalParams.params.executionFee);
+      // expect(withdrawal.numbers.callbackGasLimit).eq(createWithdrawalParams.params.callbackGasLimit);
+      // expect(withdrawal.flags.shouldUnwrapNativeToken).eq(createWithdrawalParams.params.shouldUnwrapNativeToken);
+      // expect(withdrawal._dataList).deep.eq(createWithdrawalParams.params.dataList);
     });
   });
 });
--- utils/relay/multichain.ts
+++ utils/relay/multichain.ts
@@ -3,6 +3,7 @@ import { GELATO_RELAY_ADDRESS } from "./addresses";
 import { hashRelayParams, signTypedData } from "./helpers";
 import { getDomain } from "./helpers";
 import { getRelayParams } from "./helpers";
+import { contractAt } from "../deploy";
 
 export async function sendCreateDeposit(p: {
   signer: ethers.Signer;
@@ -18,12 +19,6 @@ export async function sendCreateDeposit(p: {
     refundTokens: string[];
     refundReceivers: string[];
   };
-  tokenPermits?: {
-    token: string;
-    spender: string;
-    value: BigNumberish;
-    deadline: BigNumberish;
-  }[];
   feeParams: {
     feeToken: string;
     feeAmount: BigNumberish;
@@ -70,6 +65,56 @@ export async function sendCreateDeposit(p: {
   });
 }
 
+export async function sendCreateWithdrawal(p: {
+  signer: ethers.Signer;
+  sender: ethers.Signer;
+  transferRequests: {
+    tokens: string[];
+    receivers: string[];
+    amounts: BigNumberish[];
+  };
+  account: string;
+  params: any;
+  signature?: string;
+  userNonce?: BigNumberish;
+  chainId: BigNumberish;
+  srcChainId: BigNumberish;
+  desChainId: BigNumberish;
+  feeParams: any;
+  deadline: BigNumberish;
+  relayRouter: ethers.Contract;
+  relayFeeToken: string;
+  relayFeeAmount: BigNumberish;
+}) {
+  const relayParams = await getRelayParams(p);
+  let signature = p.signature;
+  if (!signature) {
+    signature = await getCreateWithdrawalSignature({
+      signer: p.signer,
+      relayParams,
+      transferRequests: p.transferRequests,
+      verifyingContract: p.relayRouter.address,
+      params: p.params,
+      chainId: p.chainId,
+    });
+  }
+  const createWithdrawalCalldata = p.relayRouter.interface.encodeFunctionData("createWithdrawal", [
+    { ...relayParams, signature },
+    p.account,
+    p.srcChainId,
+    p.transferRequests,
+    p.params,
+  ]);
+  const calldata = ethers.utils.solidityPack(
+    ["bytes", "address", "address", "uint256"],
+    [createWithdrawalCalldata, GELATO_RELAY_ADDRESS, p.relayFeeToken, p.relayFeeAmount]
+  );
+  return p.sender.sendTransaction({
+    to: p.relayRouter.address,
+    data: calldata,
+  });
+}
+
 async function getCreateDepositSignature({
   signer,
   relayParams,
@@ -77,6 +122,13 @@ async function getCreateDepositSignature({
   verifyingContract,
   params,
   chainId,
+}: {
+  signer: ethers.Signer;
+  relayParams: any;
+  transferRequests: { tokens: string[]; receivers: string[]; amounts: BigNumberish[] };
+  verifyingContract: string;
+  params: any;
+  chainId: BigNumberish;
 }) {
   if (relayParams.userNonce === undefined) {
     throw new Error("userNonce is required");
@@ -119,5 +171,63 @@ async function getCreateDepositSignature({
   };
   const domain = getDomain(chainId, verifyingContract);
 
+  return signTypedData(signer, domain, types, typedData);
+}
+
+async function getCreateWithdrawalSignature({
+  signer,
+  relayParams,
+  transferRequests,
+  verifyingContract,
+  params,
+  chainId,
+}: {
+  signer: ethers.Signer;
+  relayParams: any;
+  transferRequests: { tokens: string[]; receivers: string[]; amounts: BigNumberish[] };
+  verifyingContract: string;
+  params: any;
+  chainId: BigNumberish;
+}) {
+  if (relayParams.userNonce === undefined) {
+    throw new Error("userNonce is required");
+  }
+  const types = {
+    CreateWithdrawal: [
+      { name: "transferTokens", type: "address[]" },
+      { name: "transferReceivers", type: "address[]" },
+      { name: "transferAmounts", type: "uint256[]" },
+      { name: "addresses", type: "CreateWithdrawalAddresses" },
+      { name: "minLongTokenAmount", type: "uint256" },
+      { name: "minShortTokenAmount", type: "uint256" },
+      { name: "shouldUnwrapNativeToken", type: "bool" },
+      { name: "executionFee", type: "uint256" },
+      { name: "callbackGasLimit", type: "uint256" },
+      { name: "dataList", type: "bytes32[]" },
+      { name: "relayParams", type: "bytes32" },
+    ],
+    CreateWithdrawalAddresses: [
+      { name: "receiver", type: "address" },
+      { name: "callbackContract", type: "address" },
+      { name: "uiFeeReceiver", type: "address" },
+      { name: "market", type: "address" },
+      { name: "longTokenSwapPath", type: "address[]" },
+      { name: "shortTokenSwapPath", type: "address[]" },
+    ],
+  };
+  const typedData = {
+    transferTokens: transferRequests.tokens,
+    transferReceivers: transferRequests.receivers,
+    transferAmounts: transferRequests.amounts,
+    addresses: params.addresses,
+    minLongTokenAmount: params.minLongTokenAmount,
+    minShortTokenAmount: params.minShortTokenAmount,
+    shouldUnwrapNativeToken: params.shouldUnwrapNativeToken,
+    executionFee: params.executionFee,
+    callbackGasLimit: params.callbackGasLimit,
+    dataList: params.dataList,
+    relayParams: hashRelayParams(relayParams),
+  };
+  const domain = getDomain(chainId, verifyingContract);
   return signTypedData(signer, domain, types, typedData);
 }
