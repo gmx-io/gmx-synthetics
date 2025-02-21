e05f6a96f19eb7a167818d5ab0c015ce446acb52
Solar <call.sol.dev@gmail.com>
2025-02-21 10:50:00 +0300

    squash - refactor callbacks to eventdata

--- deploy/deploySwapUtils.ts
+++ deploy/deploySwapUtils.ts
@@ -2,7 +2,7 @@ import { createDeployFunction } from "../utils/deploy";
 
 const func = createDeployFunction({
   contractName: "SwapUtils",
-  libraryNames: ["FeeUtils", "MarketEventUtils", "SwapPricingUtils"],
+  libraryNames: ["FeeUtils", "MarketEventUtils", "SwapPricingUtils", "MarketStoreUtils"],
 });
 
 export default func;
