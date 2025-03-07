af28470f518da84ca87116fadfaa2369bd593947
Max <maxgmx@protonmail.com>
2025-03-07 12:36:36 +0200

    fix test after merge

--- test/router/SubaccountRouter.ts
+++ test/router/SubaccountRouter.ts
@@ -267,8 +267,8 @@ describe("SubaccountRouter", () => {
     expect(order._dataList).deep.eq(dataList);
 
     // 0.1 WETH in total
-    expect(order.numbers.executionFee).eq("2111028760000000");
-    await expectBalance(wnt.address, user2.address, "97888971240000000");
+    expect(order.numbers.executionFee).eq("2111032600000000");
+    await expectBalance(wnt.address, user2.address, "97888967400000000");
 
     expect(
       await dataStore.getUint(
