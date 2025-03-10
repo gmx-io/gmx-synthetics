1dea05284569477671e79e82aa7940ffb567f075
Max <maxgmx@protonmail.com>
2025-03-10 13:42:09 +0200

    Move WIP bridgeOut test from MultichainRouter into MultichainTransferRouter

--- test/multichain/MultichainTransferRouter.ts
+++ test/multichain/MultichainTransferRouter.ts
@@ -4,17 +4,20 @@ import { deployFixture } from "../../utils/fixture";
 import { expandDecimals } from "../../utils/math";
 import { logGasUsage } from "../../utils/gas";
 import * as keys from "../../utils/keys";
+import { sendBridgeOut } from "../../utils/relay/multichain";
+import { GELATO_RELAY_ADDRESS } from "../../utils/relay/addresses";
+import { mintAndBridge } from "./MultichainRouter";
 
 describe("MultichainTransferRouter", () => {
   let fixture;
   let user1;
-  let dataStore, multichainVault, router, multichainTransferRouter, wnt, usdc;
+  let dataStore, multichainVault, router, multichainTransferRouter, wnt, usdc, mockStargatePool;
   let chainId;
 
   beforeEach(async () => {
     fixture = await deployFixture();
     ({ user1 } = fixture.accounts);
-    ({ dataStore, multichainVault, router, multichainTransferRouter, wnt, usdc } = fixture.contracts);
+    ({ dataStore, multichainVault, router, multichainTransferRouter, wnt, usdc, mockStargatePool } = fixture.contracts);
 
     chainId = await hre.ethers.provider.getNetwork().then((network) => network.chainId);
   });
@@ -76,6 +79,61 @@ describe("MultichainTransferRouter", () => {
     await logGasUsage({
       tx,
       label: "multichainTransferRouter.bridgeIn",
+    });
+  });
+
+  describe("bridgeOut", () => {
+    let relaySigner;
+
+    const feeAmount = expandDecimals(6, 15);
+    const bridgeAmount = expandDecimals(1000, 6);
+
+    let defaultBridgeOutParams;
+    beforeEach(async () => {
+      defaultBridgeOutParams = {
+        token: usdc.address,
+        amount: bridgeAmount,
+      };
+    });
+
+    let bridgeOutParams: Parameters<typeof sendBridgeOut>[0];
+    beforeEach(async () => {
+      relaySigner = await hre.ethers.getSigner(GELATO_RELAY_ADDRESS);
+
+      bridgeOutParams = {
+        sender: relaySigner,
+        signer: user1,
+        feeParams: {
+          feeToken: wnt.address,
+          feeAmount: feeAmount,
+          feeSwapPath: [],
+        },
+        provider: mockStargatePool.address,
+        account: user1.address,
+        data: "0x", // e.g. Eid
+        params: defaultBridgeOutParams,
+        deadline: 9999999999,
+        srcChainId: chainId, // 0 means non-multichain action
+        desChainId: chainId, // for non-multichain actions, desChainId is the same as chainId
+        relayRouter: multichainTransferRouter,
+        chainId,
+        relayFeeToken: wnt.address,
+        relayFeeAmount: feeAmount,
+      };
+    });
+
+    // TODO: mock StargatePool and enable bridging out test
+    it.skip("bridgeOut", async () => {
+      await dataStore.setBool(keys.isMultichainProviderEnabledKey(mockStargatePool.address), true);
+      await mintAndBridge(fixture, { account: user1, token: usdc, tokenAmount: bridgeAmount });
+
+      expect(await usdc.balanceOf(user1.address)).eq(0);
+      expect(await dataStore.getUint(keys.multichainBalanceKey(user1.address, usdc.address))).to.eq(bridgeAmount);
+
+      await sendBridgeOut(bridgeOutParams);
+
+      expect(await usdc.balanceOf(user1.address)).eq(bridgeAmount);
+      expect(await dataStore.getUint(keys.multichainBalanceKey(user1.address, usdc.address))).to.eq(0);
     });
   });
 });
