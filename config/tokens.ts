1f27f201e48e4475023a1967bf056da29e4d8dda
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2025-03-17 13:30:27 +0000

    redeploy ExchangeRouter only

--- config/tokens.ts
+++ config/tokens.ts
@@ -1074,6 +1074,7 @@ const config: {
     LINK: {
       address: "0x5947BB275c521040051D82396192181b413227A3",
       decimals: 18,
+      transferGasLimit: 200 * 1000,
     },
   },
   arbitrumSepolia: {
