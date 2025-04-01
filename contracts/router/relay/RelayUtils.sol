1a871250ddb0fdf58d93d345b8e36bd9de441138
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2025-04-01 21:25:49 +0400

    comments

--- contracts/router/relay/RelayUtils.sol
+++ contracts/router/relay/RelayUtils.sol
@@ -18,12 +18,20 @@ struct Contracts {
 }
 
 struct FeeParams {
+    // 1. if no swap is needed then `feeToken` should be WNT address, `feeAmount` should be correct amount and `feeSwapPath` should be empty
+    // 2. if GMX v2 swap is used to swap relay fee then `feeToken` should be the address of the input token,
+    //    `feeAmount` should be the amount of the input token enough to cover the relay fee in WNT, and `feeSwapPath` should be the list of markets
+    //    through which the input token should be swapped to get the output token
+    // 3. if external calls are used then `feeToken` should be WNT address (even though the input token is different)
+    //    `feeAmount` should be 0 because the input token and amount will be specified in `externalCalls`
+    //    `feeSwapPath` should be empty
     address feeToken;
     uint256 feeAmount;
     address[] feeSwapPath;
 }
 
 struct TokenPermit {
+    // EIP-2612 permit https://eips.ethereum.org/EIPS/eip-2612
     address owner;
     address spender;
     uint256 value;
@@ -34,22 +42,46 @@ struct TokenPermit {
     address token;
 }
 
+// external calls could be used to swap relay fee and/or position collateral
+// GMX v2 swaps consume relatively a lot of gas, so using external calls could reduce txn fee
+// for relay fee the funds should be sent to the RelayRouter contract
+// for position collateral the funds should be sent to the OrderVault contract
 struct ExternalCalls {
-    address[] sendTokens;
-    uint256[] sendAmounts;
-    address[] externalCallTargets;
-    bytes[] externalCallDataList;
-    address[] refundTokens;
-    address[] refundReceivers;
+    // Relay contracts do not support `multicall` and `sendTokens` methods
+    // so all tokens and amounts should be specified here
+    address[] sendTokens; // tokens to send to ExternalHandler
+    uint256[] sendAmounts; // tokens amounts to send to ExternalHandler
+
+    // lists of external calls to be made
+    address[] externalCallTargets; // external targets to call
+    bytes[] externalCallDataList; // external call data list
+
+    // refundTokens and refundReceivers are used to send residual funds left in the ExchangeHandler
+    // for example, if "swapExactOut" is used some amount of "tokenIn" could be lefts
+    address[] refundTokens; // tokens to refund to user
+    address[] refundReceivers; // receivers of the refunds
 }
 
 struct RelayParams {
+    // oracle params are used for relay fee swap through GMX v2 pools
+    // if swap is not needed then `oracleParams` values should be empty
     OracleUtils.SetPricesParams oracleParams;
+
     ExternalCalls externalCalls;
+
+    // token permits could be used to approve spending of tokens by the Router contract
+    // instead of sending separate approval transactions
     TokenPermit[] tokenPermits;
+
     FeeParams fee;
+
+    // should be retrieved from userNonces(account)
     uint256 userNonce;
+
+    // deadline for the transaction. should be used for extra safety so signed message
+    // can't be used in future if a user signs and forgets about it
     uint256 deadline;
+
     bytes signature;
 }
 
@@ -62,6 +94,9 @@ struct UpdateOrderParams {
     uint256 minOutputAmount;
     uint256 validFromTime;
     bool autoCancel;
+
+    // should be non zero if order's execution fee should be increased
+    // otherwise should be 0
     uint256 executionFeeIncrease;
 }
 
@@ -134,7 +169,12 @@ library RelayUtils {
             )
         );
 
-    function swapFeeTokens(Contracts memory contracts, EventEmitter eventEmitter, Oracle oracle, FeeParams calldata fee) external {
+    function swapFeeTokens(
+        Contracts memory contracts,
+        EventEmitter eventEmitter,
+        Oracle oracle,
+        FeeParams calldata fee
+    ) external {
         oracle.validateSequencerUp();
 
         // swap fee tokens to WNT
