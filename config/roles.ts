3f5b4be6df9d513bbc6779c6099192da956715d0
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2025-04-03 19:39:44 +0400

    testnet roles

--- config/roles.ts
+++ config/roles.ts
@@ -93,6 +93,7 @@ export default async function (hre: HardhatRuntimeEnvironment): Promise<RolesCon
     "0xc9e1CE91d3f782499cFe787b6F1d2AF0Ca76C049": true,
     "0x9f7198eb1b9Ccc0Eb7A07eD228d8FbC12963ea33": true,
     "0xCD9706B6B71fdC4351091B5b1D910cEe7Fde28D0": true,
+    "0x508cbC56Ab57A9b0221cf1810a483f8013c92Ff3": true, // An
   };
 
   const testnetConfig = {
