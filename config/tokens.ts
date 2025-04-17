7e813f02005f92b84e536ba8b0cf7d9aef7cfa2b
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2025-04-17 21:55:09 +0000

    update buyback max price impact

--- config/tokens.ts
+++ config/tokens.ts
@@ -1070,7 +1070,7 @@ const config: {
         decimals: 8,
         heartbeatDuration: (24 + 1) * 60 * 60,
       },
-      buybackMaxPriceImpactFactor: percentageToFloat("0.8%"),
+      buybackMaxPriceImpactFactor: percentageToFloat("0.4%"),
     },
     LINK: {
       address: "0x5947BB275c521040051D82396192181b413227A3",
