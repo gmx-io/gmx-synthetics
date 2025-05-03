f81e93c5682d29bd288016d1c0bfd6e9c770b787
Max <maxgmx@protonmail.com>
2025-05-04 00:09:01 +0300

    Add/update verification files for timelock controller contracts

---
+++ verification/gov/configTimelockController.js
@@ -0,0 +1,9 @@
+// npx hardhat verify --network arbitrumSepolia --constructor-args ./verification/gov/configTimelockController.js --contract contracts/config/ConfigTimelockController.sol:ConfigTimelockController 0x14F4FEAe27D8B97C027b28e57891d7Cb176C6589
+
+module.exports = [
+  86400,
+  [],
+  [],
+  "0x73bd4303FDA1EB2d347278FEEe1Bec918d05aA1e",
+  "0xAd4759d41195aA5bA8592b8c9B454CD4E1735841"
+];
