77b2906a22059dfedbb316ad5246d463bc81d56a
Solar <call.sol.dev@gmail.com>
2025-06-06 11:41:41 +0300

    fix multichain test execution fee refund

--- test/multichain/MultichainOrderRouter.ts
+++ test/multichain/MultichainOrderRouter.ts
@@ -223,7 +223,7 @@ describe("MultichainOrderRouter", () => {
       expect(await getPositionCount(dataStore)).to.eq(1);
       // execution fee is ~0.002113 ETH and the excess is returned to user's multichain balance
       expect(await dataStore.getUint(keys.multichainBalanceKey(user1.address, wnt.address))).closeTo(
-        "1920055983360448",
+        "1923767983390144",
         expandDecimals(1, 12)
       ); // 0.004 - ~0.0021 = ~0.0019 ETH
       expect(await dataStore.getUint(keys.multichainBalanceKey(user1.address, usdc.address))).to.eq(0);
