557c9b5d39f4e21c87650c612bd28aac7dacf3fc
Max <maxgmx@protonmail.com>
2025-06-16 18:59:17 +0300

    M-02 - Incorrect positionKey When Updating Last ChainId

--- contracts/order/OrderUtils.sol
+++ contracts/order/OrderUtils.sol
@@ -24,6 +24,7 @@ import "../utils/Array.sol";
 import "../utils/AccountUtils.sol";
 import "../referral/ReferralUtils.sol";
 import "../multichain/MultichainUtils.sol";
+import "../swap/SwapUtils.sol";
 
 // @title OrderUtils
 // @dev Library for order functions
@@ -209,7 +210,15 @@ library OrderUtils {
         DataStore dataStore,
         Order.Props memory order
     ) private {
-        bytes32 positionKey = Position.getPositionKey(order.account(), order.market(), order.initialCollateralToken(), order.isLong());
+        if (Order.isSwapOrder(order.orderType())) {
+            return;
+        }
+        address collateralToken = SwapUtils.getOutputToken(
+            dataStore,
+            order.swapPath(),
+            order.initialCollateralToken()
+        );
+        bytes32 positionKey = Position.getPositionKey(order.account(), order.market(), collateralToken, order.isLong());
         dataStore.setUint(Keys.positionLastSrcChainId(positionKey), order.srcChainId());
     }
 
