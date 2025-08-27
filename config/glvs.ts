fa6230ec8ef2c93188b3260ddc13a3698c7bd5ef
An <angmxio@gmail.com>
2025-08-27 16:39:30 +0300

    enable GLV for new markets

--- config/glvs.ts
+++ config/glvs.ts
@@ -91,8 +91,8 @@ export default async function ({ network }: HardhatRuntimeEnvironment) {
           createGlvMarketConfig("CVX", arbitrum_ethUsdcDefaultCap, 1),
           createGlvMarketConfig("OKB", arbitrum_ethUsdcDefaultCap, 1),
           createGlvMarketConfig("PEPE", arbitrum_ethUsdcDefaultCap, 1),
-          // createGlvMarketConfig("AERO", arbitrum_ethUsdcDefaultCap, 1),
-          // createGlvMarketConfig("BRETT", arbitrum_ethUsdcDefaultCap, 1),
+          createGlvMarketConfig("AERO", arbitrum_ethUsdcDefaultCap, 1),
+          createGlvMarketConfig("BRETT", arbitrum_ethUsdcDefaultCap, 1),
         ],
       },
       {
