8db4fae7eb3bf7a8e39cb350165cf9576abb2d42
X <phi@gmx.io>
2025-09-19 10:41:13 +0800

    update role configs

--- scripts/roles/rolesToAdd/arbitrum.ts
+++ scripts/roles/rolesToAdd/arbitrum.ts
@@ -1,7 +1,12 @@
 export const ROLES_TO_ADD = [
   {
     role: "CONTROLLER",
-    member: "0xb8fc96d7a413c462f611a7ac0c912c2fe26eabc4",
-    contractName: "Oracle",
+    member: "0x5ac4e27341e4cccb3e5fd62f9e62db2adf43dd57",
+    contractName: "ExchangeRouter",
+  },
+  {
+    role: "ROUTER_PLUGIN",
+    member: "0x5ac4e27341e4cccb3e5fd62f9e62db2adf43dd57",
+    contractName: "ExchangeRouter",
   },
 ];
