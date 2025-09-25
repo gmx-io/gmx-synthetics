016641a36e60361ca9f1cbd71604fbb8b73ec515
Max <maxgmx@protonmail.com>
2025-09-25 09:23:44 +0300

    Increase factors

--- config/markets.ts
+++ config/markets.ts
@@ -3628,15 +3628,15 @@ const config: {
 
       minCollateralFactorForOpenInterestMultiplier: exponentToFloat("1.78e-06"),
 
-      reserveFactor: percentageToFloat("55%"),
-      openInterestReserveFactor: percentageToFloat("50%"),
+      reserveFactor: percentageToFloat("105%"),
+      openInterestReserveFactor: percentageToFloat("100%"),
       maxPnlFactorForTraders: percentageToFloat("50%"),
 
-      maxOpenInterest: decimalToFloat(250_000),
-      maxPoolUsdForDeposit: decimalToFloat(375_000),
+      maxOpenInterest: decimalToFloat(1_000_000),
+      maxPoolUsdForDeposit: decimalToFloat(1_500_000),
 
-      maxLongTokenPoolAmount: expandDecimals(5, 8), // ~500K USD (2x max open interest)
-      maxShortTokenPoolAmount: expandDecimals(500_000, 6), // ~500K USD (2x max open interest)
+      maxLongTokenPoolAmount: expandDecimals(18, 8), // ~2M USD (2x max open interest)
+      maxShortTokenPoolAmount: expandDecimals(2_000_000, 6), // ~2M USD (2x max open interest)
     },
     {
       tokens: { indexToken: "BNB", longToken: "WBTC.e", shortToken: "USDC" },
