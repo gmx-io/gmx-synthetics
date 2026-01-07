20e80fb489a41dc9e5ebab32e4db5685cdc42769
X <phi@gmx.io>
2026-01-07 09:10:47 +0800

    update role configs

--- scripts/roles/rolesToRemove/arbitrum.ts
+++ scripts/roles/rolesToRemove/arbitrum.ts
@@ -1,5 +1,20 @@
 export const ROLES_TO_REMOVE = [
   {
+    role: "CONFIG_KEEPER",
+    member: "0x2966bE26c325796c0e63D769AbD457532411Ffed",
+    contractName: "ConfigSyncer",
+  },
+  {
+    role: "CONTROLLER",
+    member: "0x2966bE26c325796c0e63D769AbD457532411Ffed",
+    contractName: "ConfigSyncer",
+  },
+  {
+    role: "CONTROLLER",
+    member: "0x0BBbbF9D0cbdE8069e926c859E530B00Bfe90072",
+    contractName: "Config",
+  },
+  {
     role: "CONTROLLER",
     member: "0x7129Ea01F0826c705d6F7ab01Cf3C06bb83E9397",
     contractName: "LayerZeroProvider",
