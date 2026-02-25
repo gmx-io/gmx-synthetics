b464358259151a11e58719d1fad35f64b6c9e4f9
Solar <call.sol.dev@gmail.com>
2026-02-25 09:08:00 +0300

    update deployment docs

--- scripts/roles/rolesToAdd/arbitrum.ts
+++ scripts/roles/rolesToAdd/arbitrum.ts
@@ -1,17 +1,1 @@
-export const ROLES_TO_ADD = [
-  {
-    role: "CONFIG_KEEPER",
-    member: "0x4c8569e2f8a8af32e0464e53d76449ca7a004baf",
-    contractName: "ConfigSyncer",
-  },
-  {
-    role: "CONTROLLER",
-    member: "0x33d1a645b9e9fc19b06fe02981180c8ddaee75b1",
-    contractName: "Config",
-  },
-  {
-    role: "CONTROLLER",
-    member: "0x4c8569e2f8a8af32e0464e53d76449ca7a004baf",
-    contractName: "ConfigSyncer",
-  },
-];
+export const ROLES_TO_ADD = [];
--- scripts/roles/rolesToRemove/arbitrum.ts
+++ scripts/roles/rolesToRemove/arbitrum.ts
@@ -1,22 +1,7 @@
 export const ROLES_TO_REMOVE = [
   {
-    role: "CONFIG_KEEPER",
-    member: "0x2966bE26c325796c0e63D769AbD457532411Ffed",
-    contractName: "ConfigSyncer",
-  },
-  {
     role: "CONTROLLER",
-    member: "0x2966bE26c325796c0e63D769AbD457532411Ffed",
-    contractName: "ConfigSyncer",
-  },
-  {
-    role: "CONTROLLER",
-    member: "0x0BBbbF9D0cbdE8069e926c859E530B00Bfe90072",
-    contractName: "Config",
-  },
-  {
-    role: "CONTROLLER",
-    member: "0x7129Ea01F0826c705d6F7ab01Cf3C06bb83E9397",
-    contractName: "LayerZeroProvider",
+    member: "0x7EB417637a3E6d1C19E6d69158c47610b7a5d9B3",
+    contractName: "FeeHandler",
   },
 ];
