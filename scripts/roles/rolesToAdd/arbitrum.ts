e1cb51ac10294fef507508a4c26dfaa4bc667d1b
X <phi@gmx.io>
2026-03-25 16:00:45 +0800

    update roles

--- scripts/roles/rolesToAdd/arbitrum.ts
+++ scripts/roles/rolesToAdd/arbitrum.ts
@@ -1,10 +1,5 @@
 export const ROLES_TO_ADD = [
   {
-    role: "CONTRIBUTOR_KEEPER",
-    member: "0x58f582455b54d7c83d03bceed95faf72b37fddd7",
-    contractName: "SafeProxy",
-  },
-  {
     role: "TIMELOCK_ADMIN",
     member: "0x58f582455b54d7c83d03bceed95faf72b37fddd7",
     contractName: "SafeProxy",
