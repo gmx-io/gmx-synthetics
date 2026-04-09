46e865b167e3e6cb4be448ce388ea0bedee09a0c
An <angmxio@gmail.com>
2026-04-09 13:23:12 +0300

    update market addresses

--- scripts/switchover/arbitrumSepolia/GOLD_closed.sh
+++ scripts/switchover/arbitrumSepolia/GOLD_closed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-MARKET=0x0000000000000000000000000000000000000001 \
+MARKET=0x0Df2BE76F517BCF0000AbfFcB6344B3b2aC4Cc4f \
 MARKET_STATE=offHours \
 npx hardhat run scripts/updateMarketConfig.ts --network arbitrumSepolia
--- scripts/switchover/arbitrumSepolia/SILVER_open.sh
+++ scripts/switchover/arbitrumSepolia/SILVER_open.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-MARKET=0x0000000000000000000000000000000000000002 \
+MARKET=0x448Fa722717df299ee197E2F6d8EB7911EFF6cEc \
 MARKET_STATE=onHours \
 npx hardhat run scripts/updateMarketConfig.ts --network arbitrumSepolia
