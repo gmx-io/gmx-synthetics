54b1d44b7792357ca821c97b06c0a2c80b05c28b
An <angmxio@gmail.com>
2026-04-09 14:30:18 +0300

    upd RWAs switchover scripts

--- scripts/switchover/setup_cron.sh
+++ scripts/switchover/setup_cron.sh
@@ -5,16 +5,15 @@ set -euo pipefail
 REPO_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
 RUN_SH="$REPO_DIR/scripts/switchover/run.sh"
 
-# XAU (arbitrumSepolia)
 # Weekly: closed Fri 20:45 UTC, open Sun 22:15 UTC
 # Daily Mon-Thu: closed 20:45 UTC, open 22:15 UTC
 CRON_ENTRIES="
 45 20 * * 1-5  $RUN_SH $REPO_DIR/scripts/switchover/arbitrumSepolia/XAU_closed.sh
 15 22 * * 0,1-4  $RUN_SH $REPO_DIR/scripts/switchover/arbitrumSepolia/XAU_open.sh
-45 20 * * 1-5  $RUN_SH $REPO_DIR/scripts/switchover/arbitrumSepolia/GOLD_closed.sh
-15 22 * * 0,1-4  $RUN_SH $REPO_DIR/scripts/switchover/arbitrumSepolia/GOLD_open.sh
-45 20 * * 1-5  $RUN_SH $REPO_DIR/scripts/switchover/arbitrumSepolia/SILVER_closed.sh
-15 22 * * 0,1-4  $RUN_SH $REPO_DIR/scripts/switchover/arbitrumSepolia/SILVER_open.sh
+45 20 * * 1-5  $RUN_SH $REPO_DIR/scripts/switchover/arbitrum/GOLD_closed.sh
+15 22 * * 0,1-4  $RUN_SH $REPO_DIR/scripts/switchover/arbitrum/GOLD_open.sh
+45 20 * * 1-5  $RUN_SH $REPO_DIR/scripts/switchover/arbitrum/SILVER_closed.sh
+15 22 * * 0,1-4  $RUN_SH $REPO_DIR/scripts/switchover/arbitrum/SILVER_open.sh
 "
 
 # remove old switchover entries, append new ones
