def5bcc1b0ec5d9254dff10551078b426bc0a6c8
An <angmxio@gmail.com>
2026-04-14 14:22:13 +0300

    upd caps

--- config/markets.ts
+++ config/markets.ts
@@ -3978,8 +3978,8 @@ const config: {
       reserveFactor: percentageToFloat("105%"),
       openInterestReserveFactor: percentageToFloat("100%"),
 
-      maxOpenInterest: decimalToFloat(500),
-      maxPoolUsdForDeposit: decimalToFloat(500),
+      maxOpenInterest: decimalToFloat(1),
+      maxPoolUsdForDeposit: decimalToFloat(1),
 
       maxLongTokenPoolAmount: expandDecimals(6, 18), // ~21K USD
       maxShortTokenPoolAmount: expandDecimals(21_000, 6), // ~21K USD
