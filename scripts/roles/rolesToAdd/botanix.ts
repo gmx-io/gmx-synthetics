3ca548a3bbbc7ba6cebd5c58fc511d3706ce5072
X <phi@gmx.io>
2026-04-14 11:36:53 +0800

    update role configs

--- scripts/roles/rolesToAdd/botanix.ts
+++ scripts/roles/rolesToAdd/botanix.ts
@@ -4,4 +4,24 @@ export const ROLES_TO_ADD = [
     member: "0x58f582455b54d7c83d03bceed95faf72b37fddd7",
     contractName: "SafeProxy",
   },
+  {
+    role: "CLAIM_ADMIN",
+    member: "0x8d1d2e24ec641edc6a1ebe0f3ae7af0ebc573e0d",
+    contractName: "Safe",
+  },
+  {
+    role: "CONFIG_KEEPER",
+    member: "0x8d1d2e24ec641edc6a1ebe0f3ae7af0ebc573e0d",
+    contractName: "Safe",
+  },
+  {
+    role: "TIMELOCK_ADMIN",
+    member: "0x8d1d2e24ec641edc6a1ebe0f3ae7af0ebc573e0d",
+    contractName: "Safe",
+  },
+  {
+    role: "TIMELOCK_MULTISIG",
+    member: "0x8d1d2e24ec641edc6a1ebe0f3ae7af0ebc573e0d",
+    contractName: "Safe",
+  },
 ];
