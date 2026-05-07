e21e1653dd7288a282793b5aab98e7e60960bfc9
Solar <call.sol.dev@gmail.com>
2026-05-07 05:17:45 +0300

    do not setAddress for EIP6492 for mainnets

--- deploy/deployEIP6492Deployer.ts
+++ deploy/deployEIP6492Deployer.ts
@@ -1,6 +1,7 @@
 import { createDeployFunction } from "../utils/deploy";
 import { EIP6492_DEPLOYER } from "../utils/keys";
 import { grantRoleIfNotGranted } from "../utils/role";
+import { isExistingMainnetDeployment } from "../config/chains";
 
 const func = createDeployFunction({
   contractName: "EIP6492Deployer",
@@ -11,10 +12,16 @@ const func = createDeployFunction({
     const dataStore = await ethers.getContractAt("DataStore", dataStoreDeployment.address);
     const currentValue = await dataStore.getAddress(EIP6492_DEPLOYER);
     if (currentValue.toLowerCase() !== deployedContract.address.toLowerCase()) {
-      console.log(`Setting EIP6492_DEPLOYER in DataStore to ${deployedContract.address}`);
-      // const configDeployment = await get("Config");
-      // const config = await ethers.getContractAt("Config", configDeployment.address);
-      // await config.setAddress(EIP6492_DEPLOYER, "0x", deployedContract.address);
+      if (hre.gmx.isExistingMainnetDeployment) {
+        console.info(
+          "skipping EIP6492_DEPLOYER address config, as deployer does not have access to setAddress in the config"
+        );
+      } else {
+        console.log(`Setting EIP6492_DEPLOYER in DataStore to ${deployedContract.address}`);
+        const configDeployment = await get("Config");
+        const config = await ethers.getContractAt("Config", configDeployment.address);
+        await config.setAddress(EIP6492_DEPLOYER, "0x", deployedContract.address);
+      }
     }
   },
   id: "EIP6492Deployer_1",
