6cf99120a93d26eca554c21d844d0ac03d9778ae
Max <maxgmx@protonmail.com>
2026-05-13 17:32:14 +0300

    add script to grant keeper role to MultichainOrderRouter on ReferralStorageTimelock

---
+++ scripts/updateReferralStorageKeepers.ts
@@ -0,0 +1,25 @@
+import hre from "hardhat";
+
+import { getExistingContractAddresses } from "../config/overwrite";
+import { signExternally } from "../utils/signer";
+
+export async function main() {
+  const { ReferralStorage: referralStorageInfo } = getExistingContractAddresses(hre.network);
+  const referralStorage = await hre.ethers.getContractAt("ReferralStorage", referralStorageInfo.address);
+
+  const govAddress = await referralStorage.gov();
+  const gov = await hre.ethers.getContractAt("MockTimelockV1", govAddress);
+
+  const multichainOrderRouter = await hre.ethers.getContract("MultichainOrderRouter");
+  if (await gov.isKeeper(multichainOrderRouter.address)) {
+    console.log("MultichainOrderRouter is already a keeper.");
+    return;
+  }
+
+  await signExternally(await gov.populateTransaction.setKeeper(multichainOrderRouter.address, true));
+}
+
+main().catch((ex) => {
+  console.error(ex);
+  process.exit(1);
+});
