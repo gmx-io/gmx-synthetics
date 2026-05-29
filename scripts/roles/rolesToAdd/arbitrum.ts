1be688e5625176c7b1a217df3ea3148f180b811b
Solar <call.sol.dev@gmail.com>
2026-05-29 07:44:53 +0300

    update rolesToAdd

--- scripts/roles/rolesToAdd/arbitrum.ts
+++ scripts/roles/rolesToAdd/arbitrum.ts
@@ -1,7 +1,6 @@
 export const ROLES_TO_ADD = [
   {
-    role: "CONTROLLER",
-    member: "0x5ca40cbb5321bbe4ef74ddf01ccd2b4bcc76a9f9",
-    contractName: "RiskOracleConfig",
+    role: "RISK_ORACLE",
+    member: "0x16ffb5a90be6a0c0c5179d62ced944fdd8108c72",
   },
 ];
--- scripts/roles/rolesToAdd/avalanche.ts
+++ scripts/roles/rolesToAdd/avalanche.ts
@@ -1,7 +1,6 @@
 export const ROLES_TO_ADD = [
   {
-    role: "CONTROLLER",
-    member: "0x23BaFd10C7c15eaff7754D624BC660cbAA7d2Ec9",
-    contractName: "RiskOracleConfig",
+    role: "RISK_ORACLE",
+    member: "0x16ffb5a90be6a0c0c5179d62ced944fdd8108c72",
   },
 ];
