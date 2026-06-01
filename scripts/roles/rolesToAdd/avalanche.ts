fbe90d3df609b4257686611af1f9c5d4dde42964
Solar <call.sol.dev@gmail.com>
2026-06-01 05:24:53 +0300

    grant risk oracle roles

--- scripts/roles/rolesToAdd/avalanche.ts
+++ scripts/roles/rolesToAdd/avalanche.ts
@@ -3,4 +3,9 @@ export const ROLES_TO_ADD = [
     role: "RISK_ORACLE",
     member: "0x16ffb5a90be6a0c0c5179d62ced944fdd8108c72",
   },
+  {
+    role: "CONTROLLER",
+    member: "0x23BaFd10C7c15eaff7754D624BC660cbAA7d2Ec9",
+    contractName: "RiskOracleConfig",
+  },
 ];
