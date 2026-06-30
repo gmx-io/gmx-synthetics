378e23cca8825a4055e54d2204422b977beeb69d
Solar <call.solar.dev@gmail.com>
2026-07-01 02:07:46 +0300

    fix tests(removed whitespace in hash)

--- contracts/router/relay/RelayUtils.sol
+++ contracts/router/relay/RelayUtils.sol
@@ -167,7 +167,7 @@ library RelayUtils {
         keccak256(bytes("TransferRequests(address[] tokens,address[] receivers,uint256[] amounts)"));
 
     bytes32 public constant BRIDGE_OUT_TYPEHASH =
-        keccak256(bytes("BridgeOut(address account, address token,uint256 amount,uint256 minAmountOut,address provider,bytes data,address bridgeFeeToken,uint256 bridgeFeeAmount,address[] bridgeFeeSwapPath,bytes32 relayParams)"));
+        keccak256(bytes("BridgeOut(address account,address token,uint256 amount,uint256 minAmountOut,address provider,bytes data,address bridgeFeeToken,uint256 bridgeFeeAmount,address[] bridgeFeeSwapPath,bytes32 relayParams)"));
 
     bytes32 public constant CLAIM_FUNDING_FEES_TYPEHASH =
         keccak256(bytes("ClaimFundingFees(address account,address[] markets,address[] tokens,address receiver,bytes32 relayParams)"));
