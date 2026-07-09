a3e702dda8132bd0dad3c8bee2e0c44c19231b13
Max <maxgmx@protonmail.com>
2026-07-09 13:27:15 +0300

    Add SimulationRouter to requiredRolesForContracts so validateRoles catches new contracts with no old counterpart

--- config/roles.ts
+++ config/roles.ts
@@ -32,6 +32,7 @@ const requiredRolesForContracts = {
     "GlvRouter",
     "GelatoRelayRouter",
     "SubaccountGelatoRelayRouter",
+    "SimulationRouter",
 
     "OrderHandler",
     "JitOrderHandler",
@@ -66,6 +67,7 @@ const requiredRolesForContracts = {
     "GlvRouter",
     "GelatoRelayRouter",
     "SubaccountGelatoRelayRouter",
+    "SimulationRouter",
 
     "MultichainGlvRouter",
     "MultichainGmRouter",
