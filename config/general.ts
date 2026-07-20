30c95353fea802088de040df308030073b044698
Solar <call.solar.dev@gmail.com>
2026-07-20 10:38:40 +0300

    eipDeployer address update

--- config/general.ts
+++ config/general.ts
@@ -225,6 +225,7 @@ export default async function ({ network }: HardhatRuntimeEnvironment) {
       estimatedGasFeeMultiplierFactor: false,
       executionGasFeeMultiplierFactor: false,
       sequencerUptimeFeed: "0xFdB631F5EE196F0ed6FAa767959853A9F217697D",
+      eip6492Deployer: "0x597A7898B0D31BefEE52488b3F046eAc870F94CF",
 
       increaseOrderGasLimit: 3_000_000,
       decreaseOrderGasLimit: 3_000_000,
@@ -260,6 +261,7 @@ export default async function ({ network }: HardhatRuntimeEnvironment) {
       },
     },
     avalanche: {
+      eip6492Deployer: "0xf78C3dd7FEaBfDa87B7B3079538633557ba53cbA",
       multichainProviders: {
         "0x5634c4a5FEd09819E3c46D86A965Dd9447d86e47": true, // StargatePoolUSDC
         "0x12dC9256Acc9895B076f6638D628382881e62CeE": true, // StargatePoolUSDT
@@ -288,6 +290,7 @@ export default async function ({ network }: HardhatRuntimeEnvironment) {
       maxCallbackGasLimit: 5_200_000,
       nativeTokenTransferGasLimit: 150_000,
       refundExecutionFeeGasLimit: 260_000,
+      eip6492Deployer: "0x32784C86a35284316427a4C18e02E68bD7d352be",
     },
   }[network.name];
 
